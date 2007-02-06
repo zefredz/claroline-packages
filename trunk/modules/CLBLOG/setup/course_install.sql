7387e6082a7b280bbfb5495d49b556f6cfd53d7f
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-02-06 16:33:16 +0000

    CLBLOG - setup

---
+++ trunk/modules/CLBLOG/setup/course_install.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS __CL_COURSE__blog_posts (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  userId int(11) unsigned NOT NULL default '0',
+  groupId int(11) unsigned NOT NULL default '0',
+  title VARCHAR(255) NOT NULL DEFAULT '',
+  chapo TEXT NOT NULL DEFAULT '',
+  contents TEXT NOT NULL DEFAULT '',
+  ctime datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY(id)
+) TYPE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS __CL_COURSE__blog_comments (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  postId int(11) unsigned NOT NULL default '0',
+  userId int(11) unsigned NOT NULL default '0',
+  contents TEXT NOT NULL DEFAULT '',
+  ctime datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY(id)
+) TYPE=MyISAM;
