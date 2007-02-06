a1ccaa80d37c93f7d443f520780be46e70f85562
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2007-02-06 16:34:53 +0000

    CLBLOG - libs

---
+++ modules/CLBLOG/lib/blog/comment.class.php
@@ -0,0 +1,218 @@
+<?php // $Id$
+
+    // vim: expandtab sw=4 ts=4 sts=4:
+    
+    /**
+     * Blog Comment class
+     *
+     * @version     1.9 $Revision$
+     * @copyright   2001-2007 Universite catholique de Louvain (UCL)
+     * @author      Frederic Minne <zefredz@claroline.net>
+     * @license     http://www.gnu.org/copyleft/gpl.html 
+     *              GNU GENERAL PUBLIC LICENSE
+     * @package     CLBLOG
+     */
+    
+    if ( count( get_included_files() ) == 1 ) die( '---' );
+    
+    require_once dirname(__FILE__) . "/../database/connection.class.php";
+    
+    class Blog_Comment
+    {
+        var $config;
+        
+        var $connection;
+        
+        function Blog_Comment( &$connection, $config = null )
+        {
+            $this->config = $config ;
+            $this->connection =& $connection;
+        }
+        
+        function addComment( $postId, $userId, $contents )
+        {
+            $this->connection->connect();
+            
+            $sql = "INSERT INTO " . $this->config['blog_comments'] . " "
+                . "SET userId = ".(int) $userId.", "
+                . "contents = '".addslashes($contents)."', "
+                . "ctime = '".date( "Y-m-d H:i:s" )."', "
+                . "postId = ".(int) $postId
+                ;
+                
+            $this->connection->executeQuery( $sql );
+            
+            $id = $this->connection->getLastInsertId();
+            
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return $id;
+            }
+        }
+        
+        function editComment( $id, $postId, $userId, $contents )
+        {
+            $this->connection->connect();
+            
+            $sql = "UPDATE " . $this->config['blog_comments'] . " "
+                . "SET userId = ".(int) $userId.", "
+                . "contents = '".addslashes($contents)."', "
+                . "ctime = '".date( "Y-m-d H:i:s" )."', "
+                . "postId = ".(int) $postId . " "
+                . "WHERE id = " . (int) $id
+                ;
+                
+            $this->connection->executeQuery( $sql );
+            
+            $id = $this->connection->getLastInsertId();
+            
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return $id;
+            }
+        }
+        
+        function getPostComment( $postId )
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * "
+                . "FROM " . $this->config['blog_comments'] . " "
+                . "WHERE postId = " . (int) $postId . " "
+                . "ORDER BY id ASC"
+                ;
+
+            $result = $this->connection->getAllRowsFromQuery( $sql );
+            
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return $result;
+            }
+        }
+        
+        function getComment( $id )
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * \n"
+                . "FROM " . $this->config['blog_comments'] . " \n"
+                . "WHERE id = " . (int) $id . " \n"
+                . "ORDER BY id ASC"
+                ;
+
+            $result = $this->connection->getRowFromQuery( $sql );
+            
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return $result;
+            }
+        }
+        
+        function getAll()
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * "
+                . "FROM " . $this->config['blog_comments'] . " "
+                . "ORDER BY id ASC"
+                ;
+
+            $result = $this->connection->getAllRowsFromQuery( $sql );
+            
+            if ( $this->connection->hasError() )
+            {
+                return null;
+            }
+            else
+            {
+                $ret = array();
+                
+                foreach ( $result as $row )
+                {
+                    $ret[] = $row;
+                }
+                
+                return $ret;
+            }
+        }
+        
+        function deleteComment( $id )
+        {
+            $this->connection->connect();
+            
+            $sql = "DELETE FROM " . $this->config['blog_comments'] . " "
+                . "WHERE id = " . (int) $id
+                ;
+                
+            $this->connection->executeQuery( $sql );
+
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        function deletePostComment( $postId )
+        {
+            $this->connection->connect();
+            
+            $sql = "DELETE FROM " . $this->config['blog_comments'] . " "
+                . "WHERE postId = " . (int) $postId
+                ;
+                
+            $this->connection->executeQuery( $sql );
+
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        function getCommentNumber( $postId )
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT count(*) FROM " . $this->config['blog_comments'] . " \n"
+                . "WHERE postId = " . (int) $postId
+                ;
+            
+            return $this->connection->getSingleValueFromQuery( $sql );
+        }
+        
+        function commentExists( $id )
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * "
+                . "FROM " . $this->config['blog_comments'] . " "
+                . "WHERE id = " . (int) $id
+                ;
+
+            return $this->connection->queryReturnsResult( $sql );
+        }
+    }
+?>
---
+++ modules/CLBLOG/lib/blog/post.class.php
@@ -0,0 +1,142 @@
+<?php // $Id$
+
+    // vim: expandtab sw=4 ts=4 sts=4:
+    
+    /**
+     * Blog Post class
+     *
+     * @version     1.9 $Revision$
+     * @copyright   2001-2007 Universite catholique de Louvain (UCL)
+     * @author      Frederic Minne <zefredz@claroline.net>
+     * @license     http://www.gnu.org/copyleft/gpl.html 
+     *              GNU GENERAL PUBLIC LICENSE
+     * @package     CLBLOG
+     */
+    
+    if ( count( get_included_files() ) == 1 ) die( '---' );
+    
+    require_once dirname(__FILE__) . "/../database/connection.class.php";
+    
+    class Blog_Post
+    {
+        var $config;
+        
+        var $connection;
+        
+        function Blog_Post( &$connection, $config )
+        {
+            $this->config = $config ;
+            $this->connection =& $connection;
+        }
+        
+        function addPost( $userId, $title, $contents, $chapo = '', $groupId = 0 )
+        {
+            $this->connection->connect();
+            
+            $sql = "INSERT INTO " . $this->config['blog_posts'] . " "
+                . "SET userId = ".(int) $userId.", "
+                . "groupId = ".(int) $groupId.", "
+                . "title = '".addslashes($title)."', "
+                . "chapo = '".addslashes($chapo)."', "
+                . "contents = '".addslashes($contents)."', "
+                . "ctime = '".date( "Y-m-d H:i:s" )."' "
+                ;
+                
+            $this->connection->executeQuery( $sql );
+            
+            $id = $this->connection->getLastInsertId();
+            
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return $id;
+            }
+        }
+        
+        function updatePost( $postId, $userId, $title, $contents, $chapo = '', $groupId )
+        {
+            $this->connection->connect();
+            
+            $sql = "UPDATE " . $this->config['blog_posts'] . " "
+                . "SET userId = ".(int) $userId.", "
+                . "groupId = ".(int) $groupId.", "
+                . "title = '".addslashes($title)."', "
+                . "chapo = '".addslashes($chapo)."', "
+                . "contents = '".addslashes($contents)."' "
+                . "WHERE id = ".(int) $postId
+                ;
+                
+            $this->connection->executeQuery( $sql );
+            
+            $id = $this->connection->getLastInsertId();
+            
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return $id;
+            }
+        }
+        
+        function postExists( $postId )
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * "
+                . "FROM " . $this->config['blog_posts'] . " "
+                . "WHERE id = " . (int) $postId
+                ;
+
+            return $this->connection->queryReturnsResult( $sql );
+        }
+        
+        function getPost( $postId )
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * "
+                . "FROM " . $this->config['blog_posts'] . " "
+                . "WHERE id = " . (int) $postId
+                ;
+
+            return $this->connection->getRowFromQuery( $sql );
+        }
+        
+        function getAll()
+        {
+            $this->connection->connect();
+            
+            $sql = "SELECT * "
+                . "FROM " . $this->config['blog_posts'] . " "
+                . "ORDER BY id DESC"
+                ;
+
+            return $this->connection->getAllRowsFromQuery( $sql );
+        }
+        
+        function deletePost( $id )
+        {
+            $this->connection->connect();
+            
+            $sql = "DELETE FROM " . $this->config['blog_posts'] . " "
+                . "WHERE id = " . (int) $id
+                ;
+                
+            $this->connection->executeQuery( $sql );
+
+            if ( $this->connection->hasError() )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+?>
