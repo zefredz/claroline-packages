8631387750931f738bc0c88b26f22f2022065d2d
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-03-01 13:32:20 +0000

    Do not echo empty properties

--- trunk/modules/ADSENSE/adsense.class.php
+++ trunk/modules/ADSENSE/adsense.class.php
@@ -20,17 +20,18 @@ class adsense
 	{
 		$this->prefs = array();
 		
-		$this->prefs['client'] = get_conf('google_ad_client');
-		$this->prefs['width'] = get_conf('google_ad_width');		
-		$this->prefs['height'] = get_conf('google_ad_height');		
-    	$this->prefs['format'] = get_conf('google_ad_format');
-    	$this->prefs['type'] = get_conf('google_ad_type');    	
-    	$this->prefs['channel'] = get_conf('google_ad_channel');    	
-		$this->prefs['borderColor'] = get_conf('google_ad_borderColor');		
-		$this->prefs['bgColor'] = get_conf('google_ad_bgColor');	
-		$this->prefs['linkColor'] = get_conf('google_ad_linkColor');		
-    	$this->prefs['urlColor'] = get_conf('google_ad_urlColor');
-		$this->prefs['textColor'] = get_conf('google_ad_textColor');				
+		$this->prefs['ad_client'] = get_conf('google_ad_client');
+		$this->prefs['ad_width'] = get_conf('google_ad_width');		
+		$this->prefs['ad_height'] = get_conf('google_ad_height');		
+    	$this->prefs['ad_format'] = get_conf('google_ad_format');
+    	$this->prefs['ad_type'] = get_conf('google_ad_type');    	
+    	$this->prefs['cpa_choice'] = get_conf('google_cpa_choice');
+    	$this->prefs['ad_channel'] = get_conf('google_ad_channel');    	    	
+		$this->prefs['ad_borderColor'] = get_conf('google_ad_borderColor');		
+		$this->prefs['ad_bgColor'] = get_conf('google_ad_bgColor');	
+		$this->prefs['ad_linkColor'] = get_conf('google_ad_linkColor');		
+    	$this->prefs['ad_urlColor'] = get_conf('google_ad_urlColor');
+		$this->prefs['ad_textColor'] = get_conf('google_ad_textColor');				
 	}
 	
 	function display()
@@ -38,17 +39,21 @@ class adsense
 	    $html = "\n";
 
         $html .= '<script type="text/javascript"><!--' . "\n"
-        .    '    google_ad_client = "'.$this->prefs['client'].'";' . "\n"
-        .    '    google_ad_width = '.$this->prefs['width'].';' . "\n"
-        .    '    google_ad_height = '.$this->prefs['height'].';' . "\n"
-        .    '    google_ad_format = "'.$this->prefs['format'].'";' . "\n"
-        .    '    google_ad_type = "'.$this->prefs['type'].'";' . "\n"
-        .    '    google_ad_channel = "'.$this->prefs['channel'].'";' . "\n"
-        .    '    google_color_border = "'.$this->prefs['borderColor'].'";' . "\n"
-        .    '    google_color_bg = "'.$this->prefs['bgColor'].'";' . "\n"
-        .    '    google_color_link = "'.$this->prefs['linkColor'].'";' . "\n"
-        .    '    google_color_url = "'.$this->prefs['urlColor'].'";' . "\n"
-        .    '    google_color_text = "'.$this->prefs['textColor'].'";' . "\n"
+        .    '    google_ad_client = "'.$this->prefs['ad_client'].'";' . "\n"
+        .    '    google_ad_width = '.$this->prefs['ad_width'].';' . "\n"
+        .    '    google_ad_height = '.$this->prefs['ad_height'].';' . "\n"
+        .    '    google_ad_format = "'.$this->prefs['ad_format'].'";' . "\n";
+        
+        if( !empty($this->prefs['ad_type']) )       $html .= '    google_ad_type = "'.$this->prefs['ad_type'].'";' . "\n";
+        if( !empty($this->prefs['cpa_choice']) )    $html .= '    google_cpa_choice = "'.$this->prefs['cpa_choice'].'";' . "\n";                
+        if( !empty($this->prefs['ad_channel']) )    $html .= '    google_ad_channel = "'.$this->prefs['ad_channel'].'";' . "\n";
+
+
+        $html .= '    google_color_border = "'.$this->prefs['ad_borderColor'].'";' . "\n"
+        .    '    google_color_bg = "'.$this->prefs['ad_bgColor'].'";' . "\n"
+        .    '    google_color_link = "'.$this->prefs['ad_linkColor'].'";' . "\n"
+        .    '    google_color_url = "'.$this->prefs['ad_urlColor'].'";' . "\n"
+        .    '    google_color_text = "'.$this->prefs['ad_textColor'].'";' . "\n"
         .    '//--></script>' . "\n";
         
         $html .= '<script type="text/javascript"' . "\n"
