7835d104e5da7cd396f7c96af3d847242bae7ce8
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-03-01 15:02:33 +0000

    Add SPREADFF module on this repository

---
+++ modules/SPREADFF/entry.php
@@ -0,0 +1,37 @@
+<?php // $Id$
+/**
+ *
+ * @version 0.1 $Revision$
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @author Claroline team <info@claroline.net>
+ *
+ * @package SPREADFF
+ *
+ */
+
+if ( count( get_included_files() ) == 1 ) die( '---' );
+
+$tlabelReq = 'SPREADFF';
+include_once claro_get_conf_repository().'SPREADFF.conf.php';
+
+$html = '';
+
+if( !strstr($_SERVER['HTTP_USER_AGENT'], 'Firefox') || get_conf('spreadff_show_in_all_browsers') ) 
+{
+
+    $html .= "\n\n"
+    .   '<!-- Spread Firefox -->' . "\n"
+    .   '<p align="center">'
+    .   '<a href="'.get_conf('spreadff_link').'">'
+    .   '<img border="0" alt="" title="" src="'.get_conf('spreadff_img_url').'"/>'
+    .   '</a>'
+    .   '</p>' . "\n"
+    .   '<!-- Spread Firefox -->' . "\n\n";
+
+}
+
+$claro_buffer->append($html);
+
+?>
