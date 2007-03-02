f209dc66d1b6494637ad51591f5c5e706f8ab2a5
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-03-02 13:21:56 +0000

    Add SPREADFF module on this repository; !!! cannot work with claroline 1.8.3, it requires the new text type in config tool

---
+++ trunk/modules/HTMLJS/entry.php
@@ -0,0 +1,35 @@
+<?php // $Id$
+/**
+ * CLAROLINE
+ *
+ * @copyright 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @see http://www.claroline.net/wiki/index.php/Config
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ * @package HTMLJS
+ */
+
+if ( count( get_included_files() ) == 1 ) die( '---' );
+
+$tlabelReq = 'HTMLJS';
+include_once claro_get_conf_repository().'HTMLJS.conf.php';
+
+$htmljs_text = get_conf('htmljs_text');
+
+$html = '';
+
+if( !empty( $htmljs_text ) )
+{
+    $html .= "\n\n"
+    .   '<div>'
+    .   get_conf('htmljs_text')
+    .   '</div>' . "\n\n";
+}
+
+$claro_buffer->append($html);
+
+?>
