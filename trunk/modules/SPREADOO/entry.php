20d914a7cbd32beb7dc72f2e94724dff30628258
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-04-18 15:44:02 +0000

    SPREADOO applet

---
+++ trunk/modules/SPREADOO/entry.php
@@ -0,0 +1,32 @@
+<?php // $Id$
+/**
+ *
+ * @version 0.1 $Revision$
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @author Claroline team <info@claroline.net>
+ *
+ * @package SPREADOO
+ *
+ */
+
+if ( count( get_included_files() ) == 1 ) die( '---' );
+
+include_once claro_get_conf_repository().'SPREADOO.conf.php';
+
+$html = '';
+
+$html .= "\n\n"
+.   '<!-- Spread Open Office -->' . "\n"
+.   '<div align="center">'
+.   '<a href="'.get_conf('spreadoo_link').'">'
+.   '<img border="0" alt="" title="" src="'.get_conf('spreadoo_img_url').'"/>'
+.   '</a>'
+.   '</div>' . "\n"
+.   '<!-- /Spread Open Office -->' . "\n\n";
+
+
+$claro_buffer->append($html);
+
+?>
