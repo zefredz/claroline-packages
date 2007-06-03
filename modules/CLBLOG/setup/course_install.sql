2338569af313da75178b6c6d7bfcffb78184d6d3
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2007-06-03 12:19:26 +0000

    remove NOT NULL DEFAULT '' from text fields since it causes the install to fail with some MySQL configurations

--- modules/CLBLOG/setup/course_install.sql
+++ modules/CLBLOG/setup/course_install.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS `__CL_COURSE__blog_posts` (
   userId int(11) unsigned NOT NULL default '0',
   groupId int(11) unsigned NOT NULL default '0',
   title VARCHAR(255) NOT NULL DEFAULT '',
-  chapo TEXT NOT NULL DEFAULT '',
-  contents TEXT NOT NULL DEFAULT '',
+  chapo TEXT,
+  contents TEXT,
   ctime datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY(id)
 ) TYPE=MyISAM;
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `__CL_COURSE__blog_comments` (
   id INT(11) NOT NULL AUTO_INCREMENT,
   postId int(11) unsigned NOT NULL default '0',
   userId int(11) unsigned NOT NULL default '0',
-  contents TEXT NOT NULL DEFAULT '',
+  contents TEXT,
   ctime datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY(id)
 ) TYPE=MyISAM;
