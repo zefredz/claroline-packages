b1d592bc531981b152c440bb15f83699b3746afc
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2007-07-18 11:02:13 +0000

    MySQL admin module

---
+++ modules/CLMYADM/entry.php
@@ -0,0 +1,20 @@
+<?php // $Id$
+
+    // vim: expandtab sw=4 ts=4 sts=4 foldmethod=marker:
+
+    if ( claro_is_platform_admin() )
+    {
+        $old_tlabel = $GLOBALS['tlabelReq'];
+        $GLOBALS['tlabelReq'] = 'CLMYADM';
+        $claro_buffer->append( 
+            claro_html_icon_button(
+                get_module_url('CLMYADM') . '/wmsa.php',
+                'sqladmin',
+                get_lang('MySQLAdmin'),
+                get_lang('Database administration')
+            )
+        );
+
+        $GLOBALS['tlabelReq'] = $old_tlabel;
+    }
+?>
