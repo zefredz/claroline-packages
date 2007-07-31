9141de93d8429404386b6fee5cf86e0b2124c798
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-07-31 14:23:31 +0000

    Remove ready function as it will be duplicate in frames if placed directly in library; update docommit function

--- modules/CLLP/js/CLLP.js
+++ modules/CLLP/js/CLLP.js
@@ -1,4 +1,4 @@
-$(document).ready(init);
+//$(document).ready(init);
 
 function init()
 {
@@ -9,25 +9,29 @@ function init()
     	$('#breadcrumbLine').hide();
     	$('#campusFooter').hide;
 	},"top.lp_content" );
-	
+
 	$.frameReady(refreshToc, "top.lp_toc");
 	*/
-	
-	setTimeout("refreshToc()", 1000);
+
+	//setTimeout("refreshToc()", 1000);
 }
 
 /**
- * Send all values to server to store them 
+ * Send all values to server to store them
  *
  */
 function doCommit()
 {
+	var scormdata = $.toJSON(elementList);
+
     $.ajax({
-        url: moduleUrl + "viewer/scormServer.php?cmd=doCommit&cidReq=" + cidReq, 
+    	type: "POST",
+        url: moduleUrl + "viewer/scormServer.php",
+        data: "cmd=doCommit&cidReq="+ cidReq + "&itemId=" + itemId + "&scormdata=" + scormdata,
         success: function(response){
-            refreshViewer();
-            }, 
-        dataType: 'html'}); 
+            refreshToc();
+            },
+        dataType: 'html'});
 
     return false;
 }
@@ -47,23 +51,23 @@ function refreshViewer()
 function refreshToc()
 {
     $("#table_of_content", top.frames["lp_toc"].document).empty();
-    
+
     getToc(pathId);
 }
 
 function refreshNav()
 {
-    
+
 }
 
 function refreshContent()
 {
-    
+
 }
 
 function refreshApi()
 {
-    
+
 }
 
 /**
@@ -71,33 +75,41 @@ function refreshApi()
  *
  */
 
-function getContentUrlById(pathId, itemId)
-{
-    var contentUrl = "";
-    
-    $.ajax({
-    url: moduleUrl + "viewer/scormServer.php?cmd=rqContentUrl&pathId=" + pathId + " &itemId=" + itemId + "&cidReq=" + cidReq, 
-    success: function(response){
-        contentUrl = response;
-        }, 
-    dataType: 'html'});
-         
-    return contentUrl;
-}
-
-
 function getToc(pathId)
 {
     var tocHtml = "";
     // return all the toc that will fill the div in the toc frame
     $.ajax({
-    url: moduleUrl + "viewer/scormServer.php?cmd=rqToc&pathId=" + pathId + "&cidReq=" + cidReq, 
+    url: moduleUrl + "viewer/scormServer.php?cmd=rqToc&pathId=" + pathId + "&cidReq=" + cidReq,
     success: function(response){
         $("#table_of_content", top.frames["lp_toc"].document).append(response);
-        }, 
+        },
     dataType: 'html'});
-         
-    return tocHtml;
+
+    return true;
+}
+
+function rqOpenItem(itemId)
+{
+	// get url and set frame location to this url
+    $.ajax({
+    url: moduleUrl + "viewer/scormServer.php?cmd=rqContentUrl&pathId=" + pathId + "&itemId=" + itemId + "&cidReq=" + cidReq,
+    success: mkOpenItem,
+    dataType: 'html'});
+}
+
+function mkOpenItem(response)
+{
+	if( response != '' )
+	{
+		parent.lp_content.location = response;
+	}
+	else
+	{
+		parent.lp_content.location = 'blank.htm';
+	}
+	//refreshApi();
+	//refreshContent();
 }
 
 /**
@@ -120,9 +132,9 @@ function isolateContent()
 	$('#campusFooter').hide();
 }
 
-function array_indexOf(arr,val) 
+function array_indexOf(arr,val)
 {
-    for ( var i = 0; i < arr.length; i++ ) 
+    for ( var i = 0; i < arr.length; i++ )
     {
         if ( arr[i] == val )
         {
@@ -132,13 +144,37 @@ function array_indexOf(arr,val)
     return -1;
 }
 
-function isDefined(a) 
-{ 
+function isDefined(a)
+{
 	return typeof a != 'undefined';
 }
 
-function isNull(a) 
+function isNull(a)
 {
     return typeof a == 'object' && !a;
 }
 
+function dump(arr,level) {
+	var dumped_text = "";
+	if(!level) level = 0;
+
+	//The padding given at the beginning of the line.
+	var level_padding = "";
+	for(var j=0;j<level+1;j++) level_padding += "    ";
+
+	if(typeof(arr) == 'object') { //Array/Hashes/Objects
+		for(var item in arr) {
+			var value = arr[item];
+
+			if(typeof(value) == 'object') { //If it is an array,
+				dumped_text += level_padding + "'" + item + "' ...\n";
+				dumped_text += dump(value,level+1);
+			} else {
+				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+			}
+		}
+	} else { //Stings/Chars/Numbers etc.
+		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+	}
+	return dumped_text;
+}
