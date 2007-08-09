26b9653b795fa87b8f1cd361f338b3129711f6f6
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-08-09 12:17:35 +0000

    Use an js object to store all method and to share them more easily between frames

--- trunk/modules/CLLP/js/CLLP.js
+++ trunk/modules/CLLP/js/CLLP.js
@@ -1,115 +1,128 @@
 //$(document).ready(init);
 
-function init()
+
+function lpClient(pathId, cidReq, moduleUrl, debugMode)
 {
-    /*
-    // use frameReady to access the inner frames
-    $.frameReady(function(){
-		$('#topBanner').hide();
-    	$('#breadcrumbLine').hide();
-    	$('#campusFooter').hide;
-	},"top.lp_content" );
+	this.pathId = pathId;
+	this.cidReq = cidReq;
+	this.moduleUrl = moduleUrl;
+	this.debugMode = debugMode;
 
-	$.frameReady(refreshToc, "top.lp_toc");
-	*/
+	// refreshed by api data
+	this.itemId;
+	this.APIInitialized = false;
+	this.APILastError = "301";
+	this.elementList = {};
 
-	//setTimeout("refreshToc()", 1000);
-}
+	/**
+	 * Commit
+ 	 * Send all values to server to store them
+ 	 *
+ 	 */
+	this.commit = function commit() {
+		var scormdata = $.toJSON(this.elementList);
 
-/**
- * Send all values to server to store them
- *
- */
-function doCommit()
-{
-	var scormdata = $.toJSON(elementList);
+	    $.ajax({
+	    	type: "POST",
+	        url: this.moduleUrl + "viewer/scormServer.php",
+	        data: "cmd=doCommit&cidReq="+ this.cidReq + "&itemId=" + this.itemId + "&scormdata=" + scormdata,
+	        success: this.refreshToc(),
+	        dataType: 'html'
+	    });
 
-    $.ajax({
-    	type: "POST",
-        url: moduleUrl + "viewer/scormServer.php",
-        data: "cmd=doCommit&cidReq="+ cidReq + "&itemId=" + itemId + "&scormdata=" + scormdata,
-        success: function(response){
-            refreshToc();
-            },
-        dataType: 'html'});
-
-    return false;
-}
-
-/**
- * Ask a refresh of viewer (toc, nav, api, content)
- *
- */
-function refreshViewer()
-{
-    refreshToc();
-    refreshNav();
-    refreshApi();
-    refreshContent();
-}
-
-function refreshToc()
-{
-    $("#table_of_content", top.frames["lp_toc"].document).empty();
-
-    getToc(pathId);
-}
-
-function refreshNav()
-{
-
-}
-
-function refreshContent()
-{
-
-}
-
-function refreshApi()
-{
-
-}
-
-/**
- * Ajax methods
- *
- */
-
-function getToc(pathId)
-{
-    var tocHtml = "";
-    // return all the toc that will fill the div in the toc frame
-    $.ajax({
-    url: moduleUrl + "viewer/scormServer.php?cmd=rqToc&pathId=" + pathId + "&cidReq=" + cidReq,
-    success: function(response){
-        $("#table_of_content", top.frames["lp_toc"].document).append(response);
-        },
-    dataType: 'html'});
-
-    return true;
-}
-
-function rqOpenItem(itemId)
-{
-	// get url and set frame location to this url
-    $.ajax({
-    url: moduleUrl + "viewer/scormServer.php?cmd=rqContentUrl&pathId=" + pathId + "&itemId=" + itemId + "&cidReq=" + cidReq,
-    success: mkOpenItem,
-    dataType: 'html'});
-}
-
-function mkOpenItem(response)
-{
-	if( response != '' )
-	{
-		parent.lp_content.location = response;
+	    return false;
 	}
-	else
-	{
-		parent.lp_content.location = 'blank.htm';
+
+	/**
+	 * Set Content
+ 	 * Reinitialize API then open content in content frame
+ 	 *
+ 	 */
+	this.setContent = function setContent(itemId) {
+		// set item id
+		this.itemId = itemId;
+		// refresh api then refresh content
+		$.getScript("apiData.php?cidReq="+ this.cidReq + "&pathId=" + this.pathId + "&itemId=" + itemId, this.rqOpenItem() );
 	}
-	//refreshApi();
-	//refreshContent();
+
+	/**
+	 * Refresh the table of content
+	 * TODO : does it depends on user id ?
+	 */
+	this.refreshToc = function refreshToc() {
+
+	    // return all the toc that will fill the div in the toc frame
+	    $.ajax({
+	    	url: this.moduleUrl + "viewer/scormServer.php?cmd=rqToc&cidReq=" + this.cidReq + "&pathId=" + this.pathId,
+	    	success: function(response){
+		    	$("#table_of_content", top.frames["lp_toc"].document).empty();
+		        $("#table_of_content", top.frames["lp_toc"].document).append(response);
+		        },
+	    	dataType: 'html'
+	    });
+
+	    return true;
+	}
+
+	/**
+	 * Change the content of content frame according to itemId received
+	 * Should be call as callback for the load of the api
+	 *
+	 */
+	this.rqOpenItem = function rqOpenItem() {
+
+		// get url and set frame location to this url
+	    $.ajax({
+	    	url: this.moduleUrl + "viewer/scormServer.php?cmd=rqContentUrl&cidReq=" + this.cidReq + "&pathId=" + this.pathId + "&itemId=" + this.itemId,
+	    	success: this.mkOpenItem,
+	    	dataType: 'html'
+	    });
+	}
+
+	/**
+	 * Change the content of content frame according to url received, also refresh the API
+	 * Should be called by rqOpenItem
+	 *
+	 */
+	this.mkOpenItem = function mkOpenItem(itemUrl) {
+
+		if( itemUrl != '' )
+		{
+			parent.lp_content.location = itemUrl;
+		}
+		else
+		{
+			parent.lp_content.location = 'blank.htm';
+		}
+
+		this.isolateContent();
+	}
+
+	/**
+	 * Append a debug msg to navigation frame
+	 * TODO use a debug frame ?
+	 *
+	 */
+	this.debug = function debug(msg, level) {
+
+	    if( this.debugMode > level )
+	    {
+	        $("#lp_debug", top.frames["lp_nav"].document).append(msg + "<br />\n\n");
+	    }
+	}
+
+	/**
+	 * Remove headers, footers,... from a claroline content inserted as a module
+	 *
+	 */
+	this.isolateContent = function isolateContent() {
+
+		$("#topBanner", top.frames["lp_content"].document).hide();
+		$("#userBanner", top.frames["lp_content"].document).hide();
+		$("#courseBanner", top.frames["lp_content"].document).hide();
+		$("#breadcrumbLine", top.frames["lp_content"].document).hide();
+		$("#campusFooter", top.frames["lp_content"].document).hide();
+	}
 }
 
 /**
@@ -117,20 +130,6 @@ function mkOpenItem(response)
  *
  */
 
-function debug(msg, level)
-{
-    if( debug_mode > level )
-    {
-        $("#lp_debug", top.frames["lp_nav"].document).append(msg + "<br />\n\n");
-    }
-}
-
-function isolateContent()
-{
-	$('#topBanner').hide();
-	$('#breadcrumbLine').hide();
-	$('#campusFooter').hide();
-}
 
 function array_indexOf(arr,val)
 {
