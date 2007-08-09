25afd5f88a377207955488ff5d6a619935e3c282
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-08-09 12:22:51 +0000

    remove deprecated getViewerHtmlHeaders function

--- trunk/modules/CLLP/lib/CLLP.lib.php
+++ trunk/modules/CLLP/lib/CLLP.lib.php
@@ -15,25 +15,6 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
  *
  */
 
-function getViewerHtmlHeaders($pathId)
-{
-	$headers = "\n"
-	.     '<script type="text/javascript">' . "\n"
-	.    '  var pathId = "'.(int) $pathId.'";' . "\n"
-	.    '  var moduleUrl = "'.get_module_url('CLLP').'/";' . "\n"
-	.    '  var cidReq = "'.claro_get_current_course_id().'";' . "\n"
-	.    '  var debug_mode = '.get_conf('scorm_api_debug').';' . "\n"
-	.    '  var jQueryPath = "'.get_module_url('CLLP').'/js/jquery.js";' . "\n"
-	.    '</script>' . "\n\n"
-	.    '<script type="text/javascript" src="'.get_module_url('CLLP').'/js/jquery.js"></script>' . "\n"
-	.    '<script type="text/javascript" src="'.get_module_url('CLLP').'/js/jquery.frameready.js"></script>' . "\n"
-	.    '<script type="text/javascript" src="'.get_module_url('CLLP').'/js/json.jquery.js"></script>' . "\n"
-	.    '<script type="text/javascript" src="'.get_module_url('CLLP').'/js/CLLP.js"></script>' . "\n"
-	.    '<script type="text/javascript" src="'.get_module_url('CLLP').'/viewer/scormAPI.php?pathId='.$pathId.'"></script>' . "\n\n";
-
-	return $headers;
-}
-
 /**
  * this function should be used in tool to generate API calls required to handle
  * progression when tool is used as a learning path item using javascript
