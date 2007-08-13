73190c92d7e49f577cd03c87b1248347cacdaed2
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-08-13 14:39:19 +0000

    Change switch full screen / embedded method

--- modules/CLLP/viewer/index.php
+++ modules/CLLP/viewer/index.php
@@ -50,8 +50,6 @@ require_once get_path('clarolineRepositorySys') . '/linker/resolver.lib.php';
 if( isset($_REQUEST['pathId']) && is_numeric($_REQUEST['pathId']) )   $pathId = (int) $_REQUEST['pathId'];
 else                                                                  $pathId = null;
 
-if( isset($_REQUEST['view']) )   $rqFullscreen = ($_REQUEST['view'] == 'fullscreen')? true: false;
-else                             $rqFullscreen = false;
 
 /*
  * init other vars
@@ -77,8 +75,6 @@ else
 
 claro_set_display_mode_available(false);
 
-$fullScreen = $path->isFullScreen() || ( isset($rqFullscreen) && $rqFullscreen );
-
 $thisAttempt = new attempt();
 
 if( !$thisAttempt->load($pathId, claro_get_current_user_id()) )
@@ -95,30 +91,6 @@ $_SESSION['thisAttempt'] = serialize($thisAttempt);
  * Output
  */
 
-// prepare frames and framesets
-$menuFrameset = new ClaroFrameset();
-
-// menu frames
-$menuFrameToc = new ClaroFrame('lp_toc', 'toc.php?pathId='.$pathId);
-$menuFrameToc->allowScrolling(true);
-$menuFrameToc->noFrameBorder();
-$menuFrameNav = new ClaroFrame('lp_nav', 'navigation.php?pathId='.$pathId);
-$menuFrameNav->allowScrolling(true);
-$menuFrameNav->noFrameBorder();
-
-$menuFrameset->addRow($menuFrameToc, '*');
-$menuFrameset->addRow($menuFrameNav, '100');
-
-
-// content frame
-$contentFrame = new ClaroFrame('lp_content', 'blank.htm');
-$contentFrame->allowScrolling(true);
-$contentFrame->noFrameBorder();
-
-// prepare inner frameset that contains
-$innerFrameset = new ClaroFrameSet();
-$innerFrameset->addCol($menuFrameset, '200');
-$innerFrameset->addCol($contentFrame, '*');
 
 // prepare html header
 $htmlHeaders = "\n"
@@ -137,36 +109,48 @@ $htmlHeaders .= "\n"
 .	 '  var cidReq = "'.claro_get_current_course_id().'";' . "\n"
 .	 '  var moduleUrl = "'.get_module_url('CLLP').'/";' . "\n"
 .    '  var debugMode = '.get_conf('scorm_api_debug').';' . "\n\n"
-.	 '  var lpClient = new lpClient(pathId,cidReq,moduleUrl,debugMode);' . "\n"
+.	 '  var lpHandler = new lpHandler(pathId,cidReq,moduleUrl,debugMode);' . "\n"
 .	 '  $(document).ready(function() {' . "\n"
-.    '    setTimeout("lpClient.refreshToc()", 1000);' . "\n"
+.    '    setTimeout("lpHandler.refreshToc()", 900);' . "\n"
+.    '    setTimeout("lpHandler.setContent(' .$thisAttempt->getLastItemId().')", 1000);' . "\n"
 .	 '  });' . "\n"
 .    '</script>' . "\n\n";
 
-if( !$fullScreen )
-{
-    // create outer frameset
-    $outerFrameset = new ClaroFrameset();
 
-    // header frame
-    $headerFrame = new ClaroFrame('lp_header', 'header.php?pathId='.$pathId);
-    $headerFrame->noFrameBorder();
+// prepare frames and framesets
 
-    // add header frame and inner frameset
-    $outerFrameset->addRow($headerFrame, '150');
-    $outerFrameset->addRow($innerFrameset, '*');
+// menu frames
+$tocFrame = new ClaroFrame('lp_toc', 'toc.php?pathId='.$pathId);
+$tocFrame->allowScrolling(true);
+$tocFrame->noFrameBorder();
 
-    $outerFrameset->addHtmlHeader($htmlHeaders);
-    // output outer frameset with inner frameset within in embedded mode
-    $outerFrameset->output();
-}
-else
-{
-    $innerFrameset->addHtmlHeader($htmlHeaders);
-    // output inner frameset only in fullscreen mode
-    $innerFrameset->output();
-}
 
+// content frame
+$contentFrame = new ClaroFrame('lp_content', 'blank.htm');
+$contentFrame->allowScrolling(true);
+$contentFrame->noFrameBorder();
+
+// inner frameset that contains toc and content frames
+$innerFrameset = new ClaroFrameSet();
+$innerFrameset->addCol($tocFrame, '200');
+$innerFrameset->addCol($contentFrame, '*');
+
+// outer frameset that contains header frame and inner frameset
+$outerFrameset = new ClaroFrameset();
+
+// header frame
+$headerFrame = new ClaroFrame('lp_header', 'header.php?pathId='.$pathId);
+$headerFrame->noFrameBorder();
+
+// add header frame and inner frameset
+$headerFrameSize = $path->isFullScreen()?'0':'150';
+
+$outerFrameset->addRow($headerFrame, $headerFrameSize);
+$outerFrameset->addRow($innerFrameset, '*');
+
+$outerFrameset->addHtmlHeader($htmlHeaders);
+// output outer frameset with inner frameset within in embedded mode
+$outerFrameset->output();
 
 
 ?>
