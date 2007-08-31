6f1742a034f0c9d5c893702522790979b85f302a
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-08-31 12:55:56 +0000

    Video webconference module, work in progress...

--- modules/DIMDIM/setup/course_install.sql
+++ modules/DIMDIM/setup/course_install.sql
@@ -2,12 +2,14 @@ CREATE TABLE IF NOT EXISTS `__CL_COURSE__dim_conference` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL default '',
   `description` TEXT NOT NULL,
+  `visibility` ENUM('VISIBLE','INVISIBLE') NOT NULL DEFAULT 'VISIBLE',  
   `waitingArea` ENUM('ENABLE','DISABLE') NOT NULL DEFAULT 'DISABLE',
   `maxUsers` INT(11) NOT NULL default '20',
   `duration` INT(11) NOT NULL default '1',
   `type` enum('AUDIO','AUDIOVIDEO') NOT NULL default 'AUDIO',
-  `attendees` INT(11) NOT NULL default '0',
+  `attendeeMikes` INT(11) NOT NULL default '0',
   `network` ENUM('DIALUP', 'CABLEDSL', 'LAN') NOT NULL DEFAULT 'DIALUP',
   `startTime` DATETIME NOT NULL default now(),
+  `confKey` VARCHAR(255) NOT NULL default '',
   PRIMARY KEY(`id`)
 ) TYPE = MyISAM;
