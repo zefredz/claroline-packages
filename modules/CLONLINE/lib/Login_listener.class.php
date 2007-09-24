f39fcc833b18b1886b877f26b23546afc9b76359
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-09-24 09:35:21 +0000

    Define class  in a separate file (was in functions.php previously ...)

---
+++ modules/CLONLINE/lib/Login_listener.class.php
@@ -0,0 +1,86 @@
+<?php // $Id$
+/**
+ *
+ * @version 0.1 $Revision$
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @author Claroline team <info@claroline.net>
+ *
+ * @package CLONLINE
+ *
+ */
+
+//declare  the online user listener for this module
+
+class Login_listener extends EventDriven
+{
+    var $tblUserOnline;
+
+    /**
+     * constructor
+     */
+    function Login_listener ( &$registry )
+    {
+        parent::EventDriven( $registry );
+
+        $tbl_names = claro_sql_get_tbl('user_online', array('course'=>null));
+		$this->tblUserOnline = $tbl_names['user_online'];
+    }
+
+    /**
+     * @param object $event
+     */
+    function insert_login_online($event)
+    {
+        $event_arguments = $event->getArgs();
+
+        // delete any potential entry in the database with the same user,
+        // to avoid double entries
+
+        $sql = "DELETE FROM `" . $this->tblUserOnline . "`
+                      WHERE `user_id`=" . (int)$event_arguments['uid'];
+
+        claro_sql_query($sql);
+
+        $sql = "INSERT INTO `" . $this->tblUserOnline . "`
+                SET `user_id` = " . (int) $event_arguments['uid'] . ",
+                    `last_action` = '" . date('Y-m-d H:i:s') . "'";
+
+        claro_sql_query($sql);
+
+        $this->refresh_login_DB();
+    }
+
+    function delete_login_online($event)
+    {
+        $event_arguments = $event->getArgs();
+
+        //delete any entry in the database with the same user
+
+        $sql = "DELETE FROM `" . $this->tblUserOnline . "`
+                      WHERE `user_id` = " . (int) $event_arguments['uid'];
+
+        claro_sql_query($sql);
+
+        $this->refresh_login_DB();
+
+    }
+
+    /**
+     * flush old connection
+     */
+    function refresh_login_DB()
+    {
+        // Refresh time should not be less than 10 minutes
+        $refreshTime = get_conf('clonline_refreshTime',5);
+
+        $sql = "DELETE
+                FROM `" . $this->tblUserOnline . "`
+                WHERE `last_action` < DATE_SUB( NOW() , INTERVAL " . $refreshTime . " MINUTE )";
+
+        claro_sql_query($sql);
+    }
+}
+
+?>
