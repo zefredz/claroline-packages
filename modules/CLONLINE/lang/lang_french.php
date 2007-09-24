dcec7fb45737e40321fa592a2d97fb1255c15aec
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-09-24 07:34:17 +0000

    Add french translation

---
+++ modules/CLONLINE/lang/lang_french.php
@@ -0,0 +1,21 @@
+<?php // $Id$
+/**
+ * CLSURVEY
+ *
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLONLINE
+ * @subpackage LANG-FR
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ */
+$_lang['User(s) online'] = 'Utilisateurs connectŽs';
+$_lang['List of active users for the last %time minutes :'] = 'Liste des utilisateurs actifs depuis au moins %time minutes :';
+$_lang['1 user connected'] = '1 utilisateur en ligne';
+$_lang['%countOfUsers users connected'] = '%countOfUsers utilisateurs en ligne';
+$_lang['No user connected'] = 'Aucun utilisateur connectŽ';
+?>
