ef126758847314bc545b95240487056affc5d717
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-09-28 07:08:12 +0000

    Add french localization; Remove "not allowed" var as it exists already in complete

--- modules/CLSKYPE/lang/lang_dutch.php
+++ modules/CLSKYPE/lang/lang_dutch.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLSKYPE
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ */
 $_lang['Skype status notifier successfully deactivated.'] = 'Skype statusvermelding met succes gedesactiveerd';
 $_lang['Leave empty to deactivate status notifier.'] = 'Laat leeg om de statusvermelding te deactiveren.';
 $_lang['Do not forget to allow your status to be shown from your Skype client.'] = 'Vergeet niet in je Skype cliënt aan te geven dat je status mag worden getoond.';
 $_lang['Skype status notifier'] = 'Skype statusvermelding';
 $_lang['Cannot save change.'] = 'Kan wijziging niet vastleggen.';
 $_lang['Skype name successfully changed to %skypeName.'] = 'Skype naam met succes gewijzigd in %skypeName.';
-$_lang['Not allowed'] = 'Niet toegelaten'; 
 ?>
