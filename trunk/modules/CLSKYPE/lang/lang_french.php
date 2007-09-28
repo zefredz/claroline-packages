6ec665fbf6c01ffd8ee7de1fda37ce5e5b1ab03e
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-09-28 07:08:12 +0000

    Add french localization; Remove "not allowed" var as it exists already in complete

---
+++ trunk/modules/CLSKYPE/lang/lang_french.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLSKYPE
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ */
+$_lang['Skype status notifier successfully deactivated.'] = 'La notification de statut Skype a été désactivée.';
+$_lang['Leave empty to deactivate status notifier.'] = 'Laissez ce champs vide pour désactiver la notification.';
+$_lang['Do not forget to allow your status to be shown from your Skype client.'] = 'N\'oubliez pas d\'autoriser l\'affichage de votre statut dans votre logiciel Skype.';
+$_lang['Skype status notifier'] = 'Notification de statut Skype';
+$_lang['Cannot save change.'] = 'Impossible d\'enregistrer les modifications.';
+$_lang['Skype name successfully changed to %skypeName.'] = 'Le nom d\'utilisateur Skype a été changé en %skypeName.';
+?>
