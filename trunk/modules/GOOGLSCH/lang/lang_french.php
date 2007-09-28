26ae4a6a1b0e63a202fecf32edfa69bf258ca0ce
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-09-28 07:11:47 +0000

    Add french locale

---
+++ trunk/modules/GOOGLSCH/lang/lang_french.php
@@ -0,0 +1,16 @@
+<?php // $Id$
+/**
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package GOOGLSCH
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ */
+$_lang['On the web'] = 'Sur internet';
+$_lang['Only on this campus'] = 'Sur le campus uniquement';
+$_lang['On %siteName'] = 'Sur %siteName';
+?>
