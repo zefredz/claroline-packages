a2736232a4d68dba3fbda458d443ae0442ef5b60
moosh <moosh@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-10-07 10:02:41 +0000

    CLSURVEY : translation of CLSURVEY By OLAF Nohring

---
+++ modules/CLSURVEY/lang/lang_german.php
@@ -0,0 +1,53 @@
+<?php // $Id$
+/**
+ * CLSURVEY
+ *
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLSURVEY
+ * @subpackage LANG-DE
+ *
+ * @author Christophe Gesché <moosh@claroline.net>
+ * @author Philippe Dekimpe <dkp@ecam.be>
+ * @author Claro Team <cvs@claroline.net>
+ * @author Olaf Nöhring <olaf2007@team-noehring.de>
+ *
+ */
+
+$_lang['Survey'] = "Umfrage";
+$_lang['Surveys'] = "Umfragen";
+$_lang['List of surveys'] = "Umfragenübersicht";
+$_lang['Multiple choice - horizontal answer'] = "Multiple Choice, Antworten horizontal";
+$_lang['Multiple choice - vertical answer'] = "Multiple Choice, Antworten vertikal";
+$_lang['Textbox'] = "Text";
+$_lang['Question has been updated'] = "Frage wurde aktualisiert";
+$_lang['Question has been inserted'] = "Frage wurde eingefügt";
+$_lang['Survey has been updated'] = "Umfrage wurde aktualisiert";
+$_lang['Survey has been inserted'] = "Umfrage wurde eingefügt";
+$_lang['New survey'] = "Neue Umfrage";
+$_lang['Edit this survey'] = "Umfrage bearbeiten";
+$_lang['Date of creation'] = "Erstellungsdatum";
+$_lang['Survey has been deleted'] = "Umfrage wurde entfernt";
+$_lang['Add survey'] = "Umfrage hinzufügen";
+$_lang['No survey'] = "Keine Umfrage";
+$_lang['Results'] = "Ergebnisse";
+$_lang['Question has been deleted'] = "Frage wurde entfernt";
+$_lang['Answers have been saved'] = "Antwort wurde gespeichert";
+$_lang['No question'] = "Keine Frage";
+$_lang['No result available'] = "Keine Ergebnisse vorhanden";
+$_lang['Results have been deleted'] = "Ergebnisse wurden gelöscht";
+$_lang['Delete all results'] = "Alle Ergebnisse löschen";
+$_lang['Export results'] = "Ergebnisse exportieren";
+$_lang['Number of answer/Total'] = "Fragenummer/Gesamt ";
+$_lang['Percentage'] = "Prozent";
+$_lang['Add question'] = "Eine Frage hinzufügen";
+$_lang['This survey is anonymous.  We only save your answer not your identification.'] = "Diese Umfrage ist anonym. Es wird nur die Antwort gespeichert, eine Zurückverfolgung zu Ihnen ist nicht möglich";
+$_lang['This survey is already filled by you. Your answer are not saved again. As The survey are anonymous, we cannot show you your answers']="Sie haben schon an dieser Umfrage teilgenommen. Da diese Umfrage anonym ist, können Ihre bisherigen Antworten nicht angezeigt werden.";
+$_lang['Position modified'] = "Eintrag geändert";
+$_lang['No answer']="Keine Antwort";
+$_lang['View this survey']="Umfrage ansehen";
+$_lang['Vote for this survey']="Abstimmen";
+?>
