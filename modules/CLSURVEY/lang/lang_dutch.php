e8310d336b749ceaa24759ff3d1f01bbf94f5dfd
moosh <moosh@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-10-07 12:55:32 +0000

    First medium translation (to be validated)

---
+++ modules/CLSURVEY/lang/lang_dutch.php
@@ -0,0 +1,52 @@
+<?php // $Id$
+/**
+ * CLSURVEY
+ *
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLSURVEY
+ * @subpackage LANG-NL
+ *
+ * @author Christophe Gesché <moosh@claroline.net>
+ * @author Philippe Dekimpe <dkp@ecam.be>
+ * @author Claro Team <cvs@claroline.net>
+ *
+ */
+
+$_lang['Survey'] = "Opiniepeiling";
+$_lang['Surveys'] = "Opiniepeilingen";
+$_lang['List of surveys'] = "Lijst van opiniepeilingen";
+//$_lang['Multiple choice - horizontal answer'] = "Choix multiple - réponses alignées horizontalement";
+//$_lang['Multiple choice - vertical answer'] = "Choix multiple - réponses alignées verticalement";
+$_lang['Textbox'] = "Text";
+$_lang['Question has been updated'] = "De vraag is bijwerkt";
+$_lang['Question has been inserted'] = "De vraag is invougt";
+$_lang['Survey has been updated'] = "De opiniepeiling is bijwerkt";
+$_lang['Survey has been inserted'] = "De opiniepeiling is invougt";
+$_lang['New survey'] = "Nieuwe opiniepeiling";
+$_lang['Edit this survey'] = "Verander de opiniepeiling";
+$_lang['Date of creation'] = "Datum of creatie";
+$_lang['Survey has been deleted'] = "De opiniepeilin is uitwis";
+$_lang['Add survey'] = "Invoegen un opiniepeiling";
+$_lang['No survey'] = "Geen opiniepeilingen";
+$_lang['Results'] = "Resultaaten";
+$_lang['Question has been deleted'] = "De vraag is uitwis";
+$_lang['Answers have been saved'] = "De antwoorden zijn geregistreerd";
+$_lang['No question'] = "Geen vraag";
+$_lang['No result available'] = "Geen resultaat bestaande";
+$_lang['Results have been deleted'] = "De rsultaten zijn uitwiss";
+$_lang['Delete all results'] = "Uitwissen alle resultaaten";
+$_lang['Export results'] = "Exporteren de resultaaten";
+//$_lang['Number of answer/Total'] = "Nombre de réponses sur un total de ";
+//$_lang['Percentage'] = "Percentage";
+//$_lang['Add question'] = "Ajouter une question";
+//$_lang['This survey is anonymous.  We only save your answer not your identification.'] = "Ce sondage est anonyme. Nous enregistrons uniquement vos réponses, pas votre identification";
+//$_lang['This survey is already filled by you. Your answer are not saved again. As The survey are anonymous, we cannot show you your answers']="Vous avez déjà répondu à ce sondage.  Vos réponses ne seront donc pas sauvées.  Comme ce sondage est anonyme, nous ne pouvons vous montrer vos réponses.";
+$_lang['Position modified'] = "Positie geverandeerdt";
+$_lang['No answer']="Geen antwoordt";
+$_lang['View this survey']="Zie deze opiniepeiling";
+$_lang['Vote for this survey']="Voteren";
+?>
