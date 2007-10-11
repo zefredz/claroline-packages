84201a05a5f39d1b73c1484a185038baffee34f9
seb <seb@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-10-11 13:31:47 +0000

    add id to textarea

--- modules/CLPAGES/lib/plugins/text.class.php
+++ modules/CLPAGES/lib/plugins/text.class.php
@@ -33,8 +33,7 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
 		 */
     	function editor()
     	{
-    		return '<textarea name="content_'.$this->getId().'" rows="20" cols="80" style="width: 100%;">'.htmlspecialchars($this->render()).'</textarea>';
-    		//return claro_html_textarea_editor('content_'. $this->getId(), $this->render(), 20, 80 );
+    		return '<textarea name="content_'.$this->getId().'" id="content_'.$this->getId().'" rows="20" cols="80" style="width: 100%;">'.htmlspecialchars($this->render()).'</textarea>';
     	}
 
 		/**
