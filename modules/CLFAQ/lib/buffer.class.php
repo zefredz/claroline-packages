03f231290c4912e20f7f677550082d4b8ebb7a47
moosh <moosh@d46d66fa-363b-41f8-8237-52ce30e9732d>
2007-11-05 05:33:07 +0000

    - CR, indent, phpdoc, 2007

--- modules/CLFAQ/lib/buffer.class.php
+++ modules/CLFAQ/lib/buffer.class.php
@@ -1,58 +1,56 @@
 <?php // $Id$
+if (count ( get_included_files () ) == 1)
+{
+    die ( 'The file ' . __FILE__ . ' cannot be accessed directly, use include instead' ) ;
+}
 
-    // vim: expandtab sw=4 ts=4 sts=4:
+/**
+ * Output Buffering Class
+ *
+ * @version     1.9 $Revision$
+ * @copyright   2001-2007 Universite catholique de Louvain (UCL)
+ * @author      Frederic Minne <zefredz@claroline.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ *              GNU GENERAL PUBLIC LICENSE
+ * @package     Output
+ */
+
+class Output_Buffer
+{
+    var $contents = '' ;
     
-    if ( count( get_included_files() ) == 1 )
+    function send()
     {
-        die( 'The file ' . __FILE__ . ' cannot be accessed directly, use include instead' );
+        echo $this->contents ;
     }
     
-    /**
-     * Output Buffering Class
-     *
-     * @version     1.9 $Revision$
-     * @copyright   2001-2007 Universite catholique de Louvain (UCL)
-     * @author      Frederic Minne <zefredz@claroline.net>
-     * @license     http://www.gnu.org/copyleft/gpl.html
-     *              GNU GENERAL PUBLIC LICENSE
-     * @package     Output
-     */
-
-    class Output_Buffer
+    function clean()
     {
-        var $contents = '';
-        
-        function send()
-        {
-            echo $this->contents;
-        }
-        
-        function clean()
-        {
-            $this->contents = '';
-        }
-        
-        function flush()
-        {
-            $this->send();
-            // force I/O flush
-            flush();
-            $this->clean();
-        }
-        
-        function append( $str )
-        {
-            $this->contents .= $str;
-        }
-        
-        function replace( $str )
-        {
-            $this->contents = $str;
-        }
-        
-        function getContents()
-        {
-            return $this->contents;
-        }
+        $this->contents = '' ;
     }
+    
+    function flush()
+    {
+        $this->send () ;
+        // force I/O flush
+        flush () ;
+        $this->clean () ;
+    }
+    
+    function append( $str )
+    {
+        $this->contents .= $str ;
+    }
+    
+    function replace( $str )
+    {
+        $this->contents = $str ;
+    }
+    
+    function getContents()
+    {
+        return $this->contents ;
+    }
+}
+// vim: expandtab sw=4 ts=4 sts=4:
 ?>
