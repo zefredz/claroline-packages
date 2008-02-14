e406dc981b0fd48a4aff899f371c9a7a2caca5f9
fragile_be <fragile_be@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-02-14 10:17:58 +0000

    Interface and libs for SCORM adapters

---
+++ trunk/modules/CLLP/lib/scorm13.lib.php
@@ -0,0 +1,61 @@
+<?php
+
+
+class Scorm13 implements ScormInterface
+{
+	private $apiName;
+	private $apiFileName;
+	private $version;
+
+	private $mapping;
+
+	function __construct()
+	{
+		$this->apiName = 'API_1484_11';
+		$this->apiFileName = 'scorm13.api';
+		$this->version = '1.3';
+
+		$this->mapping = '';
+	}
+
+	public function getApiName()
+	{
+		return $this->apiName;
+	}
+
+	public function getApiFileName()
+	{
+		return $this->apiName;
+	}
+
+	public function getVersion()
+	{
+		return $this->version;
+	}
+
+	/**
+	 * @see Scorm class
+	 */
+	public function api2ItemAttempt( $dataModelValues, &$itemAttempt)
+	{
+
+		$itemAttempt->setLocation($dataModelValues['cmi.location']);
+		$itemAttempt->setCompletionStatus($dataModelValues['cmi.completion_status']);
+		$itemAttempt->setEntry($dataModelValues['cmi.entry']);
+		$itemAttempt->setScoreRaw($dataModelValues['cmi.score.raw']);
+		$itemAttempt->setScoreMin($dataModelValues['cmi.score.min']);
+		$itemAttempt->setScoreMax($dataModelValues['cmi.score.max']);
+		$itemAttempt->setSessionTime($dataModelValues['cmi.session_time']);
+		$itemAttempt->setTotalTime($dataModelValues['cmi.total_time']);
+		$itemAttempt->setSuspendData($dataModelValues['cmi.suspend_data']);
+		$itemAttempt->setCredit($dataModelValues['cmi.credit']);
+
+		return true;
+	}
+
+	public function itemAttempt2Api($dataModel)
+	{
+
+	}
+}
+?>
