258f9f8a9094c4d95835794b27ba7809f90cf7fe
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-04-02 10:45:50 +0000

    Use new version of file finder lib

--- trunk/modules/ICPRINT/lib/filefinder.lib.php
+++ trunk/modules/ICPRINT/lib/filefinder.lib.php
@@ -1,96 +1,60 @@
 <?php // $Id$
 
-    // vim: expandtab sw=4 ts=4 sts=4:
-    
-    /**
-     * File finders
-     * @package     kernel
-     */
+// vim: expandtab sw=4 ts=4 sts=4:
 
-    if ( count( get_included_files() ) == 1 )
+/**
+* File finders
+* @package     kernel
+*/
+
+class FileFinder extends FilterIterator
+{
+    protected $searchString;
+    
+    public function __construct( $path, $searchString, $recursive = true )
     {
-        die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+        $this->searchString = $searchString;
+        
+        if ( ! $recursive )
+        {
+            parent::__construct( 
+                new IteratorIterator( 
+                    new DirectoryIterator($path) ) ); 
+        }
+        else
+        {
+             parent::__construct(
+                new RecursiveIteratorIterator(
+                    new RecursiveDirectoryIterator($path)));
+        }
     }
     
-// From PEAR PHP_Compat 1.5.0
-if (!defined('PATH_SEPARATOR'))
-{
-    define('PATH_SEPARATOR',
-        strtoupper(substr(PHP_OS, 0, 3) == 'WIN') ? ';' : ':'
-    );
-}
-    
-// From SPL examples by (c) Marcus Boerger, 2003 - 2007 
-if ( ! class_exists( 'FindFile', false ) )
-{
-    class FindFile extends FilterIterator
+    public function getSearchString()
     {
-        protected $searchString;
-        
-        public function __construct( $path, $searchString )
-        {
-            $this->searchString = $searchString;
-            
-            $list = split(PATH_SEPARATOR, $path);
-            
-            if (count($list) <= 1)
-            {
-                parent::__construct(
-                    new RecursiveIteratorIterator(
-                        new RecursiveDirectoryIterator($path)));
-            } 
-            else
-            {
-                $it = new AppendIterator();
-                
-                foreach ( $list as $path )
-                {
-                    $it->append(new RecursiveIteratorIterator(
-                        new RecursiveDirectoryIterator($path)));
-                }
-                
-                parent::__construct($it);
-            }   
-        }
-        
-        public function getSearchString()
-        {
-            return $this->searchString;
-        }
-        
-        public function accept()
-        {
-            return !strcmp($this->getSearchString(), $this->current() );
-        }
+        return $this->searchString;
+    }
+    
+    public function accept()
+    {
+        return !strcmp($this->getSearchString(), $this->current() );
     }
 }
 
 // From SPL examples by (c) Marcus Boerger, 2003 - 2007
-if ( ! class_exists( 'FindFile', false ) )
-{   
-    class RegexFileFind extends FindFile
+
+class RegexpFileFinder extends FileFinder
+{
+    public function accept()
     {
-        public function accept()
-        {
-            return preg_match( $this->current(), $this->getSearchString() );
-        }
+        return preg_match( $this->current(), $this->getSearchString() );
     }
 }
-    
-    class ExtensionFindFile extends FindFile
+
+class ExtensionFileFinder extends FileFinder
+{
+    public function accept()
     {
-        public function accept()
-        {
-            return ( substr( $this->current(), - ( strlen($this->getSearchString()) ) )
-                == $this->getSearchString() );
-        }
+        return ( substr( $this->current(), - ( strlen($this->getSearchString()) ) )
+            == $this->getSearchString() );
     }
-    
-    class PdfFindFile extends ExtensionFindFile
-    {
-        public function __construct( $path )
-        {
-            parent::__construct( $path, '.pdf' );
-        }
-    }
-?>
+}
