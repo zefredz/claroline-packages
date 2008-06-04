16572412803d5f4f02c3f4f6e2fdc4f33c1136bb
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-06-04 12:05:52 +0000

    refactoring + test svnmail

--- trunk/modules/ICPRINT/service.php
+++ trunk/modules/ICPRINT/service.php
@@ -1,26 +1,40 @@
 <?php // $Id$
 
-    // vim: expandtab sw=4 ts=4 sts=4:
-    
-    /**
-     * Description
-     *
-     * @version     1.8-backport $Revision$
-     * @copyright   2001-2008 Universite catholique de Louvain (UCL)
-     * @author      Claroline Team <info@claroline.net>
-     * @author      Frederic Minne <zefredz@claroline.net>
-     * @license     http://www.gnu.org/copyleft/gpl.html
-     *              GNU GENERAL PUBLIC LICENSE version 2 or later
-     * @package     PACKAGE_NAME
-     */
-    
+// vim: expandtab sw=4 ts=4 sts=4:
+
+/**
+ * Description
+ *
+ * @version     1.8-backport $Revision$
+ * @copyright   2001-2008 Universite catholique de Louvain (UCL)
+ * @author      Claroline Team <info@claroline.net>
+ * @author      Frederic Minne <zefredz@claroline.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ *              GNU GENERAL PUBLIC LICENSE version 2 or later
+ * @package     PACKAGE_NAME
+ */
+try
+{    
     // load Claroline kernel
     require dirname(__FILE__) . '/../../claroline/inc/claro_init_global.inc.php';
+    
+    require_once get_path('includePath') . '/lib/user.lib.php';
+    require_once get_path('includePath') . '/lib/file.lib.php';
+    
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdocrudclaro.lib.php';
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdocrud.lib.php';
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdofactory.lib.php';
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdosqlscript.lib.php';
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdomapper.lib.php';
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdomapperbuilder.lib.php';
+    require_once dirname(__FILE__) . '/lib/pdocrud/pdomapperschema.lib.php';
+    
+    require_once dirname(__FILE__) . '/lib/time.lib.php';
+    require_once dirname(__FILE__) . '/lib/phptemplate.lib.php';
     
     require_once dirname(__FILE__) . '/lib/request/userinput.lib.php';
     require_once dirname(__FILE__) . '/lib/request/inputfilters.lib.php';
     
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdofactory.lib.php';
     require_once dirname(__FILE__) . '/keyring/keyring.lib.php';
     
     $userInput = FilteredUserInput::getInstance();
@@ -55,45 +69,33 @@
         exit();
     }
     
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdocrudclaro.lib.php';
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdocrud.lib.php';
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdofactory.lib.php';
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdosqlscript.lib.php';
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdomapper.lib.php';
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdomapperbuilder.lib.php';
-    require_once dirname(__FILE__) . '/lib/pdocrud/pdomapperschema.lib.php';
-    
-    require_once dirname(__FILE__) . '/lib/time.lib.php';
-    require_once dirname(__FILE__) . '/lib/phptemplate.lib.php';
-    
-    require_once get_path('includePath') . '/lib/user.lib.php';
-    require_once get_path('includePath') . '/lib/file.lib.php';
-    
     define ( 'APP_PATH', dirname(__FILE__).'/crud' );
     !defined( 'CLARO_DSN' ) && define ( 'CLARO_DSN', 'mysql://'.get_conf('dbLogin')
         .':'.get_conf('dbPass').'@'.get_conf('dbHost').'/'
         .get_conf('mainDbName') );
  
-try
-{
     date_default_timezone_set('Europe/Brussels');
     
     // Init database :
     PDOCrud::init( CLARO_DSN );
+    $mapperBuilder = PDOCrud::getBuilder();
+    
     // Init data objects :
     require_once APP_PATH . '/classes/document.class.php';
     require_once APP_PATH . '/classes/action.class.php';
-    $mapperBuilder = PDOCrud::getBuilder();
+    
+    // Init schemas
     $mapperBuilder->register( PDOMapperSchema::fromFile(APP_PATH.'/schemas/document.xml'));
     $mapperBuilder->register( PDOMapperSchema::fromFile(APP_PATH.'/schemas/action.xml'));
+    
+    // Get mappers
     $documentMapper = $mapperBuilder->getMapper('PrintServiceDocument');
     $actionMapper = $mapperBuilder->getMapper('PrintServiceAction');
     
-    $allowedCommandList = array( 'list', 'get' );
-        
+    // Process user commands    
     $userInput->setFilter( 
         'cmd', 
-        array( new AllowedValueListFilter( $allowedCommandList ), 'isValid' ) 
+        array( new AllowedValueListFilter( array( 'list', 'get' ) ), 'isValid' ) 
     );
         
     $cmd = $userInput->get( 'cmd', 'list' );
