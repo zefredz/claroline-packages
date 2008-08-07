11f13944bfe6d92e0ba0202ab7e0913bd547222f
fragile_be <fragile_be@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-08-07 09:39:38 +0000

    Add empty tracking display files for CLLP

---
+++ trunk/modules/CLLP/track_path.php
@@ -0,0 +1,19 @@
+<?php // $Id$
+/**
+ * CLAROLINE
+ *
+ * @version 1.8 $Revision$
+ *
+ * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLLP
+ *
+ * @author Claro Team <cvs@claroline.net>
+ * @author Sebastien Piraux <pir@cerdecam.be>
+ */
+
+
+
+?>
