16e1a0a1f2dbd8aeb8d40b13ae0532fd4d37c9aa
bsokay <bsokay@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-09-25 14:20:59 +0000

    Key management administration module

---
+++ trunk/modules/CLKRNG/lang/lang_english.php
@@ -0,0 +1,25 @@
+<?php
+$_lang['Are you sure to delete'] ='Are you sure to delete';
+$_lang[' deleted'] =' deleted';
+$_lang['Delete service '] ='Delete service "';
+$_lang['don\'t exist !'] = 'don\'exist !';
+$_lang['Invalid action'] ='Invalid action';
+$_lang['Host address']= 'Host address';
+$_lang['Manage key ring'] ='Manage key ring';
+$_lang['Missing new service host'] = 'Missing new service host'; 
+$_lang['Missing new service key'] = 'Missing new service key'; 
+$_lang['Missing new service name'] = 'Missing new service name';  
+$_lang['Missing old service name or host !'] ='Missing old service name or host !';
+$_lang['Missing service name or host !'] = 'Missing service name or host !';
+$_lang['Missing service name, key or host !']= 'Missing service name, key or host !';
+$_lang['No service registered'] ='No service registered';
+$_lang['Registered services'] ='Registered services';
+$_lang['Register a service'] ='Register a service';
+$_lang['Service ']='Service ';
+$_lang['Service host'] ='Service host';
+$_lang['Service key'] ='Service key';
+$_lang['Service key administration'] ='Service key administration';
+$_lang['Service key added for service '] ='Service key added for service ';
+$_lang['Service key changed for service '] ='Service key changed for service ';
+$_lang['Service name'] ='Service name';
+?>
---
+++ trunk/modules/CLKRNG/lang/lang_french.php
@@ -0,0 +1,25 @@
+<?php
+$_lang['Are you sure to delete'] ='Êtes vous certain de vouloir effacer';
+$_lang[' deleted'] =' a été effacé';
+$_lang['Delete service '] ='Effacer le service "';
+$_lang['don\'t exist !'] = 'n\'existe pas !';
+$_lang['Invalid action'] ='Action invalide';
+$_lang['Host address'] ='Adresse de l\'hôte';
+$_lang['Manage key ring'] ='Gestion du trousseau de clés';
+$_lang['Missing new service host'] = 'Hôte du nouveau service manquant'; 
+$_lang['Missing new service key'] = 'Clé du nouveau service manquante'; 
+$_lang['Missing new service name'] = 'Nom du nouveau service manquant'; 
+$_lang['Missing old service name or host !'] = 'Ancien nom ou hôte de service manquant !';
+$_lang['Missing service name or host !'] = 'Nom ou hôte du service manquant !';
+$_lang['Missing service name, key or host !']= 'Nom, clé ou hôte de service manquant !';
+$_lang['No service registered'] ='Aucun service enregistré';
+$_lang['Registered services'] ='Services enregistrés';
+$_lang['Register a service'] ='Enregistrer un service';
+$_lang['Service ']='Le service ';
+$_lang['Service host'] ='Hôte de service';
+$_lang['Service key'] ='Clé de service';
+$_lang['Service key administration'] ='Administration des clés de service';
+$_lang['Service key added for service '] ='La clé de service a été ajoutée au service ';
+$_lang['Service key changed for service '] ='La clé de service a été modifiée pour le service ';
+$_lang['Service name'] ='Nom de service';
+?>
