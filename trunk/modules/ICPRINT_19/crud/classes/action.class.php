710db5ab938a1e7c1bc68506246181dcb6f1ac90
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-09-30 12:46:38 +0000

    replace \t with spaces + Zend coding conventions

--- trunk/modules/ICPRINT_19/crud/classes/action.class.php
+++ trunk/modules/ICPRINT_19/crud/classes/action.class.php
@@ -1,82 +1,81 @@
 <?php // $Id$
 
-    // vim: expandtab sw=4 ts=4 sts=4:
-    
-  /**
-     * Description
-     *
-     * @version     1.9 $Revision$
-     * @copyright   2001-2008 Universite catholique de Louvain (UCL)
-     * @author      Claroline Team <info@claroline.net>
-     * @author      Frederic Minne <zefredz@claroline.net>
-     * @license     http://www.gnu.org/copyleft/gpl.html
-     *              GNU GENERAL PUBLIC LICENSE version 2 or later
-     * @package     PACKAGE_NAME
-     */
+// vim: expandtab sw=4 ts=4 sts=4:
 
-    if ( count( get_included_files() ) == 1 )
+/**
+ * Description
+ *
+ * @version     1.9 $Revision$
+ * @copyright   2001-2008 Universite catholique de Louvain (UCL)
+ * @author      Claroline Team <info@claroline.net>
+ * @author      Frederic Minne <zefredz@claroline.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ *              GNU GENERAL PUBLIC LICENSE version 2 or later
+ * @package     PACKAGE_NAME
+ */
+
+if ( count( get_included_files() ) == 1 )
+{
+    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+}
+
+FromKernel::uses('utils/time.lib');
+
+
+class PrintServiceAction
+{    
+    public $id;
+    public $documentId;
+    public $courseId;
+    public $userId;
+    public $action;
+    public $timestamp;
+    
+    public static function actionAdd( $doc )
     {
-        die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+        $action = new PrintServiceAction;
+        $action->action = 'add';
+        $action->userId = claro_get_current_user_id();
+        $action->courseId = claro_get_current_course_id();
+        $action->timestamp = Claro_Utils_Time::timeToDatetime();
+        $action->documentId = $doc->id;
+        $action->documentHash = $doc->hash;
+        $action->documentLocalPath = $doc->localPath;
+        
+        $action->document = $doc;
+        
+        return $action;
     }
-	
-		FromKernel::uses('utils/time.lib');
-
     
-    class PrintServiceAction
-    {	
-        public $id;
-        public $documentId;
-        public $courseId;
-        public $userId;
-        public $action;
-        public $timestamp;
+    public static function actionDelete( $doc )
+    {
+        $action = new PrintServiceAction;
+        $action->action = 'delete';
+        $action->userId = claro_get_current_user_id();
+        $action->courseId = claro_get_current_course_id();
+        $action->timestamp = Claro_Utils_Time::timeToDatetime();
+        $action->documentId = $doc->id;
+        $action->documentHash = $doc->hash;
+        $action->documentLocalPath = $doc->localPath;
         
-        public static function actionAdd( $doc )
-        {
-            $action = new PrintServiceAction;
-            $action->action = 'add';
-            $action->userId = claro_get_current_user_id();
-            $action->courseId = claro_get_current_course_id();
-            $action->timestamp = Claro_Utils_Time::timeToDatetime();
-            $action->documentId = $doc->id;
-            $action->documentHash = $doc->hash;
-            $action->documentLocalPath = $doc->localPath;
-            
-            $action->document = $doc;
-            
-            return $action;
-        }
+        $action->document = $doc;
         
-        public static function actionDelete( $doc )
-        {
-            $action = new PrintServiceAction;
-            $action->action = 'delete';
-            $action->userId = claro_get_current_user_id();
-            $action->courseId = claro_get_current_course_id();
-            $action->timestamp = Claro_Utils_Time::timeToDatetime();
-            $action->documentId = $doc->id;
-            $action->documentHash = $doc->hash;
-            $action->documentLocalPath = $doc->localPath;
-            
-            $action->document = $doc;
-            
-            return $action;
-        }
-        
-        public static function actionModify( $doc )
-        {
-            $action = new PrintServiceAction;
-            $action->action = 'modify';
-            $action->userId = claro_get_current_user_id();
-            $action->courseId = claro_get_current_course_id();
-            $action->timestamp = Claro_Utils_Time::timeToDatetime();
-            $action->documentId = $doc->id;
-            $action->documentHash = $doc->hash;
-            $action->documentLocalPath = $doc->localPath;
-            
-            $action->document = $doc;
-            
-            return $action;
-        }
+        return $action;
     }
-?>
+    
+    public static function actionModify( $doc )
+    {
+        $action = new PrintServiceAction;
+        $action->action = 'modify';
+        $action->userId = claro_get_current_user_id();
+        $action->courseId = claro_get_current_course_id();
+        $action->timestamp = Claro_Utils_Time::timeToDatetime();
+        $action->documentId = $doc->id;
+        $action->documentHash = $doc->hash;
+        $action->documentLocalPath = $doc->localPath;
+        
+        $action->document = $doc;
+        
+        return $action;
+    }
+}
--- trunk/modules/ICPRINT_19/crud/classes/document.class.php
+++ trunk/modules/ICPRINT_19/crud/classes/document.class.php
@@ -1,63 +1,62 @@
 <?php // $Id$
 
-    // vim: expandtab sw=4 ts=4 sts=4:
-    
-    /**
-     * Description
-     *
-     * @version     1.9 $Revision$
-     * @copyright   2001-2008 Universite catholique de Louvain (UCL)
-     * @author      Claroline Team <info@claroline.net>
-     * @author      Frederic Minne <zefredz@claroline.net>
-     * @license     http://www.gnu.org/copyleft/gpl.html
-     *              GNU GENERAL PUBLIC LICENSE version 2 or later
-     * @package     PACKAGE_NAME
-     */
+// vim: expandtab sw=4 ts=4 sts=4:
 
-    if ( count( get_included_files() ) == 1 )
-    {
-        die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
-    }
+/**
+ * Description
+ *
+ * @version     1.9 $Revision$
+ * @copyright   2001-2008 Universite catholique de Louvain (UCL)
+ * @author      Claroline Team <info@claroline.net>
+ * @author      Frederic Minne <zefredz@claroline.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ *              GNU GENERAL PUBLIC LICENSE version 2 or later
+ * @package     PACKAGE_NAME
+ */
+
+if ( count( get_included_files() ) == 1 )
+{
+    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+}
+
+class PrintServiceDocument
+{
+    public $id;
+    public $title;
+    public $localPath;
+    public $globalPath;
+    public $length;
+    public $hash;
+    public $publisher;
+    public $courseId;
+    public $action;
     
-    class PrintServiceDocument
+    public static function fromLocalPath( $localPath )
     {
-        public $id;
-        public $title;
-        public $localPath;
-        public $globalPath;
-        public $length;
-        public $hash;
-        public $publisher;
-        public $courseId;
-        public $action;
+        $courseDir = realpath( 
+              get_path('coursesRepositorySys') 
+            . claro_get_course_path()
+            .'/document' );
         
-        public static function fromLocalPath( $localPath )
+        $globalPath = $courseDir . $localPath;
+        
+        if ( file_exists( $globalPath ) )
         {
-            $courseDir = realpath( 
-                  get_path('coursesRepositorySys') 
-                . claro_get_course_path()
-                .'/document' );
+            $doc = new PrintServiceDocument;
             
-            $globalPath = $courseDir . $localPath;
+            $doc->title = basename( $localPath );
+            $doc->localPath = $localPath;
+            $doc->globalPath = $globalPath;
+            $doc->length = filesize( $globalPath );
+            $doc->hash = md5_file( $globalPath );
+            $doc->publisher = claro_get_current_user_id();
+            $doc->courseId = claro_get_current_course_id();
             
-            if ( file_exists( $globalPath ) )
-            {
-                $doc = new PrintServiceDocument;
-                
-                $doc->title = basename( $localPath );
-                $doc->localPath = $localPath;
-                $doc->globalPath = $globalPath;
-                $doc->length = filesize( $globalPath );
-                $doc->hash = md5_file( $globalPath );
-                $doc->publisher = claro_get_current_user_id();
-                $doc->courseId = claro_get_current_course_id();
-                
-                return $doc;
-            }
-            else
-            {
-                throw new Exception( 'File not found : ' . htmlspecialchars($localPath) );
-            }
+            return $doc;
+        }
+        else
+        {
+            throw new Exception( 'File not found : ' . htmlspecialchars($localPath) );
         }
     }
-?>
+}
