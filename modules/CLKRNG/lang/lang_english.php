da6742ae0995d628b98589e64240b53f4f27ce8a
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2008-09-30 13:53:17 +0000

    refactor language variables

--- modules/CLKRNG/lang/lang_english.php
+++ modules/CLKRNG/lang/lang_english.php
@@ -1,26 +1,24 @@
 <?php
 $_lang['Are you sure to delete'] ='Are you sure to delete';
-$_lang[' deleted'] =' deleted';
-$_lang['Delete service '] ='Delete service "';
-$_lang['don\'t exist !'] = 'don\'exist !';
+$_lang['Service %service deleted'] = 'Service %service deleted';
+$_lang['Delete service %service ?'] ='Delete service %service ?';
+$_lang['Cannot delete service %service'] = 'Cannot delete service %service';
 $_lang['Invalid action'] ='Invalid action';
-$_lang['Host address']= 'Host address';
 $_lang['Manage key ring'] ='Manage key ring';
 $_lang['Missing new service host'] = 'Missing new service host'; 
 $_lang['Missing new service key'] = 'Missing new service key'; 
 $_lang['Missing new service name'] = 'Missing new service name';  
-$_lang['Missing old service name or host !'] ='Missing old service name or host !';
-$_lang['Missing service name or host !'] = 'Missing service name or host !';
-$_lang['Missing service name, key or host !']= 'Missing service name, key or host !';
+$_lang['Missing old service name or host'] ='Missing old service name or host';
+$_lang['Missing service name or host'] = 'Missing service name or host';
+$_lang['Missing service name, key or host']= 'Missing service name, key or host';
 $_lang['No service registered'] ='No service registered';
 $_lang['Registered services'] ='Registered services';
 $_lang['Register a service'] ='Register a service';
-$_lang['Service ']='Service ';
+$_lang['Service name']='Service name';
 $_lang['Service host'] ='Service host';
 $_lang['Service key'] ='Service key';
-$_lang['Service key already exist for service '] ='Une clé de service existe déjà pour le service ';
 $_lang['Service key administration'] ='Service key administration';
-$_lang['Service key added for service '] ='Service key added for service ';
-$_lang['Service key changed for service '] ='Service key changed for service ';
-$_lang['Service name'] ='Service name';
+$_lang['Service key added for service %service'] ='Service key added for service %service';
+$_lang['Service key already exist for service %service'] ='Service key already exist for service %service';
+$_lang['Service key changed for service %service'] ='Service key changed for service %service';
 ?>
