41d14574acffc9566fd6a0f5fb48e9cf0e4ea7fd
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-10-07 11:56:42 +0000

    indent, zend conventions, PHP5 conventions, Claroline conventions

--- trunk/modules/CLPAGES/lib/plugins/file.class.php
+++ trunk/modules/CLPAGES/lib/plugins/file.class.php
@@ -1,76 +1,73 @@
 <?php // $Id$
-if ( count( get_included_files() ) == 1 ) die( '---' );
+
+if ( count( get_included_files() ) == 1 ) die( basename(__FILE__) );
+
 /**
  * CLAROLINE
  *
  * $Revision$
- *
- * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
- *
+ * @copyright (c) 2001-2008 Universite catholique de Louvain (UCL)
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- *
  * @package CLPAGES
- *
  * @author Claroline team <info@claroline.net>
- *
  */
-    // vim: expandtab sw=4 ts=4 sts=4 foldmethod=marker:
 
-    class FileComponent extends Component
+// vim: expandtab sw=4 ts=4 sts=4:
+
+class FileComponent extends Component
+{
+    private $url = '';
+
+    /**
+     * @see Component
+     */
+    public function render()
     {
-    	private $url = '';
+        if( !empty($this->url) )
+        {
+            include_once get_path('incRepositorySys') . '/lib/htmlxtra.lib.php';
 
-		/**
-		 * @see Component
-		 */
-    	function render()
-    	{
-    		if( !empty($this->url) )
-    		{
-	    		include_once get_path('incRepositorySys') . '/lib/htmlxtra.lib.php';
-
-	    		return claro_html_media_player($this->url,$this->url);
-    		}
-    		else
-    		{
-    			return '' . "\n";
-    		}
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function editor()
-    	{
-    		// use content in textarea
-    		return '<label for="url_'.$this->getId().'">' . get_lang('Url of a file') . '</label>&nbsp;<span class="required">*</span><br />' . "\n"
-    		.	 '<input type="text" name="url_'.$this->getId().'" id="url_'.$this->getId().'" maxlength="255" size="60" value="'.htmlspecialchars($this->url).'" /><br />' . "\n";
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function getEditorData()
-    	{
-    		$this->url = $this->getFromRequest('url_'.$this->getId());
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function setData( $data )
-    	{
-  			$this->url = $data['url'];
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function getData()
-    	{
-    		return array('url' => $this->url);
-    	}
+            return claro_html_media_player($this->url,$this->url);
+        }
+        else
+        {
+            return '' . "\n";
+        }
     }
 
-    PluginRegistry::register('file',get_lang('File'),'FileComponent', '', 'fileIco');
-?>
+    /**
+     * @see Component
+     */
+    public function editor()
+    {
+        // use content in textarea
+        return '<label for="url_'.$this->getId().'">' . get_lang('Url of a file') . '</label>&nbsp;<span class="required">*</span><br />' . "\n"
+        .     '<input type="text" name="url_'.$this->getId().'" id="url_'.$this->getId().'" maxlength="255" size="60" value="'.htmlspecialchars($this->url).'" /><br />' . "\n";
+    }
+
+    /**
+     * @see Component
+     */
+    public function getEditorData()
+    {
+        $this->url = $this->getFromRequest('url_'.$this->getId());
+    }
+
+    /**
+     * @see Component
+     */
+    public function setData( $data )
+    {
+          $this->url = $data['url'];
+    }
+
+    /**
+     * @see Component
+     */
+    public function getData()
+    {
+        return array('url' => $this->url);
+    }
+}
+
+PluginRegistry::register('file',get_lang('File'),'FileComponent', '', 'fileIco');
