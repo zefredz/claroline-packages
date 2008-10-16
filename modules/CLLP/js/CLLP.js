497e30f177189da2f7ca541dfaaf796d64014207
fragile_be <fragile_be@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-10-16 08:28:57 +0000

    Pass datamodel to commit function; tab2spaces

--- modules/CLLP/js/CLLP.js
+++ modules/CLLP/js/CLLP.js
@@ -11,69 +11,69 @@ function lpHandler(pathId, cidReq, moduleUrl, debugMode)
 	/**
 	 * Commit
  	 * Send all values to server to store them
- 	 *
- 	 */
-	this.commit = commit;
+      *
+      */
+    this.commit = commit;
 
-	/**
-	 * Set Content
- 	 * Reinitialize API then open content in content frame
- 	 *
- 	 */
-	this.setContent = setContent;
+    /**
+     * Set Content
+      * Reinitialize API then open content in content frame
+      *
+      */
+    this.setContent = setContent;
 
-	/**
-	 * Refresh the table of content
-	 * TODO : does it depends on user id ?
-	 */
-	this.refreshToc = refreshToc;
+    /**
+     * Refresh the table of content
+     * TODO : does it depends on user id ?
+     */
+    this.refreshToc = refreshToc;
 
-	/**
-	 * Change the content of content frame according to itemId received
-	 * Should be call as callback for the load of the api
-	 *
-	 */
-	this.rqOpenItem = rqOpenItem;
+    /**
+     * Change the content of content frame according to itemId received
+     * Should be call as callback for the load of the api
+     *
+     */
+    this.rqOpenItem = rqOpenItem;
 
-	/**
-	 * Change the content of content frame according to url received
-	 * Should be called by rqOpenItem
-	 *
-	 */
-	this.mkOpenItem = mkOpenItem;
+    /**
+     * Change the content of content frame according to url received
+     * Should be called by rqOpenItem
+     *
+     */
+    this.mkOpenItem = mkOpenItem;
 
-	// Presentation function :
-	
-	/**
-	 * jump to the previous module if any
-	 *
-	 */
-	this.goPrevious = goPrevious;
-	
-	/**
+    // Presentation function :
+    
+    /**
+     * jump to the previous module if any
+     *
+     */
+    this.goPrevious = goPrevious;
+    
+    /**
      * jump to the next module if any
      *
      */
     this.goNext = goNext;
     
-	/**
-	 * Resize frameset to have a fullscreen
-	 *
-	 */
-	this.setFullscreen = setFullscreen;
+    /**
+     * Resize frameset to have a fullscreen
+     *
+     */
+    this.setFullscreen = setFullscreen;
 
-	/**
-	 * Resize frameset to have a embedded mode
-	 *
-	 */
-	this.setEmbedded = setEmbedded;
+    /**
+     * Resize frameset to have a embedded mode
+     *
+     */
+    this.setEmbedded = setEmbedded;
 
-	/**
-	 * Append a debug msg to navigation frame
-	 * TODO use a debug frame ?
-	 *
-	 */
-	this.debug = debug;
+    /**
+     * Append a debug msg to navigation frame
+     * TODO use a debug frame ?
+     *
+     */
+    this.debug = debug;
 }
 
 
@@ -88,7 +88,7 @@ function lpHandler(pathId, cidReq, moduleUrl, debugMode)
  *
  */
 function commit(datamodel) {
-	debug("Commit",1);
+    debug("Commit",1);
 
     var datamodelValues = {};
     // avoid sending complete datamodel so get only key and values
@@ -106,10 +106,10 @@ function commit(datamodel) {
         }
     }
 
-	var jsonDatamodelValues = $.toJSON(datamodelValues);
+    var jsonDatamodelValues = $.toJSON(datamodelValues);
 
     $.ajax({
-    	type: "POST",
+        type: "POST",
         url: lpHandler.moduleUrl + "viewer/scormServer.php",
         data: "cmd=doCommit&cidReq="+ lpHandler.cidReq + "&pathId=" + lpHandler.pathId + "&itemId=" + lpHandler.itemId + "&scormdata=" + jsonDatamodelValues,
         success: refreshToc,
@@ -126,11 +126,11 @@ function commit(datamodel) {
  *
  */
 function setContent(itemId) {
-	debug("setContent("+itemId+")",1);
-	// set item id
-	this.itemId = itemId;
-	// refresh api then refresh content
-	$.getScript("apiData.php?cidReq="+ lpHandler.cidReq + "&pathId=" + lpHandler.pathId + "&itemId=" + itemId, rqOpenItem );
+    debug("setContent("+itemId+")",1);
+    // set item id
+    this.itemId = itemId;
+    // refresh api then refresh content
+    $.getScript("apiData.php?cidReq="+ lpHandler.cidReq + "&pathId=" + lpHandler.pathId + "&itemId=" + itemId, rqOpenItem );
 }
 
 /**
@@ -139,13 +139,13 @@ function setContent(itemId) {
  *
  */
 function rqOpenItem() {
-	debug("rqOpenItem()",1);
+    debug("rqOpenItem()",1);
 
     // get url and set frame location to this url
     $.ajax({
-    	url: lpHandler.moduleUrl + "viewer/scormServer.php?cmd=rqContentUrl&cidReq=" + lpHandler.cidReq + "&pathId=" + lpHandler.pathId + "&itemId=" + lpHandler.itemId,
-    	success: mkOpenItem,
-    	dataType: 'html'
+        url: lpHandler.moduleUrl + "viewer/scormServer.php?cmd=rqContentUrl&cidReq=" + lpHandler.cidReq + "&pathId=" + lpHandler.pathId + "&itemId=" + lpHandler.itemId,
+        success: mkOpenItem,
+        dataType: 'html'
     });
 }
 
@@ -156,18 +156,18 @@ function rqOpenItem() {
  *
  */
 function mkOpenItem(itemUrl) {
-	debug("mkOpenItem()",1);
+    debug("mkOpenItem()",1);
 
-	if( itemUrl != '' )
-	{
-		lp_top.lp_content.location = itemUrl;
-	}
-	else
-	{
-		lp_top.lp_content.location = 'blank.htm';
-	}
+    if( itemUrl != '' )
+    {
+        lp_top.lp_content.location = itemUrl;
+    }
+    else
+    {
+        lp_top.lp_content.location = 'blank.htm';
+    }
 
-	makeItemActive(lpHandler.itemId);
+    makeItemActive(lpHandler.itemId);
 }
 
 /**
@@ -175,17 +175,17 @@ function mkOpenItem(itemUrl) {
  * TODO : does it depends on user id ?
  */
 function refreshToc() {
-	debug("refreshToc()",1);
+    debug("refreshToc()",1);
 
     // return all the toc that will fill the div in the toc frame
     $.ajax({
-    	url: lpHandler.moduleUrl + "viewer/scormServer.php?cmd=rqToc&cidReq=" + lpHandler.cidReq + "&pathId=" + lpHandler.pathId,
-    	success: function(response){
-	    	$("#table_of_content", lp_top.frames["lp_toc"].document).empty();
-	        $("#table_of_content", lp_top.frames["lp_toc"].document).append(response);
-	        makeItemActive(lpHandler.itemId);
-	        },
-    	dataType: 'html'
+        url: lpHandler.moduleUrl + "viewer/scormServer.php?cmd=rqToc&cidReq=" + lpHandler.cidReq + "&pathId=" + lpHandler.pathId,
+        success: function(response){
+            $("#table_of_content", lp_top.frames["lp_toc"].document).empty();
+            $("#table_of_content", lp_top.frames["lp_toc"].document).append(response);
+            makeItemActive(lpHandler.itemId);
+            },
+        dataType: 'html'
     });
 
     return true;
@@ -199,7 +199,9 @@ function refreshToc() {
  */
 function goPrevious() {
     debug("goPrevious()",1);
-
+    
+    /*
+    // todo ask server which is really the previous
     $.ajax({
         type: "POST",
         url: lpHandler.moduleUrl + "viewer/scormServer.php",
@@ -212,6 +214,7 @@ function goPrevious() {
         },
         dataType: 'html'
     });
+    */
 }
 
 /**
@@ -221,6 +224,28 @@ function goPrevious() {
 function goNext() {
     debug("goNext()",1);
     
+    if( $(".active", lp_top.frames["lp_toc"].document).size() == 1 )
+    {
+        var nextItemId = $(".active", lp_top.frames["lp_toc"].document).next().attr("id");
+    }
+    else
+    {
+        // take the first
+        var nextItemId = $(".item:first", lp_top.frames["lp_toc"].document).attr("id");
+    }
+    
+    if( isDefined(nextItemId) )
+    {
+        var id = nextItemId.substring( nextItemId.indexOf('_') + 1);
+        debug(id, 1);
+        lpHandler.setContent(id);
+    }
+    else
+    {
+        return false;
+    }
+    /*
+    // todo ask server which is really the next
     $.ajax({
         type: "POST",
         url: lpHandler.moduleUrl + "viewer/scormServer.php",
@@ -233,6 +258,7 @@ function goNext() {
         },
         dataType: 'html'
     });
+    */
 }
 
 /**
@@ -240,9 +266,9 @@ function goNext() {
  *
  */
 function setFullscreen() {
-	debug("setFullScreen()",1);
+    debug("setFullScreen()",1);
 
-	lp_top.document.body.rows = "0,*";
+    lp_top.document.body.rows = "0,*";
 }
 
 /**
@@ -250,9 +276,9 @@ function setFullscreen() {
  *
  */
 function setEmbedded() {
-	debug("setEmbedded()",1);
+    debug("setEmbedded()",1);
 
-	lp_top.document.body.rows = "150,*";
+    lp_top.document.body.rows = "150,*";
 }
 
 /**
@@ -265,11 +291,11 @@ function debug(msg, level) {
     {
         if( $.browser.msie || !top.window.console || !top.window.console.log )
         {
-        	$("#lp_debug", lp_top.frames["lp_toc"].document).append(msg + "<br />\n\n");
+            $("#lp_debug", lp_top.frames["lp_toc"].document).append(msg + "<br />\n\n");
         }
         else
         {
-        	top.window.console.log(msg);
+            top.window.console.log(msg);
         }
     }
 }
@@ -281,29 +307,29 @@ function debug(msg, level) {
  */
 
 /**
- *	Make active an item in the table of content
+ *    Make active an item in the table of content
  *
  */
 function makeItemActive( itemId )
 {
-	debug("makeItemActive("+itemId+")",1);
-	if( itemId > 0 )
-	{
-		// remove header and footer of content
-		//isolateContent();
+    debug("makeItemActive("+itemId+")",1);
+    if( itemId > 0 )
+    {
+        // remove header and footer of content
+        //isolateContent();
 
-		// remove all currently active class
-		$(".active", lp_top.frames["lp_toc"].document).removeClass("active");
+        // remove all currently active class
+        $(".active", lp_top.frames["lp_toc"].document).removeClass("active");
 
-		// find correct item and add it the active class
-		$("#item_" + lpHandler.itemId, lp_top.frames["lp_toc"].document).addClass("active");
+        // find correct item and add it the active class
+        $("#item_" + lpHandler.itemId, lp_top.frames["lp_toc"].document).addClass("active");
 
-		// blink the new active item
-		$(".active a", lp_top.frames["lp_toc"].document).fadeOut("fast").fadeIn("slow");
+        // blink the new active item
+        $(".active a", lp_top.frames["lp_toc"].document).fadeOut("fast").fadeIn("slow");
 
-	}
+    }
 
-	return true;
+    return true;
 }
 
 function array_indexOf(arr,val)
@@ -320,7 +346,7 @@ function array_indexOf(arr,val)
 
 function isDefined(a)
 {
-	return typeof a != 'undefined';
+    return typeof a != 'undefined';
 }
 
 function isNull(a)
@@ -333,26 +359,26 @@ function isInteger(s) {
 }
 
 function dump(arr,level) {
-	var dumped_text = "";
-	if(!level) level = 0;
+    var dumped_text = "";
+    if(!level) level = 0;
 
-	//The padding given at the beginning of the line.
-	var level_padding = "";
-	for(var j=0;j<level+1;j++) level_padding += "    ";
+    //The padding given at the beginning of the line.
+    var level_padding = "";
+    for(var j=0;j<level+1;j++) level_padding += "    ";
 
-	if(typeof(arr) == 'object') { //Array/Hashes/Objects
-		for(var item in arr) {
-			var value = arr[item];
+    if(typeof(arr) == 'object') { //Array/Hashes/Objects
+        for(var item in arr) {
+            var value = arr[item];
 
-			if(typeof(value) == 'object') { //If it is an array,
-				dumped_text += level_padding + "'" + item + "' ...\n";
-				dumped_text += dump(value,level+1);
-			} else {
-				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-			}
-		}
-	} else { //Stings/Chars/Numbers etc.
-		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
-	}
-	return dumped_text;
+            if(typeof(value) == 'object') { //If it is an array,
+                dumped_text += level_padding + "'" + item + "' ...\n";
+                dumped_text += dump(value,level+1);
+            } else {
+                dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+            }
+        }
+    } else { //Stings/Chars/Numbers etc.
+        dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+    }
+    return dumped_text;
 }
