aa557fb21459e848feb8044396423afee38924c0
fragile_be <fragile_be@d46d66fa-363b-41f8-8237-52ce30e9732d>
2008-10-16 09:59:05 +0000

    check that value is in data model before adding it in itemAttempt object.  tab2spaces

--- trunk/modules/CLLP/lib/scorm13.lib.php
+++ trunk/modules/CLLP/lib/scorm13.lib.php
@@ -3,59 +3,103 @@
 
 class Scorm13 implements ScormInterface
 {
-	private $apiName;
-	private $apiFileName;
-	private $version;
+    private $apiName;
+    private $apiFileName;
+    private $version;
 
-	private $mapping;
+    private $mapping;
 
-	function __construct()
-	{
-		$this->apiName = 'API_1484_11';
-		$this->apiFileName = 'scorm13.api';
-		$this->version = '1.3';
+    function __construct()
+    {
+        $this->apiName = 'API_1484_11';
+        $this->apiFileName = 'scorm13.api';
+        $this->version = '1.3';
 
-		$this->mapping = '';
-	}
+        $this->mapping = '';
+    }
 
-	public function getApiName()
-	{
-		return $this->apiName;
-	}
+    public function getApiName()
+    {
+        return $this->apiName;
+    }
 
-	public function getApiFileName()
-	{
-		return $this->apiFileName;
-	}
+    public function getApiFileName()
+    {
+        return $this->apiFileName;
+    }
 
-	public function getVersion()
-	{
-		return $this->version;
-	}
+    public function getVersion()
+    {
+        return $this->version;
+    }
 
-	/**
-	 * @see Scorm class
-	 */
-	public function api2ItemAttempt( $dataModelValues, &$itemAttempt)
-	{
-	    // FIXME find better mechanism to do that
-		$itemAttempt->setLocation($dataModelValues->cmi.location);
-		$itemAttempt->setCompletionStatus($dataModelValues->cmi.completion_status);
-		$itemAttempt->setEntry($dataModelValues->cmi.entry);
-		$itemAttempt->setScoreRaw($dataModelValues->cmi.score.raw);
-		$itemAttempt->setScoreMin($dataModelValues->cmi.score.min);
-		$itemAttempt->setScoreMax($dataModelValues->cmi.score.max);
-		$itemAttempt->setSessionTime($dataModelValues->cmi.session_time);
-		$itemAttempt->setTotalTime($dataModelValues->cmi.total_time);
-		$itemAttempt->setSuspendData($dataModelValues->cmi.suspend_data);
-		$itemAttempt->setCredit($dataModelValues->cmi.credit);
+    /**
+     * @see Scorm class
+     */
+    public function api2ItemAttempt( $dataModelValues, &$itemAttempt)
+    {
+        // TODO maybe raise some kind of error if nothing is set ?
+        // cast to array for easier use
+        $dataModelValues = (array) $dataModelValues;
 
-		return true;
-	}
+        if( !empty($dataModelValues['cmi.location']) )
+        {
+            $itemAttempt->setLocation($dataModelValues['cmi.location']);
+        }
 
-	public function itemAttempt2Api($dataModel)
-	{
+        if( !empty($dataModelValues['cmi.completion_status']) )
+        {
+            $itemAttempt->setCompletionStatus($dataModelValues['cmi.completion_status']);
+        }
+        
+        if( !empty($dataModelValues['cmi.entry']) )
+        {
+            $itemAttempt->setEntry($dataModelValues['cmi.entry']);
+        }
+        
+        if( !empty($dataModelValues['cmi.score.raw']) )
+        {
+            $itemAttempt->setScoreRaw($dataModelValues['cmi.score.raw']);
+        }
+        
+        if( !empty($dataModelValues['cmi.completion_status']) )
+        {
+            $itemAttempt->setScoreMin($dataModelValues['cmi.score.min']);
+        }
+        
+        if( !empty($dataModelValues['cmi.completion_status']) )
+        {
+            $itemAttempt->setScoreMax($dataModelValues['cmi.score.max']);
+        }
+        
+        if( !empty($dataModelValues['cmi.session_time']) )
+        {
+            $itemAttempt->setSessionTime($dataModelValues['cmi.session_time']);
+        }
+        
+        if( !empty($dataModelValues['cmi.total_time']) )
+        {
+            $itemAttempt->setTotalTime($dataModelValues['cmi.total_time']);
+        }
+        
+        if( !empty($dataModelValues['cmi.suspend_data']) )
+        {
+            $itemAttempt->setSuspendData($dataModelValues['cmi.suspend_data']);
+        }
+        
+        if( !empty($dataModelValues['cmi.credit']) )
+        {
+            $itemAttempt->setCredit($dataModelValues['cmi.credit']);
+        }
+        
+        // add other elements
 
-	}
+        return true;
+    }
+
+    public function itemAttempt2Api($dataModel)
+    {
+
+    }
 }
 ?>
