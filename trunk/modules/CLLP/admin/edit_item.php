ab1afb5b7a7c4bb5fc6f15ccedbb0c73759e2428
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-01-12 13:49:02 +0000

    Move Blocking conditions in Prerequisites instead of Item modification

--- trunk/modules/CLLP/admin/edit_item.php
+++ trunk/modules/CLLP/admin/edit_item.php
@@ -36,14 +36,13 @@ if ( !claro_is_tool_allowed() )
  */
 require_once dirname( __FILE__ ) . '/../lib/path.class.php';
 require_once dirname( __FILE__ ) . '/../lib/item.class.php';
-require_once dirname( __FILE__ ) . '/../lib/blockingcondition.class.php';
 
 require_once dirname( __FILE__ ) . '/../linker/linker.inc.php';
 
 /*
  * init request vars
  */
-$acceptedCmdList = array(   'blockcondAdd', 'blockcondDelete', 'exEdit'
+$acceptedCmdList = array(   'exEdit'
                     );
 
 if( isset($_REQUEST['cmd']) && in_array($_REQUEST['cmd'], $acceptedCmdList) )   $cmd = $_REQUEST['cmd'];
@@ -95,7 +94,6 @@ $dialogBox = new DialogBox();
 /*
  * Commands
  */
-$blockcond = new blockingcondition( $itemId );
 
 if( $cmd == 'exEdit' )
 {
@@ -125,33 +123,6 @@ if( $cmd == 'exEdit' )
     }
 }
 
-if( $cmd == 'blockcondAdd' )
-{
-    $blockcond->setBlockConds($_POST);    
-    
-    if( $blockcond->save() )
-    {
-       $dialogBox->success( get_lang('Blocking condition(s) successfully saved') ); 
-    }
-    else
-    {
-        $dialogBox->error( get_lang('Unable to save the blocking condition(s)') );
-    }
-}
-
-if( $cmd == 'blockcondDelete' )
-{
-    if( $blockcond->delete() )
-    {
-        $dialogBox->success( get_lang('Blocking conditions successfully deleted') );
-    }
-    else
-    {
-        $dialogBox->error( get_lang('Fatal error : cannot delete blocking conditions') );
-    }
-}
-
-
 /*
  * Output
  */
@@ -162,11 +133,6 @@ ClaroBreadCrumbs::getInstance()->prepend( get_lang('Learning path'), '../admin/e
 
 
 //-- Content
-$jsloader = JavascriptLoader::getInstance();
-$jsloader->load('jquery');
-$jsloader->load('jquery.json');
-
-$jsloader->load('CLLP');
 
 $out = '';
 
@@ -194,111 +160,8 @@ if( $item->load( $itemId ) )
     .   '<input type="submit" value="' . get_lang( 'Save' ) . '" />' . "\n"
     .   '</form> <br />' . "\n";
     
-    $out .= $htmlEditTitleForm;
-    
-    // load blocking conditions dependencies
-    if ( $item->getParentId() > 0 )
-    {
-        $blockcondsDependencies = array_reverse( $blockcond->loadRecursive( $item->getParentId(), true ) );
-        if( count($blockcondsDependencies) )
-        {            
-            $htmlBlockCondDep = '<div>' . "\n"
-            .   '<strong>'. get_lang('Blocking conditions dependencies') .'</strong> <br />' . "\n";
-            foreach( $blockcondsDependencies  as $dependency)
-            {
-               if( isset( $dependency['data'] ) )
-               {
-                    $blockconds = $dependency['data'];
-                    $htmlBlockCondDep .= '<div>' . "\n"
-                    .    '<strong><a href="'. $_SERVER['PHP_SELF'] . '?pathId='.$pathId.'&itemId='.$dependency['id']. '">'. htmlspecialchars($dependency['title']) .'</a></strong>';
-                    foreach( $blockconds['item'] as $key => $value)
-                    {
-                         $htmlBlockCondDep .= '<div>' . "\n";
-                         if( $key > 0 )
-                         {
-                             $htmlBlockCondDep .= '<select name="_condition[]" disabled="disabled">' . "\n"
-                             .   '<option value="AND" '.($blockconds['condition'][$key-1] == 'AND' ? 'selected="selected"' : '').'>'.get_lang('AND').'</option>' . "\n"
-                             .   '<option value="OR" '.($blockconds['condition'][$key-1] == 'OR' ? 'selected="selected"' : '').'>'.get_lang('OR').'</option>' . "\n"
-                             .   '</select>'
-                             .   '<br />' . "\n";
-                         }
-                         
-                         $htmlBlockCondDep .= '<select name="_item[]" disabled="disabled">' . "\n";
-                         foreach( $itemListArray as $anItem )
-                         {
-                             $htmlBlockCondDep .= '<option value="'. $anItem['id'] .'" style="padding-left:'.(5 + $anItem['deepness']*10).'px;" '.($value == $anItem['id'] && $anItem['type'] != 'CONTAINER' ? 'selected="selected"' : '').' '.($anItem['type'] == 'CONTAINER' ? 'disabled="disabled"' : '').'>'.$anItem['title'].'</option>' . "\n";
-                         }
-                         $htmlBlockCondDep .= '</select>'
-                         .   '<select name="_operator[]" disabled="disabled">' . "\n"
-                         .   '<option value="=" '.( $blockconds['operator'][$key] == '=' ? 'selected="selected"' : '').'>=</option>' . "\n"
-                         .   '</select>'
-                         .   '<select name="_status[]" disabled="disabled">' . "\n"
-                         .   '<option value="COMPLETED" '.( $blockconds['status'][$key] == 'COMPLETED' ? 'selected="selected"' : '' ).'>'.get_lang('completed').'</option>' . "\n"
-                         .   '<option value="INCOMPLETE" '.( $blockconds['status'][$key] == 'INCOMPLETE' ? 'selected="selected"' : '' ).'>'.get_lang('incomplete').'</option>' . "\n"
-                         .   '<option value="PASSED" '.( $blockconds['status'][$key] == 'PASSED' ? 'selected="selected"' : '' ).'>'.get_lang('passed').'</option>' . "\n"
-                         .   '</select>'
-                         .   '</div>' . "\n";
-                    }
-               }           
-            }
-            $htmlBlockCondDep .=   '</div> <br />' . "\n";
-            
-            $out .= $htmlBlockCondDep;
-        }
-    }    
+    $out .= $htmlEditTitleForm; 
 }
-
-
-
-
-$htmlConditionsForm = "\n\n"
-.   '<strong>'.get_lang('Blocking conditions').'</strong>' . "\n"
-.   '<form action="' . $_SERVER['PHP_SELF'] . '?cmd=blockcondAdd&pathId='.$pathId.'&itemId='.$itemId.'"  method="post" id="blocking_conditions">' . "\n";
-
-if( $blockcond->load() )
-{
-    $blockconds = $blockcond->getBlockConds();
-    
-    foreach( $blockconds['item'] as $key => $value )
-    {
-        $htmlConditionsForm .= '<div>' . "\n";
-        if( $key > 0 )
-        {
-            $htmlConditionsForm .= '<select name="condition[]">' . "\n"
-            .   '<option value="AND" '.($blockconds['condition'][$key-1] == 'AND' ? 'selected="selected"' : '').'>'.get_lang('AND').'</option>' . "\n"
-            .   '<option value="OR" '.($blockconds['condition'][$key-1] == 'OR' ? 'selected="selected"' : '').'>'.get_lang('OR').'</option>' . "\n"
-            .   '</select>'
-            .   '<button onclick="$(this).parent().remove();">'.get_lang('Remove').'</button>'
-            .   '<br />' . "\n";
-        }
-        
-        $htmlConditionsForm .= '<select name="item[]">' . "\n";
-        foreach( $itemListArray as $anItem )
-        {
-            $htmlConditionsForm .= '<option value="'. $anItem['id'] .'" style="padding-left:'.(5 + $anItem['deepness']*10).'px;" '.($value == $anItem['id'] && $anItem['type'] != 'CONTAINER' ? 'selected="selected"' : '').' '.($anItem['type'] == 'CONTAINER' ? 'disabled="disabled"' : '').'>'.$anItem['title'].'</option>' . "\n";
-        }
-        $htmlConditionsForm .= '</select>'
-        .   '<select name="operator[]">' . "\n"
-        .   '<option value="=" '.( $blockconds['operator'][$key] == '=' ? 'selected="selected"' : '').'>=</option>' . "\n"
-        .   '</select>'
-        .   '<select name="status[]">' . "\n"
-        .   '<option value="COMPLETED" '.( $blockconds['status'][$key] == 'COMPLETED' ? 'selected="selected"' : '' ).'>'.get_lang('completed').'</option>' . "\n"
-        .   '<option value="INCOMPLETE" '.( $blockconds['status'][$key] == 'INCOMPLETE' ? 'selected="selected"' : '' ).'>'.get_lang('incomplete').'</option>' . "\n"
-        .   '<option value="PASSED" '.( $blockconds['status'][$key] == 'PASSED' ? 'selected="selected"' : '' ).'>'.get_lang('passed').'</option>' . "\n"
-        .   '</select>'
-        .   '</div>' . "\n";
-    }
-}
-
-$htmlConditionsForm .= '<input type="button" value="'. get_lang( 'Add a blocking condition' ) .'" id="block_condition_button" onclick="addBlockingCondition('.$pathId.')" />' . "\n"
-.   '<input type="submit" value="'.get_lang( 'Save the blocking condition(s)' ) .'" />' . "\n"    
-.   '</form>' . "\n";
-$htmlConditionsForm .= '<form action="' . $_SERVER['PHP_SELF'] . '?cmd=blockcondDelete&pathId='.$pathId.'&itemId='.$itemId.'"  method="post" id="blocking_conditions">' . "\n"
-.   '<input type="submit" value="'.get_lang( 'Remove all blocking conditions' ).'" />' . "\n"
-.   '</form>' . "\n";
-
-$out .= $htmlConditionsForm;
-
 
 $claroline->display->body->appendContent($out);
 
