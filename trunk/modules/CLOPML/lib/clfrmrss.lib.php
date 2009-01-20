aae615d756edc84386f106868d943b4832531e2f
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-01-20 15:53:11 +0000

    Update applet to allow RSS for specific topic and forum

---
+++ trunk/modules/CLOPML/lib/clfrmrss.lib.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+* Forum RSS Generator function : generate forum rss from user course
+*
+* @version     1.9 $Revision: 373 $
+* @copyright   2001-2009 Universite catholique de Louvain (UCL)
+* @author      Dimitri Rambout <dimitri.rambout@uclouvain.be>
+* @license     http://www.gnu.org/copyleft/gpl.html
+*              GNU GENERAL PUBLIC LICENSE
+* @package     CLFRMRSS
+*/
+
+if (count(get_included_files() ) == 1 )
+{
+    die('The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+}
+
+function generate_forum_rss( $courseId )
+{
+    include_once get_path('incRepositorySys') . '/lib/forum.lib.php';
+    require_once dirname(__FILE__) . '/rss.lib.php';
+    
+    $userId = claro_get_current_user_id();
+    
+    $tblCourse = claro_sql_get_course_tbl();
+    
+    $forum_id = isset($_REQUEST['forumId']) ? (int) $_REQUEST['forumId'] : null;
+    $topic_id = isset($_REQUEST['topicId']) ? (int) $_REQUEST['topicId'] : null;
+    
+    
+    $sql = "SELECT p.`post_id`, p.`post_time`, t.`topic_title`, pt.`post_text`, f.`forum_name`, p.`nom`, p.`prenom`
+            FROM `" . $tblCourse['bb_posts'] . "` p
+            JOIN `" . $tblCourse['bb_topics'] . "` t ON p.`topic_id` = t.`topic_id`
+            JOIN `" . $tblCourse['bb_posts_text'] . "` pt ON p.`post_id` = pt.`post_id`
+            JOIN `" . $tblCourse['bb_forums'] . "` f ON p.`forum_id` = f.`forum_id` ";
+    $sql_cond = "";
+    if( $forum_id )
+    {
+        if( $sql_cond )
+        {
+            $sql_cond .= " AND ";
+        }
+        $sql_cond .= " f.`forum_id` = '" . (int) $forum_id . "' ";
+    }
+    
+    if( $topic_id )
+    {
+        if( $sql_cond )
+        {
+            $sql_cond .= " AND ";
+        }
+        $sql_cond .= " t.`topic_id` = '" . $topic_id . "' ";
+    }
+    
+    if( $sql_cond )
+    {
+        $sql .= " WHERE " . $sql_cond;
+    }
+    
+    $sql .= " ORDER BY p.`post_time` DESC
+            LIMIT " . get_conf('clfrmrss_max_items', 10);
+            
+    $data = claro_sql_query_fetch_all_rows( $sql );
+    
+        
+    $rss = new CLFRMRss;
+    
+    $xmlAnswer = $rss->generate( $data );
+    
+    return $xmlAnswer;
+        
+    
+    
+}
+
+?>
---
+++ trunk/modules/CLOPML/lib/rss.lib.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+* Forum RSS Generator function : generate forum rss from user course
+*
+* @version     1.9 $Revision: 373 $
+* @copyright   2001-2009 Universite catholique de Louvain (UCL)
+* @author      Dimitri Rambout <dimitri.rambout@uclouvain.be>
+* @license     http://www.gnu.org/copyleft/gpl.html
+*              GNU GENERAL PUBLIC LICENSE
+* @package     CLFRMRSS
+*/
+
+if ( count( get_included_files() ) == 1 )
+{
+    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+}
+
+/**
+ * Forum RSS Generator Class
+ */
+class CLFRMRss
+{
+    public function generate( $data )
+    {
+        
+        $_course = claro_get_course_data();
+        $rssTitle = $_course['name'] . ' (' . $_course['officialCode'] . ')';
+        $rssEmail = $_course['email'] == '' ? get_conf('administrator_email') : $_course['email'];
+           
+        $rss =  '<?xml version="1.0" encoding="utf-8" standalone="yes" ?'.'>'."\n"
+        .       '<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">' . "\n"
+        .       '<channel>' . "\n"
+        .       '<title>' . trim(strip_tags($rssTitle)) . '</title>' . "\n"
+        .       '<description>' . get_lang('List of RSS from all my forums') . '</description>' . "\n"
+        .       '<link></link>' . "\n"
+        .       '<generator>Claroline-PEARSerializer</generator>' . "\n"
+        .       '<webmaster>' . get_conf('administrator_email') . '</webmaster>' . "\n"
+        .       '<managingEditor>' . $rssEmail . '</managingEditor>' . "\n"
+        .       '<language>' . get_locale('iso639_1_code') . '</language>' . "\n"
+        .       '<docs>' . 'http://blogs.law.harvard.edu/tech/rss' . '</docs>' . "\n"
+        .       '<pubDate>' . date("r", time()) . '</pubDate>' . "\n";
+        
+        foreach( $data as $item )
+        {
+            $post_time = datetime_to_timestamp($item['post_time']);
+            
+            $rss .= '<item>' . "\n"
+            .       '<title>' . trim(strip_tags($item['forum_name'])) . ' - ' . trim(strip_tags($item['topic_title'])) . ' - ' . htmlspecialchars( get_lang('%firstname %lastname', array('%firstname' => $item['prenom'], '%lastname' => $item['nom']) ) ) . '</title>' . "\n"
+            .       '<category>' . trim(strip_tags($item['forum_name'])) . '</category>' . "\n"
+            .       '<guid>' . get_conf('rootWeb') . 'claroline/phpbb/viewtopic.php?topic=' . $item['post_id'] .'&amp;cidReset=true&amp;cidReq=' . claro_get_current_course_id() . '</guid>' . "\n"
+            .       '<link>' . get_conf('rootWeb') . 'claroline/phpbb/viewtopic.php?topic=' . $item['post_id'] .'&amp;cidReset=true&amp;cidReq=' . claro_get_current_course_id() . '</link>' . "\n"
+            .       '<author>' . htmlspecialchars( get_lang('%firstname %lastname', array('%firstname' => $item['prenom'], '%lastname' => $item['nom']) ) ) .'</author>'
+            .       '<description>' . trim(strip_tags( $item['post_text'] )) . '</description>' . "\n"
+            .       '<pubDate>' . date("r", $post_time) . '</pubDate>' . "\n"
+            .       '</item>' . "\n"
+            ;
+        }
+        
+        $rss .= '</channel>' . "\n"
+        .       '</rss>' . "\n";   
+        
+        
+        return $rss;
+    }
+}
+
+?>
