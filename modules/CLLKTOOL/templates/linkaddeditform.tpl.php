53e9a400774a34589ed391f54db73226885d77c5
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-03-04 08:42:57 +0000

    Remove method GET/POST, add Width/Height option, use linkrenderer!

--- modules/CLLKTOOL/templates/linkaddeditform.tpl.php
+++ modules/CLLKTOOL/templates/linkaddeditform.tpl.php
@@ -15,13 +15,10 @@
                     <?php endforeach; ?>
                 </select>
             </dd>
-            <dt><label for="visibility"><?php echo get_lang('Visibility'); ?>&nbsp;:</label></dt>
-            <dd>
-                <select name="visibility">                    
-                    <option value="visible" <?php if( $this->visibility == 'visible' ) : echo 'selected="selected"'; endif; ?>><?php echo get_lang('Visible'); ?></option>
-                    <option value="invisible" <?php if( $this->visibility == 'invisible' ) : echo 'selected="selected"'; endif; ?>><?php echo get_lang('Invisible'); ?></option>
-                </select>
-            </dd>
+            <dt><label for="width"><?php echo get_lang('Width'); ?>&nbsp;:</label></dt>
+            <dd><input type="text" name="options[width]" id="width" value="<?php echo $this->width; ?>" style="width: 50px;" /></dd>
+            <dt><label for="height"><?php echo get_lang('Height'); ?>&nbsp;:</label></dt>
+            <dd><input type="text" name="options[height]" id="height" value="<?php echo $this->height; ?>" style="width: 50px;" /></dd>
             <dt><label><?php echo get_lang('Options'); ?>&nbsp;:</label></dt>
             <dd>
                 <div id="options">
@@ -31,24 +28,19 @@
                     ?>
                     <div style="padding: 2px;" id="option_<?php echo $i; ?>" >
                         <?php echo get_lang('Name'); ?> :
-                        <input type="text" name="options[<?php echo $i; ?>][name]" value="<?php echo $option['name']; ?>" id="name_<?php echo $i; ?>" />
+                        <input type="text" name="options[params][<?php echo $i; ?>][name]" value="<?php echo $option['name']; ?>" id="name_<?php echo $i; ?>" />
                         <?php echo get_lang('Value'); ?> :
-                        <select name="options[<?php echo $i; ?>][var]" id="option_<?php echo $i; ?>" onchange="linkLoadOptionValue(this, <?php echo $i; ?>)" ><?php echo $this->optionsList; ?></select>
+                        <select name="options[params][<?php echo $i; ?>][var]" id="option_<?php echo $i; ?>" onchange="linkLoadOptionValue(this, <?php echo $i; ?>)" ><?php echo $this->optionsList; ?></select>
                         <?php
                         if( $option['var'] == 'freeValue' ) :
-                        ?><input type="text" name="options[<?php echo $i; ?>][value]" value="<?php echo $option['value']; ?>" id="value_<?php echo $i; ?>" style="width: 100px;" />
+                        ?><input type="text" name="options[params][<?php echo $i; ?>][value]" value="<?php echo $option['value']; ?>" id="value_<?php echo $i; ?>" style="width: 100px;" />
                         <?php
                         else :
                         ?>
-                        <input type="hidden" name="options[<?php echo $i; ?>][value]" value="" id="value_<?php echo $i; ?>" />
+                        <input type="hidden" name="options[params][<?php echo $i; ?>][value]" value="" id="value_<?php echo $i; ?>" />
                         <?php
                         endif;
                         ?>
-                        <?php echo get_lang('Method'); ?> :                        
-                        <select name="options[<?php echo $i; ?>][method]" id="method_<?php echo $i; ?>">
-                        <option value="get" <?php if($option['method'] == 'get'): ?>selected="selected"<?php endif; ?> >GET</option>
-                        <option value="post" <?php if($option['method'] == 'post'): ?>selected="selected"<?php endif; ?>>POST</option>
-                        </select>
                         <a href="#" onclick="linkDelOption('option_<?php echo $i; ?>');"><img src="./img/brick_delete.png" alt="<?php echo get_lang( 'Remove option' ); ?>" /></a>
                         <script type="text/javascript" >
                         $(document).ready(function(){                            
@@ -62,7 +54,7 @@
                     endforeach;
                     ?>
                 </div>
-                <a href="#" onclick="linkAddOption('options','<?php echo get_lang('Name'); ?>','<?php echo get_lang('Value'); ?>','<?php echo get_lang('Method'); ?>', '<?php echo get_lang( 'Remove option' ); ?>')" ><img src="./img/brick_add.png" alt="<?php echo get_lang( 'Add an option' ); ?>" /><?php echo get_lang( 'Add an option'); ?></a>
+                <a href="#" onclick="linkAddOption('options','<?php echo get_lang('Name'); ?>','<?php echo get_lang('Value'); ?>', '<?php echo get_lang( 'Remove option' ); ?>')" ><img src="./img/brick_add.png" alt="<?php echo get_lang( 'Add an option' ); ?>" /><?php echo get_lang( 'Add an option'); ?></a>
             </dd>
         </dl>        
     </fieldset>
