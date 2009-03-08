8d39466ea4274b067318eb1611948aaf1371ba0c
dkp1060 <dkp1060@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-03-08 22:27:41 +0000

    module to manage unused and expired courses

---
+++ modules/trunk/LVUNUSED/lang/lang_french.php
@@ -0,0 +1,50 @@
+<?php // $Id: lang_french.php
+/**
+ *Anomaly
+ *
+ * @version 1.0.0
+ *
+ * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package Anomaly
+ * @subpackage LANG-FR
+ *
+ * @authorBanziziki Muhire Robert <bmuhirerobert@yahoo.fr>
+ *
+ */
+
+$_lang['Enabled']="Actif";
+$_lang['Enable']="Activer";
+$_lang['Disabled']="Inactif";
+$_lang['Publish date']="Date de publication";
+$_lang['Remove date']="Date d'expiration";
+$_lang['Courses disabled']="Cours désactivés";
+$_lang['Courses unpublished']="Cours non publiés";
+$_lang['Courses without lecturer\'s email']="Cours dont aucun gestionnaire n'a d'email.";
+$_lang['List of anomalies']="Liste des anomalies";
+$_lang['New research']="Nouvelle recherche";
+$_lang['Deleted']="Supprimé";
+$_lang['Listing of all submited changes']="Liste des modifications effectuées sur les cours.";
+$_lang['State']="Statut";
+$_lang['Subject :']="Sujet:";
+$_lang['Sent to:']="Envoyé à:";
+$_lang['Mail: ']="Message: ";
+$_lang['Sending Mail to: ']="Envoyer un message à:";
+$_lang['Sending email to the course teacher']="Envoi d'un message au(x) gestionnaire(s).";
+$_lang['No email for this user: ']="Cet utilisateur n'a pas d'email.";
+$_lang['No document to publish']="Aucun document à publier.";
+$_lang['Date error: date format must be :dd/mm/yy ']="Format date: jj/mm/aa, non respecté.";
+$_lang['Error on days and months']="Erreur sur les jours et les mois";
+$_lang['Submited data error.Publish date is set after expiration date.']="Erreur sur les données saisies.La date d'expiration est antérieur à la date de publication";
+$_lang['None user selected/No modification']="Aucune sélection / Aucune modification";
+$_lang['Date format: dd/mm/yy']="Format de la date : jj/mm/aa";
+$_lang['Submit changes.']="Modification sans notification au gestionnaire";
+$_lang['Sumbmit changes and send e-mail to admin.']="Modification et notification par e-mail au gestionnaire";
+$_lang['Delete a course.']="Supprimer le(les) cours";
+$_lang['Update done with success.']="Mise à jour effectuée avec succes.";
+$_lang['Course Admin']="Administrateur du cours: ";
+$_lang['Mail not sent: Users missing']="Le message n'a pas été envoyé: Manque de déstinataires.";
+$_lang['Mail sent.']="Message envoyé.";
+
+?>
