570b98873745f41f60a525d39753d0500f9d675e
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-04-16 07:57:54 +0000

    use exception to manage errors

--- trunk/modules/ICPCRDR/proxy.php
+++ trunk/modules/ICPCRDR/proxy.php
@@ -1,22 +1,45 @@
-<?php
+<?php // $Id$
 
-if ( !isset($_SERVER['HTTP_REFERER']) )
+/**
+ * Claroline Advanced Link Tool : Proxy
+ *
+ * @version     ICPCRDR 1.0-alpha $Revision$ - Claroline 1.9
+ * @copyright   2001-2009 Universite catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package     CLLKTOOL
+ * @author      Frederic Minne <frederic.minne@uclouvain.be>
+ */
+
+try
+{
+    if ( !isset($_SERVER['HTTP_REFERER']) )
+    {
+        throw new Exception('Not Allowed');
+    }
+    else
+    {
+        $tmp = parse_url($_SERVER['HTTP_REFERER']);
+        
+        if ( dirname( $tmp['path'] ) !== dirname( $_SERVER['PHP_SELF'] ) ) {
+           throw new Exception('Not Allowed');
+        }
+    }
+    
+    if ( !isset($_REQUEST['url']) || empty($_REQUEST['url']) ) {
+        throw new Exception('Missing parameter');
+    }
+    
+    require_once dirname(__FILE__) . '/lib/urlgetcontents.lib.php';
+    
+    if ( ! $content = url_get_contents($_REQUEST['url']) ) {
+        throw new Exception('Cannot load url');
+    }
+    
+    header("Content-Type: text/xml");
+    echo $content;
+}
+catch ( Exception $e )
 {
     header("HTTP/1.0 403 Forbidden");
-    die();
+    die($e->getMessage());
 }
-else
-{
-    $tmp = parse_url($_SERVER['HTTP_REFERER']);
-    
-    if ( dirname( $tmp['path'] ) !== dirname( $_SERVER['PHP_SELF'] ) ) {
-        header("HTTP/1.0 403 Forbidden");
-        var_dump($tmp);
-        die();
-    }
-}
-
-require_once dirname(__FILE__) . '/lib/urlgetcontents.lib.php';
-
-header("Content-Type: text/xml");
-echo url_get_contents($_REQUEST['url']);
