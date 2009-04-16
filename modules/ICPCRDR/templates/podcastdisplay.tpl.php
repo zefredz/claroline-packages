aef67005bfb0bc5aa1d250b00dad266618174ebd
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2009-04-16 06:50:56 +0000

    move to flowplayer 3.1.0 + utf8 bug in display

--- modules/ICPCRDR/templates/podcastdisplay.tpl.php
+++ modules/ICPCRDR/templates/podcastdisplay.tpl.php
@@ -1,22 +1,22 @@
-<h3 class="channelTitle"><?php echo htmlspecialchars($this->channel['title']);?></h3>
+<h3 class="channelTitle"><?php echo htmlspecialchars(claro_utf8_decode($this->channel['title']));?></h3>
 <p><a href="<?php echo htmlspecialchars($this->url);?>"><img src="<?php echo get_icon_url('feed'); ?>" /> <?php echo get_lang("S'abonner");?></a></p>
 <p class="channelPubDate"><?php echo htmlspecialchars($this->channel['pubDate']);?></p>
-<p class="channelDescription"><?php echo strip_tags( $this->channel['description'] ); ?></p>
+<p class="channelDescription"><?php echo claro_utf8_decode(strip_tags( $this->channel['description'] )); ?></p>
 <?php
     $videoId = 1;
     
     foreach( $this->items as $item ):
 ?>
-<h4 class="itemTitle"><?php echo htmlspecialchars( $item->metadata['title'] ); ?></h4>
+<h4 class="itemTitle"><?php echo htmlspecialchars( claro_utf8_decode($item->metadata['title']) ); ?></h4>
 <p class="itemPubDate"><?php echo htmlspecialchars( $item->metadata['pubDate'] ); ?></p>
-<p class="itemDescription"><?php echo strip_tags($item->metadata['description']); ?></p>
+<p class="itemDescription"><?php echo claro_utf8_decode(strip_tags($item->metadata['description'])); ?></p>
 <a  
     href="<?php echo htmlspecialchars($item->enclosure['url']); ?>"  
     style="display:block;width:400px;height:300px"  
     id="player<?php echo "_{$videoId}"?>"> 
 </a>
 <script type="text/javascript">
-    flowplayer("player<?php echo "_{$videoId}"?>", "./flash/flowplayer-3.0.7.swf");
+    flowplayer("player<?php echo "_{$videoId}"?>", "./flash/flowplayer-3.1.0.swf");
 </script>
 <?php
     $videoId++;
