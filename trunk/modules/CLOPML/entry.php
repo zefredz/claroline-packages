4242cf02152043638164691e6947352518e7eb3e
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-05-13 13:21:51 +0000

    revert to previous version + update manifest to 1.9

--- trunk/modules/CLOPML/entry.php
+++ trunk/modules/CLOPML/entry.php
@@ -6,11 +6,11 @@
      * OPML Generator entry point
      *
      * @version     1.9 $Revision$
-     * @copyright   2001-2009 Universite catholique de Louvain (UCL)
-     * @author      Dimitri Rambout <dimitri.rambout@uclouvain.be>
+     * @copyright   2001-2007 Universite catholique de Louvain (UCL)
+     * @author      Frederic Minne <zefredz@claroline.net>
      * @license     http://www.gnu.org/copyleft/gpl.html
      *              GNU GENERAL PUBLIC LICENSE
-     * @package     CLFRMRSS
+     * @package     CLOPML
      */
     
     if ( count( get_included_files() ) == 1 )
@@ -18,19 +18,19 @@
         die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
     }
     
-    // display link to Forum RSS file for current user
+    // display link to OPML file for current user
     if ( claro_is_user_authenticated() )
     {
-        $GLOBALS['currentModuleLabel'] = 'CLFRMRSS';
+        $GLOBALS['currentModuleLabel'] = 'CLOPML';
         
-        /*$out = '<a href="'
-            . get_module_url('CLFRMRSS')
+        $out = '<a href="'
+            . get_module_url('CLOPML')
             . '/index.php?userId=' . claro_get_current_user_id()
-            . '"><img src="'.get_icon('rss.png').'" />&nbsp;</a>'."\n"
+            . '"><img src="'.get_icon('rss.png').'" />&nbsp;'.get_lang('List of RSS from all my courses (opml file format)').'</a>'."\n"
             ;
-        */    
+            
         $GLOBALS['currentModuleLabel'] = null;
         
-        //$claro_buffer->append( $out );
+        $claro_buffer->append( $out );
     }
 ?>
