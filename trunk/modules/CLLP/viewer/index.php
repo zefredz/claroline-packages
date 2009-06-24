4afa8e521a13a2625a46efa7fc1c701412878e14
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-06-24 08:29:04 +0000

    fix newWindow bug, indent + url::contextualize

--- trunk/modules/CLLP/viewer/index.php
+++ trunk/modules/CLLP/viewer/index.php
@@ -20,14 +20,14 @@ require_once dirname( __FILE__ ) . '/../../../claroline/inc/claro_init_global.in
 
 if ( !claro_is_tool_allowed() )
 {
-	if ( claro_is_in_a_course() )
-	{
-		claro_die( get_lang( "Not allowed" ) );
-	}
-    else
-	{
-		claro_disp_auth_form( true );
-	}
+  if ( claro_is_in_a_course() )
+  {
+    claro_die( get_lang( "Not allowed" ) );
+  }
+  else
+  {
+    claro_disp_auth_form( true );
+  }
 }
 
 /*
@@ -55,19 +55,19 @@ else                                                                  $pathId = 
 // admin only page and path is required as we edit a path ...
 if( is_null($pathId) )
 {
-	header("Location: ../index.php");
-	exit();
+  header("Location: ../index.php");
+  exit();
 }
 else
 {
-    $path = new path();
+  $path = new path();
 
-    if( !$path->load($pathId) )
-    {
-        // path is required
-        header("Location: ../index.php");
-    	exit();
-    }
+  if( !$path->load($pathId) )
+  {
+    // path is required
+    header("Location: ../index.php");
+    exit();
+  }
 }
 
 
@@ -75,10 +75,10 @@ $thisAttempt = new attempt();
 
 if( !$thisAttempt->load($pathId, claro_get_current_user_id()) )
 {
-	// save the attempt as there is no attempt for this user yet
-	$thisAttempt->setPathId($pathId);
-	$thisAttempt->setUserId(claro_get_current_user_id());
-	$thisAttempt->save();
+  // save the attempt as there is no attempt for this user yet
+  $thisAttempt->setPathId($pathId);
+  $thisAttempt->setUserId(claro_get_current_user_id());
+  $thisAttempt->save();
 }
 
 $_SESSION['thisAttempt'] = serialize($thisAttempt);
@@ -86,12 +86,12 @@ $_SESSION['thisAttempt'] = serialize($thisAttempt);
 
 if( $path->getVersion() == 'scorm12' )
 {
-	$scormAPI = new Scorm12();
+  $scormAPI = new Scorm12();
 }
 else
 {
 
-	$scormAPI = new Scorm13();
+  $scormAPI = new Scorm13();
 }
 
 
@@ -104,70 +104,70 @@ claro_set_display_mode_available(false);
 
 if( $path->isVisible() || claro_is_allowed_to_edit() )
 {
-		$claroline->setDisplayType( CL_FRAMESET );
+    $claroline->setDisplayType( CL_FRAMESET );
 
-		$jsloader = JavascriptLoader::getInstance();
-		$jsloader->load('jquery');
-		$jsloader->load('jquery.json');
-		
-		$jsloader->load('CLLP');
-		$jsloader->load('scormtime');
-		$jsloader->load('claroline');
-		$jsloader->load($scormAPI->getApiFileName());
+    $jsloader = JavascriptLoader::getInstance();
+    $jsloader->load('jquery');
+    $jsloader->load('jquery.json');
+    
+    $jsloader->load('CLLP');
+    $jsloader->load('scormtime');
+    $jsloader->load('claroline');
+    $jsloader->load($scormAPI->getApiFileName());
 
-		// prepare html header
+    // prepare html header
 
-		$htmlHeaders = "\n"
-		.    '<script type="text/javascript">' . "\n"
-		
-		.	 '  var pathId = "'.(int) $pathId.'";' . "\n"
-		.	 '  var cidReq = "'.claro_get_current_course_id().'";' . "\n"
-		.	 '  var moduleUrl = "'.get_module_url('CLLP').'/";' . "\n"
-		.    '  var debugMode = '.get_conf('scorm_api_debug').';' . "\n\n"
-		
-		.	 '  var lpHandler = new lpHandler(pathId,cidReq,moduleUrl,debugMode);' . "\n"
-		.	 '  var lp_top = this;' . "\n"
-		.	 '  $(document).ready(function() {' . "\n"
-		.    '    setTimeout("lpHandler.refreshToc()", 900);' . "\n"
-		.    '    setTimeout("lpHandler.setContent(' .$thisAttempt->getLastItemId().')", 1000);' . "\n"
-		.	 '  });' . "\n"
-		.    '</script>' . "\n\n";
-		
-		
-		// prepare frames and framesets
-		
-		// header frame
-		$headerFrame = new ClaroFrame('lp_header', 'header.php?pathId='.$pathId);
-		$headerFrame->noFrameBorder();
-		
-		// add header frame and inner frameset
-		$headerFrameSize = $path->isFullScreen()?'0':'150';
-		
-		$claroline->display->addRow($headerFrame, $headerFrameSize);
-		
-		$innerFrameset = new ClaroFrameSet();
-		
-		// menu frames
-		$tocFrame = new ClaroFrame('lp_toc', 'toc.php?pathId='.$pathId);
-		$tocFrame->allowScrolling(true);
-		$tocFrame->noFrameBorder();
-		
-		// content frame
-		$contentFrame = new ClaroFrame('lp_content', 'blank.php?pathId='.$pathId);
-		$contentFrame->allowScrolling(true);
-		$contentFrame->noFrameBorder();
-		
-		// inner frameset that contains toc and content frames
-		
-		$innerFrameset->addCol($tocFrame, '240');
-		$innerFrameset->addCol($contentFrame, '*');		
-		
-		$claroline->display->header->addHtmlHeader($htmlHeaders);
-		$claroline->display->addRow($innerFrameset, '*');
+    $htmlHeaders = "\n"
+    .  '<script type="text/javascript">' . "\n"
+    
+    .   '  var pathId = "'.(int) $pathId.'";' . "\n"
+    .   '  var cidReq = "'.claro_get_current_course_id().'";' . "\n"
+    .   '  var moduleUrl = "'.get_module_url('CLLP').'/";' . "\n"
+    .  '  var debugMode = '.get_conf('scorm_api_debug').';' . "\n\n"
+    
+    .   '  var lpHandler = new lpHandler(pathId,cidReq,moduleUrl,debugMode);' . "\n"
+    .   '  var lp_top = this;' . "\n"
+    .   '  $(document).ready(function() {' . "\n"
+    .  '  setTimeout("lpHandler.refreshToc()", 900);' . "\n"
+    .  '  setTimeout("lpHandler.setContent(' .$thisAttempt->getLastItemId().')", 1000);' . "\n"
+    .   '  });' . "\n"
+    .  '</script>' . "\n\n";
+    
+    
+    // prepare frames and framesets
+    
+    // header frame
+    $headerFrame = new ClaroFrame('lp_header', 'header.php?pathId='.$pathId);
+    $headerFrame->noFrameBorder();
+    
+    // add header frame and inner frameset
+    $headerFrameSize = $path->isFullScreen()?'0':'150';
+    
+    $claroline->display->addRow($headerFrame, $headerFrameSize);
+    
+    $innerFrameset = new ClaroFrameSet();
+    
+    // menu frames
+    $tocFrame = new ClaroFrame('lp_toc', 'toc.php?pathId='.$pathId);
+    $tocFrame->allowScrolling(true);
+    $tocFrame->noFrameBorder();
+    
+    // content frame
+    $contentFrame = new ClaroFrame('lp_content', 'blank.php?pathId='.$pathId);
+    $contentFrame->allowScrolling(true);
+    $contentFrame->noFrameBorder();
+    
+    // inner frameset that contains toc and content frames
+    
+    $innerFrameset->addCol($tocFrame, '240');
+    $innerFrameset->addCol($contentFrame, '*');    
+    
+    $claroline->display->header->addHtmlHeader($htmlHeaders);
+    $claroline->display->addRow($innerFrameset, '*');
 }
 else
 {
-		claro_die( get_lang( 'Not allowed' ) );
+    claro_die( get_lang( 'Not allowed' ) );
 }
 
 
