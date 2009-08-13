5c35cb9338e185e755e25d9ea49572b76f801492
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-08-13 14:23:32 +0000

    french localization added

---
+++ modules/ICPCRDR/lang/lang_french.php
@@ -0,0 +1,19 @@
+<?php 
+
+$_lang['Add a podcast'] = 'Ajouter un podcast';
+$_lang['Choose a podcast in the list to start'] = 'Choisissez simplement un podcast dans la liste de gauche pour le visionner';
+$_lang['Do you want to delete the link: %linkTitle ?'] = 'Voulez-vous vraiment supprimer le lien: %linkTitle ?';
+$_lang['Edit information for the curent link'] = 'Modifier les informations concernant ce lien';
+$_lang['Error: unable to add the podcast'] = 'Erreur: impossible d\'ajouter ce podcast';
+$_lang['Error: unable to delete the podcast'] = 'Erreur: impossible de supprimer ce podcast';
+$_lang['Error: unable to edit the podcast'] = 'Erreur : impossible de modifier ce podcast';
+$_lang['Feed url'] = 'Adresse du flux';
+$_lang['Get title from feed'] = 'Récupérer le titre à partir du flux';
+$_lang['Podcast edited successfully'] = 'Le podcast a été modifié avec succès!';
+$_lang['Podcast feed not found'] = 'Le flux du podcast n\'a pas été trouvé';
+$_lang['S\'abonner'] = 'S\'abonner';
+$_lang['The podcast deleted successfully'] = 'Le podcast a été supprimé';
+$_lang['The podcast has been added successfully'] = 'Le podcast à été ajouté avec succès!';
+$_lang['The podcast is now invisible'] = 'Ce podcast n\'est maintenant plus visible';
+$_lang['Unable to change the visibility of the podcast'] = 'Impossible de modifier la visibilité de ce podcast';
+$_lang['Video podcast reader'] = 'Lecteur de podcast vidéo';
