2bdc64ea4ad533899c0d4c3654e457aece54750f
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-08-17 11:30:15 +0000

    new messages "realtime" notifier

---
+++ trunk/modules/CLNEWMSG/lang/lang_french.php
@@ -0,0 +1,5 @@
+<?php 
+$_lang['You got a new message!'] = 'Vous avez un nouveau message!';
+$_lang['You have an unread message!'] = 'Vous avez un message non encore lu!';
+$_lang['You got %newMsg new messages!'] = 'Vous avez %newMsg nouveaux messages!';
+$_lang['You have %newMsg unread messages!'] = 'Vous avez %newMsg messages non lus!';
