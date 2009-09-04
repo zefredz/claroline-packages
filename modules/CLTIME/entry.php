c656c65cec7037a89582c802cb686f15d8cea016
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-09-04 09:21:50 +0000

    adds the possibility to configure the refresh rate

--- modules/CLTIME/entry.php
+++ modules/CLTIME/entry.php
@@ -1,3 +1,13 @@
-<?php
+<?php // $Id$
+/**
+ * Server Time
+ *
+ * @version     CLTIME-1.0alpha $Revision$ - Claroline 1.9
+ * @copyright   2001-2009 Universite Catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package     CLTIME
+ * @author      Frédéric Minne <frederic.minne@uclouvain.be>
+ */
+
     $claro_buffer->append( get_lang('Server time') . ' : [<span id="serverTimeApplet">'.date("H:i").'</span>]' );
 ?>
