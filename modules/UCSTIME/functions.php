de595f72561b9172cbb0b780be0b7ec880015f5d
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-09-22 15:42:08 +0000

    UCSTIME finally works as expected :-)

--- modules/UCSTIME/functions.php
+++ modules/UCSTIME/functions.php
@@ -1,31 +1,34 @@
 <?php // $Id$
-
 /**
- * Server Time?
- *
- * @version     UCSTIME 0.1 $Revision$ - Claroline 1.9
- * @copyright   2001-2009 Universite Catholique de Louvain (UCL)
- * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- * @package     UCSTIME
- * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
- */
+* Server Time?
+*
+* @version     UCSTIME 0.1 $Revision$ - Claroline 1.9
+* @copyright   2001-2009 Universite Catholique de Louvain (UCL)
+* @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+* @package     UCSTIME
+* @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
+*/
 
 if ( count( get_included_files() ) == 1 ) die( '---' );
 
-$seconds_displayed = get_conf( 'displaySeconds' ) ? 8 : 5;
+$seconds_displayed = get_conf( 'displaySeconds' ) ? 'HH:mm:ss' : 'HH:mm';
+
+JavascriptLoader::getInstance()->load( 'datetime' );
+JavascriptLoader::getInstance()->load( 'Date' );
 
 ClaroHeader::getInstance()->addHtmlHeader( '
     <script type="text/javascript">
-        function serverTimeShift()
-        {
-            localTime = new Date();
-            timeShift = localTime.getTime() - ' . time() .'*1000;
+        var timeShift;
+        function serverTimeShift(){
+            var _localDate = new Date();
+            var _serverDate = Date.fromDatetime( "'. date( "Y-m-d H:i:s" ) .'");
+             timeShift = _localDate.getTime() - _serverDate.getTime();
         }
-        function serverTimeDisplay()
-        {
-            localTime = new Date();
-            serverTime = new Date ( localTime.getTime() - timeShift );
-            $( "#serverTime" ).html( serverTime.toString().substr( 16 , ' . $seconds_displayed . ' ) );
+        function serverTimeDisplay(){
+            var localDate = new Date();
+            var serverDate = new Date();
+            serverDate.setTime( localDate.getTime() - timeShift );
+            $( "#serverTime" ).html( serverDate.format( "' . $seconds_displayed .'" ) );
             setTimeout( serverTimeDisplay , 1000 );
         }
         $(function()
