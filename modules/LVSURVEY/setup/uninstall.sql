970af4ce1a50165ecacdc2dd960e8621fef20bfa
systho <systho@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-11-09 15:28:45 +0000

    minor changes to sql setup in order to match the recent rewrite of the code

--- modules/LVSURVEY/setup/uninstall.sql
+++ modules/LVSURVEY/setup/uninstall.sql
@@ -1,18 +1,5 @@
-#
-# CLSURV2
-#
-# version 1.0.0
-#
-# copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
-# license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
-#
-# package CLSURVEY
-#
-# Contact : Baudet Gregory <gregory.baudet@gmail.com>
-# Credit  : Baudet Gregory <gregory.baudet@gmail.com>
-# General-contact Claro Team <cvs@claroline.net>
 
-DROP TABLE IF EXISTS `__CL_MAIN__survey2_answer_item`
+DROP TABLE IF EXISTS `__CL_MAIN__survey2_answer_item`; 
 
 DROP TABLE IF EXISTS `__CL_MAIN__survey2_answer`;
 
