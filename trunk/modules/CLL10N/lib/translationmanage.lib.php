f4fd8b050ff3f5df94ab455a871b383b2928743d
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-11-25 09:22:23 +0000

    Add platform translation tool

--- trunk/modules/CLL10N/lib/translationmanage.lib.php
+++ trunk/modules/CLL10N/lib/translationmanage.lib.php
@@ -14,7 +14,14 @@ class TranslationManage{
   
   public function availableLanguages( $module_label, $returnPath = false )
   {
-    $path = get_module_path( $module_label ) . '/lang';
+    if( $module_label == 'PLATFORM' )
+    {
+      $path = get_path( 'clarolineRepositorySys' ) . '/lang';
+    }
+    else
+    {
+      $path = get_module_path( $module_label ) . '/lang';
+    }
     
     $availbleLanguages = array( 0 => '');
     
@@ -22,7 +29,14 @@ class TranslationManage{
     
     foreach( $files as $file )
     {
-      if( !( $file->isDot() || $file->isDir() ) && ( strpos( $file->getFilename(), 'incomplete' ) === false ) && $file->isFile() && substr($file->getFilename(), -4) == '.php' )
+      if( $module_label == 'PLATFORM' )
+      {
+        if( $file->isDir() && !$file->isDot() && !(substr($file->getBasename(),0,1) == '.') )
+        {
+          $lang = $file->getBasename();
+          $availbleLanguages[$lang] = $lang;
+        }
+      }elseif( !( $file->isDot() || $file->isDir() ) && ( strpos( $file->getFilename(), 'incomplete' ) === false ) && $file->isFile() && substr($file->getFilename(), -4) == '.php' )
       {
         $lang = substr( $file->getFilename(), strpos( $file->getFilename(), '_' ) +1 );
         $lang = substr( $lang, 0, strpos( $lang, '.' ) );
@@ -97,9 +111,17 @@ class TranslationManage{
    */
   public function compareLang( $module_label, $lang )
   {
-    $path = get_module_path( $module_label ) . '/lang/lang_' . $lang . '.php';
+    if( $module_label == 'PLATFORM' )
+    {
+      $path = get_path( 'clarolineRepositorySys' ) . '/lang/' . $lang . '/complete.lang.php';
+      $path_install = get_path( 'clarolineRepositorySys' ) . '/lang/' . $lang . '/install.lang.php';
+    }
+    else
+    {
+      $path = get_module_path( $module_label ) . '/lang/lang_' . $lang . '.php';
+    }
     
-    if( !is_file ( $path ) )
+    if( !( is_file ( $path ) ) )
     {
       return false;
     }
@@ -110,8 +132,31 @@ class TranslationManage{
     {
       return false;
     }
+    else
+    {
+      $_lang_tmp = $_lang;
+    }
     
     $extractedLangs = array_flip( $this->extractLangFromScripts( $module_label ) );
+    
+    //skip install lang
+    /*if( $module_label == 'PLATFORM' )
+    {
+      if( is_file( $path_install ) )
+      {
+        require( $path_install );
+        foreach( $_lang as $key => $value )
+        {
+          if( isset( $extractedLangs[ $key ] ) )
+          {
+            unset( $extractedLangs[ $key ] );
+          }
+        }
+      }      
+    }*/
+    
+    $_lang = $_lang_tmp;
+    unset( $_lang_tmp );
     
     $notUsedLangs = array();
     
@@ -171,7 +216,14 @@ class TranslationManage{
   {
     $progression = array();
     
-    $path = get_module_path( $module_label ) . '/lang';
+    if( $module_label == 'PLATFORM' )
+    {
+      $path = get_path( 'clarolineRepositorySys' ) . '/lang/';
+    }
+    else
+    {
+      $path = get_module_path( $module_label ) . '/lang';
+    }
     
     if( !is_dir( $path ) )
     {
@@ -181,17 +233,48 @@ class TranslationManage{
     $languageVarList = $this->extractLangFromScripts( $module_label );
     $nbLanguageVarList = count( $languageVarList );
     
+    //skip install lang
+    /*if( $module_label == 'PLATFORM' )
+    {
+      $path_install = $path . 'english/install.lang.php';
+      if( is_file( $path_install ) )
+      {
+        $languageVarList = array_flip( $languageVarList );
+        require( $path_install );
+        foreach( $_lang as $key => $value )
+        {
+          if( isset( $languageVarList[ $key ] ) )
+          {
+            var_dump( $languageVarList[ $key ] );
+            unset( $languageVarList[ $key ] );
+          }
+        }
+        $languageVarList = array_flip( $languageVarList );
+      }      
+    }*/
     $langs = $this->availableLanguages( $module_label, true);
+    
+    if( isset( $langs[0] ) && empty( $langs[0] ) )
+    {
+      unset( $langs[0] );
+    }
     
     foreach( $langs as $lang => $lang_path )
     {
-        $lang_progression = $this->getLangProgression( $lang_path, $languageVarList );
+        if( $module_label == 'PLATFORM' )
+        {
+          $lang_path = $path . $lang_path . '/complete.lang.php';
+          $lang_progression = $this->getLangProgression( $lang_path, $languageVarList );
+        }
+        else
+        {
+          $lang_progression = $this->getLangProgression( $lang_path, $languageVarList );
+        }
         
         $lang_progression = floor( 100 - (($nbLanguageVarList - $lang_progression) / $nbLanguageVarList * 100) );
         
         $progression[$lang] = $lang_progression;
     }
-    
     return $progression;
     
   }  
@@ -209,6 +292,74 @@ class TranslationManage{
     
     return $languageVarList;
   }
+  /**
+   *
+   *
+   */
+  private function extractLangFromDefConfFile( $file )
+  {
+    require_once( $file );
+    
+    $languageVarList = array();
+    $rejectValues = array( 'boolean', 'enum', 'integer', 'multi', 'relpath', 'string', 'syspath', 'urlpath' );
+    
+    if( isset( $conf_def ) && is_array( $conf_def ) )
+    {
+      if( isset( $conf_def[ 'config_name' ] ) )
+      {
+        $languageVarList[] = $conf_def['config_name'];
+      }
+      foreach( $conf_def['section'] as $section )
+      {
+        if( is_array( $section ) )
+        {
+          if( isset( $section['label'] ) )
+          {
+            $languageVarList[] = $section['label'];
+          }
+          if( isset( $section['description'] ) )
+          {
+            $languageVarList[] = $section['description'];
+          }
+        }
+      }
+      if( isset( $conf_def['section']['main']['label'] ) )
+      {
+        $languageVarList[] = $conf_def['section']['main']['label'];
+      }
+      if( isset( $conf_def['section']['quota']['description'] ) )
+      {
+        $languageVarList[] = $conf_def['section']['quota']['description'];
+      }      
+    }
+    
+    if( isset( $conf_def_property_list ) && is_array( $conf_def_property_list ) )
+    {
+      foreach( $conf_def_property_list as $key => $prop )
+      {
+        if( isset( $prop[ 'label' ] ) && $prop[ 'label' ] )
+        {
+          $languageVarList[] = $prop[ 'label' ];
+        }
+        if( isset( $prop[ 'description' ] ) && $prop[ 'description' ] )
+        {
+          $languageVarList[] = $prop[ 'description' ];
+        }
+        if( isset( $prop[ 'acceptedValue' ] ) && is_array( $prop['acceptedValue'] ) )
+        {
+          foreach( $prop[ 'acceptedValue' ] as $acceptedValue )
+          {
+            if( ! is_int( $acceptedValue ) || !in_array( $acceptedValue, $rejectValues ) )
+            {
+              $languageVarList[] = $acceptedValue;
+            }
+          }
+        }
+      }
+    }
+    
+    return array_unique( $languageVarList );
+  }
   
   /**
    * Create a file with non translated sentence
@@ -222,10 +373,15 @@ class TranslationManage{
    */
   private function buildIncompleteLangFile( $langs , $file )
   {
+    $rejectValues = array( 'boolean', 'enum', 'integer', 'multi', 'relpath', 'string', 'syspath', 'urlpath' );
+    
     $content = '<?php ' . "\n\n";
-    foreach( $langs as $lang){
-      $lang = str_replace( "'", "\'", $lang);
-      $content .= '$_lang[\''. $lang .'\'] = \'' . $lang . '\';' . "\n";
+    foreach( $langs as $lang){      
+      if( !in_array( $lang, $rejectValues ) )
+      {
+        $lang = str_replace( "'", "\'", $lang);
+        $content .= '$_lang[\''. $lang .'\'] = \'' . $lang . '\';' . "\n";
+      }
     }
     $content .= "\n" . '?>';
     
@@ -240,7 +396,7 @@ class TranslationManage{
   }
   
   /**
-   * create a lang file
+   * create a install lang file
    *
    * @param array $langs Langs that we need to put in the file
    * @param string $path Directory path of the lang file
@@ -248,18 +404,17 @@ class TranslationManage{
    * @return boolean True of False
    *
    */  
-  public function createLangFile( $langs, $path, $lang)
+  public function createInstallLangFile( $langs, $path, $lang )
   {
+    $file = $path . '/' . $lang . '/install.lang.php';
+    $file_incomplete = $path . '/' . $lang . '/missing.install.lang.php';
     
-    $file = $path . '/lang_'. $lang .'.php';
-    $file_incomplete = $path . '/lang_'. $lang .'.incomplete.php';
     if( is_file( $file ) )
     {
-      include_once( $file );
+      include( $file );
       
       if( !empty( $_lang ) && is_array( $_lang ) )
       {
-       
        $langs = array_flip( $langs );
        foreach( $_lang as $id => $l)
        {
@@ -282,14 +437,138 @@ class TranslationManage{
   }
   
   /**
+   * create a lang file
+   *
+   * @param array $langs Langs that we need to put in the file
+   * @param string $path Directory path of the lang file
+   *
+   * @return boolean True of False
+   *
+   */  
+  public function createLangFile( $langs, $path, $lang, $isPlatform = false )
+  {
+    if( $isPlatform )
+    {
+      $file = $path . '/' . $lang . '/complete.lang.php';
+      $file_install = $path . '/' . $lang . '/install.lang.php';
+      $file_incomplete = $path . '/' . $lang . '/missing.lang.php';
+    }
+    else
+    {
+      $file = $path . '/lang_'. $lang .'.php';
+      $file_incomplete = $path . '/lang_'. $lang .'.incomplete.php';      
+    }
+    
+    if( isset( $file_install) && is_file( $file_install) )
+    {
+      include( $file_install );
+    }
+    
+    if( is_file( $file ) )
+    {
+      include( $file );
+      $tmp_langs = array();
+      if( !empty( $_lang ) && is_array( $_lang ) )
+      {
+       $langs = array_flip( $langs );
+       foreach( $_lang as $id => $l)
+       {
+          if( isset( $langs[ $id ] ) )
+          {
+            unset( $langs[ $id ] );
+          }
+          elseif( $id == '' || is_numeric( $id ) )
+          {
+            unset( $langs[ $id ] );
+          }
+          else
+          {
+            $tmp_langs[] = $id;
+          }
+       }
+       $langs = $tmp_langs;
+       unset( $tmp_langs );
+       unset($_lang);
+      }
+    }
+    else
+    {
+      touch( $file );
+    }
+    // build incomplete file
+    return $this->buildIncompleteLangFile( $langs, $file_incomplete );
+  }
+  
+  /**
+   * Create all install language files (minimum english)
+   *
+   * @author Dimtiri Rambout <dim@claroline.net>
+   * @param array $langs Langs extracted from scrpts
+   * @param string $path Lang path in the module
+   * 
+   * @return boolean
+   */
+  public function createInstallLangFiles( $langs, $path )
+  {
+    if(!is_dir( $path ) )
+    {
+      if( ! claro_mkdir( $path ) )
+      {
+        claro_die( get_lang('Unable to create lang directory.' ) );
+      }
+    }
+    $files = new DirectoryIterator( $path );
+    
+    $langs_exported = array();
+    $error = false;
+    
+    if( ! $this->createInstallLangFile( $langs, $path, 'english' ) )
+    {
+      $error = true;
+    }
+    else
+    {
+      $langs_exported[ 'english' ] = 'english';
+    }
+    
+    foreach( $files as $file )
+    {
+      if( !( $file->isDot() ) && $file->isDir() && substr( $file->getBasename(), 0, 1 ) != '.' )
+      {
+        $lang = $file->getBasename();
+        //$path = $path . '/' . $lang;
+        
+        $files = new DirectoryIterator( $path );
+        
+        foreach( $files as $file )
+        {
+          if( !isset( $langs_exported[ $lang ] ) )
+          {
+            if( ! $this->createInstallLangFile( $langs, $path, $lang ) )
+            {
+              $error = true;
+              break;
+            }
+            else
+            {
+              $langs_exported[ $lang ] = $lang;
+            }
+          }
+        }
+      }
+    }
+  }
+  /**
    * Create all language files (minimum english)
    *
    * @author Dimitri Rambout <dim@claroline.net>
    * @param array $langs Langs exracted from scripts
    * @param string $path Lang path in the module
+   * @param boolean $isPlatform True if plaftorm module
+   * 
    * @return boolean
    */
-  public function createLangFiles( $langs, $path )
+  public function createLangFiles( $langs, $path, $isPlatform )
   {
     if(!is_dir( $path ) )
     {
@@ -304,7 +583,7 @@ class TranslationManage{
     $langs_exported = array();
     $error = false;
     
-    if( ! $this->createLangFile( $langs, $path, 'english' ) )
+    if( ! $this->createLangFile( $langs, $path, 'english', $isPlatform ) )
     {
       $error = true;
     }
@@ -315,13 +594,39 @@ class TranslationManage{
     
     foreach( $files as $file )
     {
-      if( !( $file->isDot() || $file->isDir() ) )
+      if( $isPlatform )
+      {
+        if( !( $file->isDot() ) && $file->isDir() && substr( $file->getBasename(), 0, 1 ) != '.' )
+        {
+          $lang = $file->getBasename();
+          //$path = $path . '/' . $lang;
+          
+          $files = new DirectoryIterator( $path );
+          
+          foreach( $files as $file )
+          {
+            if( !isset( $langs_exported[ $lang ] ) )
+            {
+              if( ! $this->createLangFile( $langs, $path, $lang, $isPlatform ) )
+              {
+                $error = true;
+                break;
+              }
+              else
+              {
+                $langs_exported[ $lang ] = $lang;
+              }
+            }
+          }
+        }
+      }
+      elseif( !( $file->isDot() || $file->isDir() ) )
       {
         $lang = substr( $file->getFilename(), strpos( $file->getFilename(), '_' ) +1 );
         $lang = substr( $lang, 0, strpos( $lang, '.' ) );
         if( ! isset( $langs_exported[ $lang ] ) )
         {
-          if( ! $this->createLangFile( $langs, $path, $lang) )
+          if( ! $this->createLangFile( $langs, $path, $lang, $isPlatform ) )
           {
             $error = true;
             break;
@@ -384,8 +689,25 @@ class TranslationManage{
    * @param string $moduleId Module ID
    * @return array language strings
    */
-  public function extractLangFromScripts( $moduleId ){
-    $module_path = get_module_path( $moduleId );
+  public function extractLangFromScripts( $moduleId, $restrictedDirectory = null ){
+    if( $moduleId == 'PLATFORM' )
+    {
+      $module_path = get_path( 'clarolineRepositorySys' ) . '/';
+      if( ! is_null( $restrictedDirectory ) )
+      {
+        $module_path .= $restrictedDirectory . '/';
+      }
+    }
+    else
+    {
+      $module_path = get_module_path( $moduleId );
+      if( ! is_null( $restrictedDirectory ) )
+      {
+        $module_path .= '/' . $restrictedDirectory . '/';
+      }
+    }
+    
+    set_time_limit(0);
     $files = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $module_path ), RecursiveIteratorIterator::SELF_FIRST );
     
     $languageVarList = array();
@@ -394,14 +716,23 @@ class TranslationManage{
     {
       if( $file->isFile() && substr($file->getFilename(), -4) == '.php' )
       {
-        $languageVarList = array_merge( $languageVarList, array_flip($this->extractLangFromFile( $file->getPathName() ) ));
+        if( strpos( $file->getFilename(), '.def.conf.inc.php' ) === false )
+        {
+          $languageVarList = array_merge( $languageVarList, array_flip($this->extractLangFromFile( $file->getPathName() ) ));
+        }
+        else
+        {
+          //extract from def.conf.inc file
+          $languageVarList = array_merge( $languageVarList, array_flip( $this->extractLangFromDefConfFile( $file->getPathName() ) ));
+        }
+        
         
       }
     }
     
     // get langs from core
     $core_lang = get_path( 'clarolineRepositorySys' ) . 'lang/english/complete.lang.php';
-    if( is_file( $core_lang ) )
+    if( $moduleId != 'PLATFORM' && is_file( $core_lang ) )
     {
       require( $core_lang );
       if( !empty($_lang)  && is_array( $_lang ) )
@@ -416,7 +747,6 @@ class TranslationManage{
         unset( $_lang );
       }
     }
-    
     ksort($languageVarList);
     $languageVarList = array_keys( $languageVarList );
     
