c9cc97d5a630e11c22d26d914703c233cf2cdf11
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2009-12-03 14:00:35 +0000

    Updage install & uninstall sql files

--- trunk/modules/CLSTATS/setup/course_uninstall.sql
+++ trunk/modules/CLSTATS/setup/course_uninstall.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS `__CL_MAIN__courses_stats`;
 DROP TABLE IF EXISTS `__CL_MAIN__stats`;
 DROP TABLE IF EXISTS `__CL_MAIN__stats_reports`;
+DROP TABLE IF EXISTS `__CL_MAIN__stats_usage`;
