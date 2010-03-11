c790ff95f4b605c7abc769e27a8e8fc82c075141
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-03-11 09:56:18 +0000

    Display Grapple Resources in LP as specific resource. Display GVIS on the Toc. Modify SQL installation files to manage Grapple Resources.

--- modules/GRAPPLE/setup/course_uninstall.sql
+++ modules/GRAPPLE/setup/course_uninstall.sql
@@ -3,3 +3,5 @@ DROP TABLE IF EXISTS `__CL_COURSE__lp_item`;
 DROP TABLE IF EXISTS `__CL_COURSE__lp_attempt`;
 DROP TABLE IF EXISTS `__CL_COURSE__lp_item_attempt`;
 DROP TABLE IF EXISTS `__CL_COURSE__lp_item_blockcondition`;
+
+DROP TABLE IF EXISTS `__CL_COURSE__lp_grapple_resources`;
