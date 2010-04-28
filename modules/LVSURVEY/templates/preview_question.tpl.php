b2e25ce987bd3229f566ad40637f60824abe1a65
systho <systho@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-04-28 09:15:50 +0000

    Fixed the question preview

--- modules/LVSURVEY/templates/preview_question.tpl.php
+++ modules/LVSURVEY/templates/preview_question.tpl.php
@@ -10,44 +10,79 @@
 	<div class="LVSURVEYQuestionTitle">
 		<?php echo htmlspecialchars($question->text); ?>
     </div>
-	<div class="LVSURVEYQuestionContent">
-	<?php if ('OPEN' == $question->type) : ?>
-		<textarea name="choiceText<?php  echo $question->id; ?>" id="choiceText<?php  echo $question->id; ?>" rows="3" cols="40">
-		</textarea>
-	<?php endif; ?>
-	<?php if ('MCSA' == $question->type) : ?>
-		<ul <?php echo ($question->choiceAlignment == 'HORIZ')?'class="horizChoiceList"':''; ?>>
-			<?php foreach($question->getChoiceList() as $choice) : ?>
-				<li>
-					<input name="choiceId<?php  echo $question->id; ?>" type="radio" value="<?php  echo $choice->id; ?>" 
-						id="choiceId<?php  echo $question->id; ?>_<?php  echo $choice->id; ?>" />
-    	    	    <label for="choiceId<?php  echo $question->id; ?>_<?php  echo $choice->id; ?>">
-    	    	        	<?php echo htmlspecialchars($choice->text); ?>
-    	    	    </label>
-				</li>
-			<?php endforeach;?>
-    	</ul>
-	<?php endif; ?>
-	<?php if ('MCMA' == $question->type) : ?>
-		<ul <?php echo ($question->choiceAlignment == 'HORIZ')?'class="horizChoiceList"':''; ?>>
-			<?php foreach($question->getChoiceList() as $choice) : ?>
-				<li>
-					<input name="choiceId<?php  echo $question->id; ?>[]" type="checkbox" value="<?php  echo $choice->id; ?>" 
-						id="choiceId<?php  echo $question->id; ?>[]_<?php  echo $choice->id; ?>" />
-                    <label for="choiceId<?php  echo $question->id; ?>[]_<?php  echo $choice->id; ?>">
-                        <?php echo htmlspecialchars($choice->text); ?>
-                    </label>
-				</li>
-			<?php endforeach;?>
-        </ul>
-	<?php endif; ?>
-	</div>
-	<div class="answerCommentBlock" id="answerCommentBlock<?php echo $question->id; ?>">
-        <?php echo get_lang('Comment'); ?> : 
-        <input maxlength="200" type="text" size="70" name="answerComment<?php echo $question->id; ?>" />
-        <span id="commentCharLeft<?php echo $question->id; ?>" class="commentCharLeft"></span>
-        <?php echo get_lang('char(s) left'); ?>
-    </div>
+    <div class="LVSURVEYQuestionContent">
+		<?php if ('OPEN' == $question->type) : ?>
+			<textarea name="choiceText" id="choiceText" rows="3" cols="40">
+			</textarea>
+		<?php endif; ?>
+		<?php if ('MCSA' == $question->type) : ?>
+			<ul>
+				<?php foreach($question->getChoiceList() as $choice) : ?>
+					<li>
+						<input 	name="choiceId" 
+								type="radio" 
+								value="" 
+								id="choiceId_<?php  echo $choice->id; ?>" 
+						/>
+                        <label 	for="choiceId_<?php  echo $choice->id; ?>">
+                        	<?php echo htmlspecialchars($choice->text); ?>
+                        </label>
+					</li>
+				<?php endforeach;?>
+             </ul>
+		<?php endif; ?>
+		<?php if ('MCMA' == $question->type) : ?>
+			<ul>
+				<?php foreach($question->getChoiceList() as $choice) : ?>
+					<li>
+						<input 	name="choiceId[]" 
+								type="checkbox" 
+								value="<?php  echo $choice->id; ?>" 
+								id="choiceId[]_<?php  echo $choice->id; ?>"								
+						/>
+                        <label for="choiceId[]_<?php  echo $choice->id; ?>">
+                        	<?php echo htmlspecialchars($choice->text); ?>
+                        </label>
+					</li>
+				<?php endforeach;?>
+            </ul>
+		<?php endif; ?>
+		<?php if ('ARRAY' == $question->type) : ?>
+			<table>
+				<?php foreach($question->getChoiceList() as $choice) : ?>
+					<tr>						
+                        <td><span>
+                        	<?php echo htmlspecialchars($choice->text); ?> : 
+                        </span></td>
+                        <?php foreach($choice->getOptionList() as $option) : ?>
+							<td><span>
+								<input 	name="choiceId_<?php  echo $choice->id; ?>" 
+										type="radio" 
+										value="<?php  echo $option->getId(); ?>" 
+										id="choiceId_<?php  echo $choice->id; ?>_optionId<?php  echo $option->getId(); ?>" 
+								/>
+		                        <label 	for="choiceId_<?php  echo $choice->id; ?>_optionId<?php  echo $option->getId(); ?>">
+		                        	<?php echo htmlspecialchars($option->getText()); ?>
+		                        </label>
+							</span></td>
+						<?php endforeach;?>                        
+					</tr>
+				<?php endforeach;?>
+            </table>
+		<?php endif; ?>
+	</div>				
+	<div class="answerCommentBlock" id="answerCommentBlock">				
+    	<?php echo get_lang('Comment'); ?> :
+        <input 	
+        	maxlength="200" 
+        	type="text" 
+        	size="70" 
+        	name="answerComment" 
+        	id="answerComment"
+        />
+        <span id="commentCharLeft" class="commentCharLeft"></span>
+        <?php echo get_lang('char(s) left'); ?>            			
+ 	</div>	
     <div>
     	<a href="question_pool.php?<?php echo isset($this->surveyId)?'surveyId='.$this->surveyId:''; ?>">
     		&gt;&gt; <?php echo get_lang('Go back to Question Pool')?>
