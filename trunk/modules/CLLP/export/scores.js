dcfa4b6ca7c273e47069b52757d1b5e9e35b8b5d
dimitrirambout <dimitrirambout@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-06-29 09:10:08 +0000

    fix bug when element is not found in scorm exercise

--- trunk/modules/CLLP/export/scores.js
+++ trunk/modules/CLLP/export/scores.js
@@ -25,35 +25,37 @@ function CalculateRawScore(objDoc, idCount, fillin)
     {
         eltId = 'scorm_' + i;
         element = objDoc.getElementById(eltId);
-        
-        myMatch = myRegexp.exec(element.name);
-        questionType = myMatch[1];
-        questionNum = myMatch[2];
-        answerNum = myMatch[3];
-        
-        switch (questionType)
+        if( element )
         {
-            case 'unique':
-            case 'multiple':
-                if (element.checked) 
-                {
+            myMatch = myRegexp.exec(element.name);
+            questionType = myMatch[1];
+            questionNum = myMatch[2];
+            answerNum = myMatch[3];
+            
+            switch (questionType)
+            {
+                case 'unique':
+                case 'multiple':
+                    if (element.checked) 
+                    {
+                        score += (+element.value);
+                    }
+                    break;
+                    
+                case 'matching':
                     score += (+element.value);
-                }
-                break;
-                
-            case 'matching':
-                score += (+element.value);
-                break;
-                
-            case 'fill':
-                var textIn = element.value;
-                if (textIn.toUpperCase() == fillin[element.name][0].toUpperCase())
-                {
-                    var w = fillin[element.name][1];
-                    score += (+w);
-                }
-                break;
-                
+                    break;
+                    
+                case 'fill':
+                    var textIn = element.value;
+                    if (textIn.toUpperCase() == fillin[element.name][0].toUpperCase())
+                    {
+                        var w = fillin[element.name][1];
+                        score += (+w);
+                    }
+                    break;
+                    
+            }
         }
     }
     
