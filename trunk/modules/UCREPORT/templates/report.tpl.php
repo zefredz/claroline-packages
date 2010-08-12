2a7c4c6607ecabfdb1265118c4e0e846908e1600
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-08-12 14:48:30 +0000

    corrected infos in files headers. ( 1000th commit reached! \o/ )

--- trunk/modules/UCREPORT/templates/report.tpl.php
+++ trunk/modules/UCREPORT/templates/report.tpl.php
@@ -2,8 +2,8 @@
 /**
  * Claroline Poll Tool
  *
- * @version     UCREPORT 0.9.0 $Revision$ - Claroline 1.9
- * @copyright   2001-2009 Universite Catholique de Louvain (UCL)
+ * @version     UCREPORT 0.9.1 $Revision$ - Claroline 1.9
+ * @copyright   2001-2010 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     UCREPORT
  * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
