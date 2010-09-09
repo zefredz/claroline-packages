20140ebd073a581dd2c95fce418ca813480c58ce
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-09-09 11:41:53 +0000

    display the message when $dateTo is set and $dateFrom is null

--- modules/CLSUBSCR/templates/subscription.tpl.php
+++ modules/CLSUBSCR/templates/subscription.tpl.php
@@ -41,10 +41,10 @@ endif;
             if( ! is_null( $subscription['visibilityFrom'] ) && ! is_null( $subscription['visibilityTo'] ) ) :
                 echo get_lang( 'from %dateFrom to %dateTo', array( '%dateFrom' => claro_date( 'Y/m/d - H:i', $subscription['visibilityFrom'] ) ,
                                                                    '%dateTo'   => claro_date( 'Y/m/d - H:i', $subscription['visibilityTo'] ) ) );
-            elseif( ! is_null( $subscription['visibilityFrom'] ) ) :
-                echo get_lang( 'from %dateFrom', array( '%dateFrom' => claro_date( 'Y/m/d - H:i', $subscription['visibilityFrom'] ) ) );
             elseif( ! is_null( $subscription['visibilityTo'] ) ) :
                 echo get_lang( 'until %dateTo', array( '%dateTo' => claro_date( 'Y/m/d - H:i', $subscription['visibilityTo'] ) ) );
+            else :
+                echo get_lang( 'from %dateFrom', array( '%dateFrom' => claro_date( 'Y/m/d - H:i', $subscription['visibilityFrom'] ) ) );
             endif;
         ?>
         </span>
