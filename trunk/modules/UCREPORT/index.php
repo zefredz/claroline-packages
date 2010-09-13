8a38b50fc7c9f019715755f25743306437c31c5f
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-09-13 07:35:31 +0000

    added forgotten $nameTools

--- trunk/modules/UCREPORT/index.php
+++ trunk/modules/UCREPORT/index.php
@@ -2,7 +2,7 @@
 /**
  * Student Report for Claroline
  *
- * @version     UCREPORT 0.9.6 $Revision$ - Claroline 1.9
+ * @version     UCREPORT 1.0.1 $Revision$ - Claroline 1.9
  * @copyright   2001-2010 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     UCREPORT
@@ -10,6 +10,7 @@
  */
 
 $tlabelReq = 'UCREPORT';
+$nameTools = 'Student report';
 
 require_once dirname(__FILE__) . '/../../claroline/inc/claro_init_global.inc.php';
 
