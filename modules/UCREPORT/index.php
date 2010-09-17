5fc36a4a23281694bcdb1e74670973c8d1646c70
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-09-17 15:18:58 +0000

    added get_lang() around $nameTools

--- modules/UCREPORT/index.php
+++ modules/UCREPORT/index.php
@@ -9,10 +9,10 @@
  * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
  */
 
-$tlabelReq = 'UCREPORT';
-$nameTools = 'Student report';
-
 require_once dirname(__FILE__) . '/../../claroline/inc/claro_init_global.inc.php';
+
+$tlabelReq = 'UCREPORT';
+$nameTools = get_lang( 'Student report' );
 
 FromKernel::uses( 'utils/input.lib' , 'utils/validator.lib' , 'display/layout.lib' , 'thirdparty/tcpdf/tcpdf' , 'fileUpload.lib' );
 From::Module( 'UCREPORT' )->uses( 'report.lib' , 'report2csv.lib' );
