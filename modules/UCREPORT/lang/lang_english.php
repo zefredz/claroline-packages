991fdfb76f36794803a52fa466c461bea4e94a4f
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-10-04 12:49:47 +0000

    added online help in french (english version will come later...)

---
+++ modules/UCREPORT/lang/lang_english.php
@@ -0,0 +1,13 @@
+<?php // $Id$
+/**
+ * Student Report for Claroline
+ *
+ * @version     UCREPORT 1.0.1 $Revision$ - Claroline 1.9
+ * @copyright   2001-2010 Universite catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package     UCREPORT
+ * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
+ */
+
+$_lang['blockReportHelp'] = '<h1>Help for Report Tool</h1>
+<h2>Coming soon...</h2>';
