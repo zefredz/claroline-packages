c30137a46ae95aafc9bea1aff5dfc58eb23eab60
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2010-10-25 13:28:22 +0000

    applet must be only activated in course context

--- trunk/modules/GRPPLAP/functions.php
+++ trunk/modules/GRPPLAP/functions.php
@@ -2,16 +2,17 @@
 
 if ( count( get_included_files() ) == 1 ) die( '---' );
 
-require_once( get_module_path( 'GRPPLAP' ) . '/lib/grapple.listener.class.php' );
-require_once( get_module_path( 'GRAPPLE' ) . '/lib/grapple.class.php' );
-
-$grappleListener = new grappleListener();
-
-$grappleListener->addListener( 'user_login', 'userLogin' );
-
-$grappleListener->addListener( 'user_added_to_course', 'studentEnrollment' );
-$grappleListener->addListener( 'user_role_change', 'roleChange' );
-
-$grappleListener->addListener( 'user_created', 'userRegistration' );
-
-?>
+if ( claro_is_in_a_course() )
+{
+    require_once( get_module_path( 'GRPPLAP' ) . '/lib/grapple.listener.class.php' );
+    require_once( get_module_path( 'GRAPPLE' ) . '/lib/grapple.class.php' );
+    
+    $grappleListener = new grappleListener();
+    
+    $grappleListener->addListener( 'user_login', 'userLogin' );
+    
+    $grappleListener->addListener( 'user_added_to_course', 'studentEnrollment' );
+    $grappleListener->addListener( 'user_role_change', 'roleChange' );
+    
+    $grappleListener->addListener( 'user_created', 'userRegistration' );
+}
