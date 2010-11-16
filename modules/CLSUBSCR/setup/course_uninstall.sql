0995de288c0a91afebdb2ac14384963c86336e57
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2010-11-16 12:16:13 +0000

    fix SQL syntax error in course_uninstall : missing "`"

--- modules/CLSUBSCR/setup/course_uninstall.sql
+++ modules/CLSUBSCR/setup/course_uninstall.sql
@@ -3,5 +3,5 @@
 
 DROP TABLE IF EXISTS `__CL_COURSE__subscr_sessions`;
 DROP TABLE IF EXISTS `__CL_COURSE__subscr_slots`;
-DROP TABLE IF EXISTS `__CL_COURSE__subscr_subscribers;
+DROP TABLE IF EXISTS `__CL_COURSE__subscr_subscribers`;
 DROP TABLE IF EXISTS `__CL_COURSE__subscr_slots_subscribers`;
