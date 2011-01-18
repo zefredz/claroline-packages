aafbe0626edccbca88d90f1b29b024997d088fad
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-01-18 16:13:24 +0000

    -added : plugin for CLQPOLL -fixed: division by zero error when $itemStats[ 'clfrm_count_topics' ]['value'] == 0 in CLFRM plugin

---
+++ trunk/modules/CLSTATS/plugins/CLQPOLL.stats.php
@@ -0,0 +1,62 @@
+<?php
+
+class CLQPOLL_Stats extends ClaroStats_CourseTask
+{
+    public function getLabel()
+    {
+        return 'CLQPOLL';
+    }
+    
+    public function getData($course)
+    {
+        $tables = $this->getCourseTables(array('poll_polls','poll_choices','poll_votes'), $course);
+        
+        $resPolls = Claroline::getDatabase()->query("SELECT COUNT(*) AS `value` FROM `{$tables['poll_polls']}`");
+        
+        $clfrm_count_polls = $resPolls->fetch();
+        
+        $resVotes = Claroline::getDatabase()->query("SELECT COUNT(DISTINCT `V`.`user_id`) AS `value` FROM `{$tables['poll_votes']}` AS `V`
+                                                    LEFT JOIN `{$tables['poll_polls']}` AS `P`
+                                                    ON `P`.`id` = `V`.`poll_id`");
+        
+        $clfrm_count_votes = $resVotes->fetch();
+        
+        return array(
+            'clfrm_count_polls' => $clfrm_count_polls['value'],
+            'clfrm_count_votes' => $clfrm_count_votes['value']
+        );
+    }
+    
+    public function getReportData( &$report, $itemStats, $nbCourses = 0 )
+    {
+        foreach( $itemStats as $itemName => $item )
+        {
+            parent::initReportData( $report, $itemName, $item );
+            parent::setReportData( $report, $itemName, $item );
+            parent::setReportMax( $report, $itemName, $item );
+            //parent::setReportAverage( $report, $itemName, $item, $nbCourses );
+        }
+        
+        return $itemStats[ 'clfrm_count_polls' ]['value'] != 0
+               ? round( $itemStats[ 'clfrm_count_votes' ]['value'] / $itemStats[ 'clfrm_count_polls' ]['value'] )
+               : 0;
+    }
+    
+    public function getSummarizedReport( $items )
+    {
+        if(isset( $items['clfrm_count_votes' ] ) )
+        {
+            $items['clfrm_count_votes']['lessFive'] = $items['clfrm_count_votes']['zero']
+                                                            + $items['clfrm_count_votes']['one']
+                                                            + $items['clfrm_count_votes']['two']
+                                                            + $items['clfrm_count_votes']['three']
+                                                            + $items['clfrm_count_votes']['four'];
+            $items['clfrm_count_votes']['moreFive'] += $items['clfrm_count_votes']['five'];
+            return $items['clfrm_count_votes' ];
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
