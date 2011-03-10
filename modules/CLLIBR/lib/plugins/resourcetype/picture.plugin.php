1bbf6a98e948ba0578914c9561114054dcd3025c
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-03-10 08:57:41 +0000

    - changing dates 2010->2011 in files headers - remove a var_dump in storedresource.lib.php

--- modules/CLLIBR/lib/plugins/resourcetype/picture.plugin.php
+++ modules/CLLIBR/lib/plugins/resourcetype/picture.plugin.php
@@ -3,7 +3,7 @@
  * Online library for Claroline
  *
  * @version     CLLIBR 0.2.6 $Revision$ - Claroline 1.9
- * @copyright   2001-2010 Universite catholique de Louvain (UCL)
+ * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
  * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
--- modules/CLLIBR/lib/resourceset.lib.php
+++ modules/CLLIBR/lib/resourceset.lib.php
@@ -3,7 +3,7 @@
  * Online library for Claroline
  *
  * @version     CLLIBR 0.2.8 $Revision$ - Claroline 1.9
- * @copyright   2001-2010 Universite catholique de Louvain (UCL)
+ * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
  * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
@@ -32,7 +32,7 @@ abstract class ResourceSet
     {
         $this->database = $database;
         $this->tbl = get_module_main_tbl( array( 'library_resource'
-                                                   , 'library_resource_set' ) );
+                                               , 'library_resource_set' ) );
         
         self::$_type = strtolower( get_class( $this ) );
         
