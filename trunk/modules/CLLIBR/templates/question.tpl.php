185812270915b0a060d96e5b23a75cc772632ee6
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-04-13 13:34:09 +0000

    use <a><input type="button"> instead of claro_html_button

--- trunk/modules/CLLIBR/templates/question.tpl.php
+++ trunk/modules/CLLIBR/templates/question.tpl.php
@@ -10,10 +10,17 @@
  */ ?>
 
 <p><?php echo $this->msg ?></p>
+
 <form method="post" action="<?php echo htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'] . '?cmd=' . $this->urlAction ) ); ?>" >
+
 <?php foreach( $this->xid as $xid => $id ) : ?>
+
     <input type="hidden" name="<?php echo $xid; ?>" value="<?php echo $id; ?>" />
+
 <?php endforeach; ?>
+
     <input type="submit" name="" value="<?php echo get_lang( 'Yes' ); ?>" />
-    <?php echo claro_html_button( htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'] . '?cmd=' . $this->urlCancel ) ) , get_lang( 'Cancel' ) ); ?>
+    <a href="<?php echo  htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'] . '?cmd=' . $this->urlCancel ) );?>">
+        <input type="button" name="cancel" value="<?php echo get_lang( 'Cancel' );?>" />
+    </a>
 </form>
