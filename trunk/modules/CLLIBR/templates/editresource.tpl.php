7b71a637eb3e207448a9699c4c0329915dce7cd0
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-04-20 16:27:22 +0000

    access to libraries forbidden for anonymous user

--- trunk/modules/CLLIBR/templates/editresource.tpl.php
+++ trunk/modules/CLLIBR/templates/editresource.tpl.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.3.4 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.4.0 $Revision$ - Claroline 1.9
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
