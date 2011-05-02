e3fb6790bd2e7016d3792c31c4d84e31af6e6745
dkp1060 <dkp1060@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-05-02 10:00:07 +0000

    FixFix install CLATT module

--- trunk/modules/CLATT/setup/course_install.sql
+++ trunk/modules/CLATT/setup/course_install.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS `__CL_COURSE__attendance` (
   `id_list` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `attendance` enum('present','absent','late','excused') NOT NULL,
+  `attendance` enum('present','partial','absent','late','excused') NOT NULL,
   `comment` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`id_list`,`user_id`) USING BTREE
-) ENGINE=MyISAM;
+  PRIMARY KEY (`id_list`,`user_id`)
+) TYPE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `__CL_COURSE__attendance_session` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_att` datetime NOT NULL,
   `title` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1;
+) TYPE=MyISAM  AUTO_INCREMENT=1;
