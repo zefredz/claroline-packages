79062503bb6b6b24d0f80eeaf70477ea6c3f589c
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-05-02 12:26:58 +0000

    phpdoc + copyright

--- trunk/modules/CLSUBSCR/lib/groupchooser.lib.php
+++ trunk/modules/CLSUBSCR/lib/groupchooser.lib.php
@@ -1,4 +1,14 @@
-<?php
+<?php // $Id$
+/**
+ * User group chooser block class
+ *
+ * @version 0.2 $Revision$
+ * @copyright (c) 2011 Universite catholique de Louvain (UCL)
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package CLSUBSCR
+ * @author Frederic Minne <zefredz@claroline.net>
+ *
+ */
 
 FromKernel::uses('group.lib.inc');
 
@@ -10,6 +20,12 @@ class UserGroupChooser implements Display
         $courseId
         ;
     
+    /**
+     * Create a HTML group chooser for a user in a course
+     * @param type $url base url 
+     * @param type $userId user id
+     * @param type $courseId course code (sys code)
+     */
     public function __construct( $url = null, $userId = null, $courseId = null )
     {
         $this->url = (empty($url))
@@ -28,6 +44,10 @@ class UserGroupChooser implements Display
         ;
     }
     
+    /**
+     * Render the group chooser
+     * @return type string
+     */
     public function render()
     {
         $userGroupList = get_user_group_list(
