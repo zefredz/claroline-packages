66b4adbd64dd007fccf4e60e821f8a57fbf88a06
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-05-03 07:07:53 +0000

    error in config check for autoplay and autoload (false in PHP was interpreted as true in Javascript... I think I was a bit tired when I wrote that xD)

--- trunk/modules/ICPCRDR/templates/podcastdisplay.tpl.php
+++ trunk/modules/ICPCRDR/templates/podcastdisplay.tpl.php
@@ -18,29 +18,28 @@
     
     foreach( $this->items as $item ):
 ?>
-<h4 class="itemTitle"><?php echo htmlspecialchars( claro_utf8_decode($item->metadata['title']) ); ?></h4>
-<a href="<?php echo htmlspecialchars( $item->enclosure['url'] );?>">
-    <img src="<?php echo get_icon_url('download'); ?>" alt="download" />
-    <?php echo get_lang( 'Download this video' ); ?>
-</a>
-<p class="itemPubDate"><?php echo htmlspecialchars( $item->metadata['pubDate'] ); ?></p>
-<p class="itemDescription"><?php echo claro_utf8_decode(strip_tags($item->metadata['description'])); ?></p>
-<a  
-    href="<?php echo htmlspecialchars($item->enclosure['url']); ?>"  
-    style="display:block;width:400px;height:300px"  
-    id="player<?php echo "_{$videoId}"?>"> 
-</a>
-<script type="text/javascript">
-    flowplayer( "player<?php echo "_{$videoId}"?>", "./flash/flowplayer-3.2.5.swf", { 
-        clip: { 
-            // these two configuration variables does the trick 
-            autoPlay: <?php echo get_conf( 'flowplayer_autoPlay', false ) ? 'false' : 'true'; ?>,
-            autoBuffering: <?php echo get_conf( 'flowplayer_autoBuffering', false ) ? 'false' : 'true'; ?>
-        } 
-    } );
-</script>
+        <h4 class="itemTitle"><?php echo htmlspecialchars( claro_utf8_decode($item->metadata['title']) ); ?></h4>
+        <a href="<?php echo htmlspecialchars( $item->enclosure['url'] );?>">
+            <img src="<?php echo get_icon_url('download'); ?>" alt="download" />
+            <?php echo get_lang( 'Download this video' ); ?>
+        </a>
+        <p class="itemPubDate"><?php echo htmlspecialchars( $item->metadata['pubDate'] ); ?></p>
+        <p class="itemDescription"><?php echo claro_utf8_decode(strip_tags($item->metadata['description'])); ?></p>
+        <a
+            href="<?php echo htmlspecialchars($item->enclosure['url']); ?>"
+            style="display:block;width:400px;height:300px"
+            id="player<?php echo "_{$videoId}"?>">
+        </a>
+        <script type="text/javascript">
+            flowplayer( "player<?php echo "_{$videoId}"?>", "./flash/flowplayer-3.2.5.swf", {
+                clip: {
+                    autoPlay: <?php echo get_conf( 'flowplayer_autoPlay', false ) ? 'true' : 'false'; ?>,
+                    autoBuffering: <?php echo get_conf( 'flowplayer_autoBuffering', false ) ? 'true' : 'false'; ?>
+                } 
+            } );
+        </script>
 <?php
-    $videoId++;
+        $videoId++;
     
     endforeach;
 ?>
