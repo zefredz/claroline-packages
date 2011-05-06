cae282fc12ba03fc88d480398c7c745e03ce3cd8
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-05-06 13:20:45 +0000

    update copyright + replace private with protected + make public functions public + format code

--- trunk/modules/CLPAGES/lib/plugins/flickr.class.php
+++ trunk/modules/CLPAGES/lib/plugins/flickr.class.php
@@ -1,20 +1,16 @@
 <?php // $Id$
-if ( count( get_included_files() ) == 1 ) die( '---' );
+
 /**
  * CLAROLINE
  *
  * $Revision$
- *
- * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
- *
+ * @copyright (c) 2001-2011 Universite catholique de Louvain (UCL)
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- *
- * @package CLAUTHOR
- *
+ * @package CLPAGES
  * @author Claroline team <info@claroline.net>
- *
  */
-    // vim: expandtab sw=4 ts=4 sts=4 foldmethod=marker:
+// vim: expandtab sw=4 ts=4 sts=4 foldmethod=marker:
+
 /*
   <item>
 			<title>P1010025</title>
@@ -46,317 +42,315 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
 
 		</item>
  */
-    class FlickrComponent extends Component
+class FlickrComponent extends Component
+{
+    protected $tag = '';
+    protected $limit = 5;
+
+    /**
+     * @see Component
+     */
+    public function render()
     {
-    	private $tag = '';
-    	private $limit = 5;
+        if( !empty($this->tag) )
+        {
+            require_once get_path('incRepositorySys') . '/lib/lastRSS/lastRSS.php';
 
-		/**
-		 * @see Component
-		 */
-    	function render()
-    	{
-    		if( !empty($this->tag) )
-    		{
-				require_once get_path('incRepositorySys') . '/lib/lastRSS/lastRSS.php';
+            $out = '';
+            $feed = $this->getFeed( 'http://www.flickr.com/services/feeds/photos_public.gne?tags='.urlencode($this->tag).'&format=rss_200' );
 
-				$out = '';
-				$feed = $this->getFeed( 'http://www.flickr.com/services/feeds/photos_public.gne?tags='.urlencode($this->tag).'&format=rss_200' );
+            if( !empty($feed) )
+            {
+                if( isset($feed['rss']['#']['channel'][0]['#']['item']) )
+                {
+                    $items = $feed['rss']['#']['channel'][0]['#']['item'];
+                }
+                else
+                {
+                    $items = array();
+                }
 
-				if( !empty($feed) )
-				{
-					if( isset($feed['rss']['#']['channel'][0]['#']['item']) )
-					{
-						$items = $feed['rss']['#']['channel'][0]['#']['item'];
-					}
-					else
-					{
-						$items = array();
-					}
+                if( is_array($items) && !empty($items) )
+                {
+                    $limit = $this->limit;
+                    $i = 0;
 
-					if( is_array($items) && !empty($items) )
-					{
-				    	$limit = $this->limit;
-				    	$i = 0;
+                    foreach( $items as $item )
+                    {
+                        $thumbUrl = claro_utf8_decode($item['#']['media:thumbnail'][0]['@']['url']);
 
-					    foreach( $items as $item )
-					    {
-					    	$thumbUrl = claro_utf8_decode($item['#']['media:thumbnail'][0]['@']['url']);
-
-					    	if( !empty($thumbUrl) )
-					    	{
-						    	$thumbHeight = claro_utf8_decode($item['#']['media:thumbnail'][0]['@']['height']);
-						    	$thumbWidth = claro_utf8_decode($item['#']['media:thumbnail'][0]['@']['width']);
+                        if( !empty($thumbUrl) )
+                        {
+                            $thumbHeight = claro_utf8_decode($item['#']['media:thumbnail'][0]['@']['height']);
+                            $thumbWidth = claro_utf8_decode($item['#']['media:thumbnail'][0]['@']['width']);
 
 
-						    	if( !empty($thumbHeight) ) $height = 'height="'.htmlspecialchars($thumbHeight).'"';
-				    			else						$height = '';
+                            if( !empty($thumbHeight) ) $height = 'height="'.htmlspecialchars($thumbHeight).'"';
+                            else						$height = '';
 
-				    			if( !empty($thumbWidth) ) $width = 'width="'.htmlspecialchars($thumbWidth).'"';
-				    			else						$width = '';
+                            if( !empty($thumbWidth) ) $width = 'width="'.htmlspecialchars($thumbWidth).'"';
+                            else						$width = '';
 
-								$link = claro_utf8_decode($item['#']['link'][0]['#']);
+                            $link = claro_utf8_decode($item['#']['link'][0]['#']);
 
-						        if( $i < $limit )
-						        {
-						            $out .= '<div style="float: left; border:1px solid #ccc; padding: 3px; margin: 5px;">' . "\n"
-						            .	 '<a href="'.htmlspecialchars($link).'" >'
-						    		.	 '<img src="'.htmlspecialchars($thumbUrl).'" '.$height.' '.$width.' alt="" />'
-						    		.	 '</a>' . "\n"
-						    		.	 '</div>' . "\n\n";
-						            $i++;
-						        }
-						        else
-						        {
-						            break;
-						        }
-					    	}
-					    } // foreach
-						$out .= '<div class="spacer"></div>';
-					} // is array
-					else
-					{
-						$out .= '<p>' . get_lang('Problem reading feed.') . '</p>' . "\n";
-					}
-				}
-				else
-				{
-				    if( claro_is_allowed_to_edit() )
-				    {
-				        $out .= '<p>' . get_lang('Error : cannot read RSS feed (Check that feed is accessible or ask your administrator if php setting "allow_url_fopen" is turned on).') . '</p>' . "\n";
-				    }
-				}
+                            if( $i < $limit )
+                            {
+                                $out .= '<div style="float: left; border:1px solid #ccc; padding: 3px; margin: 5px;">' . "\n"
+                                .	 '<a href="'.htmlspecialchars($link).'" >'
+                                .	 '<img src="'.htmlspecialchars($thumbUrl).'" '.$height.' '.$width.' alt="" />'
+                                .	 '</a>' . "\n"
+                                .	 '</div>' . "\n\n";
+                                $i++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    } // foreach
+                    $out .= '<div class="spacer"></div>';
+                } // is array
+                else
+                {
+                    $out .= '<p>' . get_lang('Problem reading feed.') . '</p>' . "\n";
+                }
+            }
+            else
+            {
+                if( claro_is_allowed_to_edit() )
+                {
+                    $out .= '<p>' . get_lang('Error : cannot read RSS feed (Check that feed is accessible or ask your administrator if php setting "allow_url_fopen" is turned on).') . '</p>' . "\n";
+                }
+            }
 
-				return $out;
-    		}
-    		else
-    		{
-    			return '' . "\n";
-    		}
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function editor()
-    	{
-    		// use content in textarea
-    		return '<label for="tag_'.$this->getId().'">' . get_lang('Tag') . '</label>&nbsp;<span class="required">*</span><br />' . "\n"
-    		.	 '<input type="text" name="tag_'.$this->getId().'" id="tag_'.$this->getId().'" maxlength="255" value="'.htmlspecialchars($this->tag).'" /><br />' . "\n"
-    		.	 '<label for="limit_'.$this->getId().'">' . get_lang('Number of displayed items ') . '</label>&nbsp;<span class="required">*</span><br />' . "\n"
-    		.	 '<input type="text" name="limit_'.$this->getId().'" id="limit_'.$this->getId().'" size="5" maxlength="10" value="'.htmlspecialchars($this->limit).'" /><br />' . "\n"
-    		;
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function getEditorData()
-    	{
-    		$this->tag = $this->getFromRequest('tag_'.$this->getId());
-    		$this->limit = $this->getFromRequest('limit_'.$this->getId());
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function setData( $data )
-    	{
-  			$this->tag = $data['tag'];
-  			$this->limit = $data['limit'];
-    	}
-
-		/**
-		 * @see Component
-		 */
-    	function getData()
-    	{
-    		return array(
-				'tag' => $this->tag,
-				'limit' => $this->limit
-			);
-    	}
-
-    	// this code is the same as Get method of lastRSS library
-    	// this way cache can be shared between the two uses
-    	private function getFeed($rss_url) {
-
-			// configure parser
-			$cache_dir = get_path('rootSys') . '/tmp/cache/';
-			$cache_time = 3600;
-
-			// If CACHE ENABLED
-			if ($cache_dir != '') {
-				$cache_file = $cache_dir . '/rsscache_' . md5($rss_url);
-				$timedif = @(time() - filemtime($cache_file));
-				if ($timedif < $cache_time) {
-					// cached file is fresh enough, return cached array
-					$result = unserialize(join('', file($cache_file)));
-					// set 'cached' to 1 only if cached file is correct
-					if ($result) $result['cached'] = 1;
-				} else {
-					// cached file is too old, create new
-					$result = $this->parseFeed($rss_url);
-					$serialized = serialize($result);
-					if ($f = @fopen($cache_file, 'w')) {
-						fwrite ($f, $serialized, strlen($serialized));
-						fclose($f);
-					}
-					if ($result) $result['cached'] = 0;
-				}
-			}
-			// If CACHE DISABLED >> load and parse the file directly
-			else {
-				$result = $this->parseFeed($rss_url);
-				if ($result) $result['cached'] = 0;
-			}
-			// return result
-			return $result;
-		}
-
-		private function parseFeed($rss_url)
-		{
-			if ($f = @fopen($rss_url, 'r'))
-			{
-				$rss_content = '';
-				while (!feof($f))
-				{
-					$rss_content .= fgets($f, 4096);
-				}
-				fclose($f);
-
-				$content = xmlize($rss_content);
-
-				if( is_array($content) )
-				{
-					return $content;
-				}
-				else
-				{
-					return array();
-				}
-			}
-    	}
-
+            return $out;
+        }
+        else
+        {
+            return '' . "\n";
+        }
     }
 
-    PluginRegistry::register('flickr',get_lang('Flickr gallery'),'FlickrComponent', 'Externals', 'flickrIco');
-
-
-
-    if( !function_exists('xmlize') )
+    /**
+     * @see Component
+     */
+    public function editor()
     {
-		function xmlize($data, $WHITE=1) {
-
-		    $data = trim($data);
-		    $vals = $index = $array = array();
-		    $parser = xml_parser_create();
-		    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-		    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, $WHITE);
-		    if ( !xml_parse_into_struct($parser, $data, $vals, $index) )
-		    {
-			    return sprintf("XML error: %s at line %d",
-		                    xml_error_string(xml_get_error_code($parser)),
-		                    xml_get_current_line_number($parser));
-
-		    }
-		    xml_parser_free($parser);
-
-		    $i = 0;
-
-		    $tagname = $vals[$i]['tag'];
-		    if ( isset ($vals[$i]['attributes'] ) )
-		    {
-		        $array[$tagname]['@'] = $vals[$i]['attributes'];
-		    } else {
-		        $array[$tagname]['@'] = array();
-		    }
-
-		    $array[$tagname]["#"] = xml_depth($vals, $i);
-
-		    return $array;
-		}
+        // use content in textarea
+        return '<label for="tag_'.$this->getId().'">' . get_lang('Tag') . '</label>&nbsp;<span class="required">*</span><br />' . "\n"
+        .	 '<input type="text" name="tag_'.$this->getId().'" id="tag_'.$this->getId().'" maxlength="255" value="'.htmlspecialchars($this->tag).'" /><br />' . "\n"
+        .	 '<label for="limit_'.$this->getId().'">' . get_lang('Number of displayed items ') . '</label>&nbsp;<span class="required">*</span><br />' . "\n"
+        .	 '<input type="text" name="limit_'.$this->getId().'" id="limit_'.$this->getId().'" size="5" maxlength="10" value="'.htmlspecialchars($this->limit).'" /><br />' . "\n"
+        ;
     }
 
-
-
-	if( !function_exists('xml_depth') )
+    /**
+     * @see Component
+     */
+    public function getEditorData()
     {
-		function xml_depth($vals, &$i) {
-		    $children = array();
-
-		    if ( isset($vals[$i]['value']) )
-		    {
-		        array_push($children, $vals[$i]['value']);
-		    }
-
-		    while (++$i < count($vals)) {
-
-		        switch ($vals[$i]['type']) {
-
-		           case 'open':
-
-		                if ( isset ( $vals[$i]['tag'] ) )
-		                {
-		                    $tagname = $vals[$i]['tag'];
-		                } else {
-		                    $tagname = '';
-		                }
-
-		                if ( isset ( $children[$tagname] ) )
-		                {
-		                    $size = sizeof($children[$tagname]);
-		                } else {
-		                    $size = 0;
-		                }
-
-		                if ( isset ( $vals[$i]['attributes'] ) ) {
-		                    $children[$tagname][$size]['@'] = $vals[$i]["attributes"];
-		                }
-
-		                $children[$tagname][$size]['#'] = xml_depth($vals, $i);
-
-		            break;
-
-
-		            case 'cdata':
-		                array_push($children, $vals[$i]['value']);
-		            break;
-
-		            case 'complete':
-		                $tagname = $vals[$i]['tag'];
-
-		                if( isset ($children[$tagname]) )
-		                {
-		                    $size = sizeof($children[$tagname]);
-		                } else {
-		                    $size = 0;
-		                }
-
-		                if( isset ( $vals[$i]['value'] ) )
-		                {
-		                    $children[$tagname][$size]["#"] = $vals[$i]['value'];
-		                } else {
-		                    $children[$tagname][$size]["#"] = '';
-		                }
-
-		                if ( isset ($vals[$i]['attributes']) ) {
-		                    $children[$tagname][$size]['@']
-		                                             = $vals[$i]['attributes'];
-		                }
-
-		            break;
-
-		            case 'close':
-		                return $children;
-		            break;
-		        }
-
-		    }
-
-			return $children;
-
-		}
+        $this->tag = $this->getFromRequest('tag_'.$this->getId());
+        $this->limit = $this->getFromRequest('limit_'.$this->getId());
     }
 
-?>
+    /**
+     * @see Component
+     */
+    public function setData( $data )
+    {
+        $this->tag = $data['tag'];
+        $this->limit = $data['limit'];
+    }
+
+    /**
+     * @see Component
+     */
+    public function getData()
+    {
+        return array(
+            'tag' => $this->tag,
+            'limit' => $this->limit
+        );
+    }
+
+    // this code is the same as Get method of lastRSS library
+    // this way cache can be shared between the two uses
+    protected function getFeed($rss_url) {
+
+        // configure parser
+        $cache_dir = get_path('rootSys') . '/tmp/cache/';
+        $cache_time = 3600;
+
+        // If CACHE ENABLED
+        if ($cache_dir != '') {
+            $cache_file = $cache_dir . '/rsscache_' . md5($rss_url);
+            $timedif = @(time() - filemtime($cache_file));
+            if ($timedif < $cache_time) {
+                // cached file is fresh enough, return cached array
+                $result = unserialize(join('', file($cache_file)));
+                // set 'cached' to 1 only if cached file is correct
+                if ($result) $result['cached'] = 1;
+            } else {
+                // cached file is too old, create new
+                $result = $this->parseFeed($rss_url);
+                $serialized = serialize($result);
+                if ($f = @fopen($cache_file, 'w')) {
+                    fwrite ($f, $serialized, strlen($serialized));
+                    fclose($f);
+                }
+                if ($result) $result['cached'] = 0;
+            }
+        }
+        // If CACHE DISABLED >> load and parse the file directly
+        else {
+            $result = $this->parseFeed($rss_url);
+            if ($result) $result['cached'] = 0;
+        }
+        // return result
+        return $result;
+    }
+
+    protected function parseFeed($rss_url)
+    {
+        if ($f = @fopen($rss_url, 'r'))
+        {
+            $rss_content = '';
+            while (!feof($f))
+            {
+                $rss_content .= fgets($f, 4096);
+            }
+            fclose($f);
+
+            $content = xmlize($rss_content);
+
+            if( is_array($content) )
+            {
+                return $content;
+            }
+            else
+            {
+                return array();
+            }
+        }
+    }
+
+}
+
+PluginRegistry::register('flickr',get_lang('Flickr gallery'),'FlickrComponent', 'Externals', 'flickrIco');
+
+
+
+if( !function_exists('xmlize') )
+{
+    function xmlize($data, $WHITE=1) {
+
+        $data = trim($data);
+        $vals = $index = $array = array();
+        $parser = xml_parser_create();
+        xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+        xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, $WHITE);
+        if ( !xml_parse_into_struct($parser, $data, $vals, $index) )
+        {
+            return sprintf("XML error: %s at line %d",
+                        xml_error_string(xml_get_error_code($parser)),
+                        xml_get_current_line_number($parser));
+
+        }
+        xml_parser_free($parser);
+
+        $i = 0;
+
+        $tagname = $vals[$i]['tag'];
+        if ( isset ($vals[$i]['attributes'] ) )
+        {
+            $array[$tagname]['@'] = $vals[$i]['attributes'];
+        } else {
+            $array[$tagname]['@'] = array();
+        }
+
+        $array[$tagname]["#"] = xml_depth($vals, $i);
+
+        return $array;
+    }
+}
+
+
+
+if( !function_exists('xml_depth') )
+{
+    function xml_depth($vals, &$i) {
+        $children = array();
+
+        if ( isset($vals[$i]['value']) )
+        {
+            array_push($children, $vals[$i]['value']);
+        }
+
+        while (++$i < count($vals)) {
+
+            switch ($vals[$i]['type']) {
+
+               case 'open':
+
+                    if ( isset ( $vals[$i]['tag'] ) )
+                    {
+                        $tagname = $vals[$i]['tag'];
+                    } else {
+                        $tagname = '';
+                    }
+
+                    if ( isset ( $children[$tagname] ) )
+                    {
+                        $size = sizeof($children[$tagname]);
+                    } else {
+                        $size = 0;
+                    }
+
+                    if ( isset ( $vals[$i]['attributes'] ) ) {
+                        $children[$tagname][$size]['@'] = $vals[$i]["attributes"];
+                    }
+
+                    $children[$tagname][$size]['#'] = xml_depth($vals, $i);
+
+                break;
+
+
+                case 'cdata':
+                    array_push($children, $vals[$i]['value']);
+                break;
+
+                case 'complete':
+                    $tagname = $vals[$i]['tag'];
+
+                    if( isset ($children[$tagname]) )
+                    {
+                        $size = sizeof($children[$tagname]);
+                    } else {
+                        $size = 0;
+                    }
+
+                    if( isset ( $vals[$i]['value'] ) )
+                    {
+                        $children[$tagname][$size]["#"] = $vals[$i]['value'];
+                    } else {
+                        $children[$tagname][$size]["#"] = '';
+                    }
+
+                    if ( isset ($vals[$i]['attributes']) ) {
+                        $children[$tagname][$size]['@']
+                                                 = $vals[$i]['attributes'];
+                    }
+
+                break;
+
+                case 'close':
+                    return $children;
+                break;
+            }
+
+        }
+
+        return $children;
+
+    }
+}
