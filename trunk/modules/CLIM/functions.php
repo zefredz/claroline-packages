e211911094e7fdee35f03b55a60458ef716ddb30
dkp1060 <dkp1060@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-05-17 07:08:08 +0000

    New module for instant messaging (applet in each page) developped by Ait Moussa Abdelaziz

---
+++ trunk/modules/CLIM/functions.php
@@ -0,0 +1,44 @@
+<?php // $Id$
+/**
+ *
+ * @version 0.1 $Revision$
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @author Yassine Hassaine <yassinehassaine@gmail.com>
+ *
+ * @package CLIM
+ *
+ */
+if ( count( get_included_files() ) == 1 ) die( '---' );
+
+include_once claro_get_conf_repository().'CLIM.conf.php';
+include_once dirname(__FILE__) . '/lib/Login_listener.class.php';
+
+if( isset($GLOBALS['_uid']) )
+{
+    // record last action time only if authed
+    $tbl = claro_sql_get_tbl('user_online', array('course'=>null));
+
+    $sql = "DELETE FROM `" . $tbl['user_online'] . "`
+            WHERE `user_id`=" . (int) $GLOBALS['_uid'];
+
+    claro_sql_query($sql);
+
+    $sql = "INSERT INTO `" . $tbl['user_online'] . "`
+            SET `user_id` = " . (int) $GLOBALS['_uid'] . ",
+            `last_action` = '" . date('Y-m-d H:i:s') . "'";
+
+    claro_sql_query($sql);
+
+}
+
+// declare event manager dependencies and listener
+$claro_login_listener = new LoginListener;
+
+//set required event listener in the Claroline event manager
+$claro_login_listener->addListener( "user_logout", 'delete_login_online' );
+$claro_login_listener->addListener( "user_login", 'insert_login_online' );
+$claro_login_listener->refresh_login_DB();
+
+?>
---
+++ trunk/modules/CLIM/setup/install.sql
@@ -0,0 +1,19 @@
+# $Id$
+
+CREATE TABLE IF NOT EXISTS `__CL_MAIN__user_online` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL default '0',
+  `last_action` datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `chat` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `from` VARCHAR(255) NOT NULL DEFAULT '',
+  `to` VARCHAR(255) NOT NULL DEFAULT '',
+  `message` TEXT NOT NULL,
+  `sent` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `recd` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
