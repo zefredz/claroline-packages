954f67eb5359eaed0e86648dbaaa076d9073940c
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2011-05-18 09:35:44 +0000

    Should correct incompatibility bug between CLNEWMSG and ICTERMS

--- modules/CLNEWMSG/entry.php
+++ modules/CLNEWMSG/entry.php
@@ -1,3 +1,34 @@
 <?php // $Id$
 
+/**
+ * New message notifier
+ *
+ * @version     CLNEWMSG 0.8 $Revision$ - Claroline 1.9
+ * @copyright   2001-2009 Universite Catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package     CLNEWMSG
+ * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
+ */
+
+CssLoader::getInstance()->load( 'bubble' , 'screen' );
+
+if ( claro_is_user_authenticated() )
+{
+    ClaroHeader::getInstance()->addHtmlHeader( '
+        <script type="text/javascript">
+            function msgNotifier()
+            {
+                $.ajax({
+                    url: "' . get_module_url( 'CLNEWMSG' ) . '/index.php",
+                    success: function(data){
+                        $("#newMsg").html(data);
+                    }
+                });
+                setTimeout( msgNotifier, '. get_conf( 'refreshTime' ) * 1000 .' );
+            }
+            $( function(){ msgNotifier(); } );
+        </script>');
+}
+
 $claro_buffer->append( '<div id="newMsg"></div>' );
+
--- modules/CLNEWMSG/functions.php
+++ modules/CLNEWMSG/functions.php
@@ -1,13 +1,4 @@
 <?php // $Id$
-/**
- * New message notifier
- *
- * @version     CLNEWMSG 0.8 $Revision$ - Claroline 1.9
- * @copyright   2001-2009 Universite Catholique de Louvain (UCL)
- * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- * @package     CLNEWMSG
- * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
- */
 
 if ( ! claro_is_user_authenticated() )
 {
@@ -18,22 +9,3 @@ elseif ( ! isset( $_SESSION[ 'start_time' ] ) || ! $_SESSION[ 'start_time' ] )
     $_SESSION[ 'start_time' ] = time();
 }
 
-CssLoader::getInstance()->load( 'bubble' , 'screen' );
-
-if ( claro_is_user_authenticated() )
-{
-    ClaroHeader::getInstance()->addHtmlHeader( '
-        <script type="text/javascript">
-            function msgNotifier()
-            {
-                $.ajax({
-                    url: "' . get_module_url( 'CLNEWMSG' ) . '/index.php",
-                    success: function(data){
-                        $("#newMsg").html(data);
-                    }
-                });
-                setTimeout( msgNotifier, '. get_conf( 'refreshTime' ) * 1000 .' );
-            }
-            $( function(){ msgNotifier(); } );
-        </script>');
-}
