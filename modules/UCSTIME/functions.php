79c67785e9b9cbf36f5259dcd23ada5272a70227
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-05-19 16:23:32 +0000

    added date (todo :localised date)

--- modules/UCSTIME/functions.php
+++ modules/UCSTIME/functions.php
@@ -3,7 +3,7 @@
 * Server Time?
 *
 * @version     UCSTIME 1.1.1 $Revision$ - Claroline 1.9
-* @copyright   2001-2009 Universite Catholique de Louvain (UCL)
+* @copyright   2001-2011 Universite Catholique de Louvain (UCL)
 * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
 * @package     UCSTIME
 * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
@@ -11,7 +11,13 @@
 
 if ( count( get_included_files() ) == 1 ) die( '---' );
 
-$seconds_displayed = get_conf( 'displaySeconds' ) ? 'HH:mm:ss' : 'HH:mm';
+
+$string_displayed = get_conf( 'displaySeconds' ) ? 'HH:mm:ss' : 'HH:mm';
+
+if ( get_conf( 'displayDate' ) )
+{
+    $string_displayed = 'yyyy-MM-dd ' . $string_displayed;
+}
 
 JavascriptLoader::getInstance()->load( 'datetime' );
 JavascriptLoader::getInstance()->load( 'Date' );
@@ -28,7 +34,7 @@ ClaroHeader::getInstance()->addHtmlHeader( '
             var localDate = new Date();
             var serverDate = new Date();
             serverDate.setTime( localDate.getTime() - timeShift );
-            $( "#serverTime" ).html( serverDate.format( "' . $seconds_displayed .'" ) );
+            $( "#serverTime" ).html( serverDate.format( "' . $string_displayed .'" ) );
             setTimeout( serverTimeDisplay , 1000 );
         }
         $(function()
