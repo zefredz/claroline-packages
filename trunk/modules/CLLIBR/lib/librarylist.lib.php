0eccd7ad4e73e555f7cd2d1539b21d6a05cae282
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-06-29 13:59:10 +0000

    - fix move command - removed link to librarian management page in public library list

--- trunk/modules/CLLIBR/lib/librarylist.lib.php
+++ trunk/modules/CLLIBR/lib/librarylist.lib.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.7.0 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.7.1 $Revision$ - Claroline 1.9
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
@@ -37,6 +37,7 @@ class LibraryList
         
         $this->userId = $userId;
         $this->is_admin = $is_admin;
+        $this->load( true );
     }
     
     /**
--- trunk/modules/CLLIBR/templates/librarylist.tpl.php
+++ trunk/modules/CLLIBR/templates/librarylist.tpl.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.7.0 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.7.1 $Revision$ - Claroline 1.9
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
@@ -53,9 +53,11 @@
                 <td>
             <?php if ( ! empty( $library[ 'librarianList' ] ) ) : ?>
                 <?php echo implode( ', ' , $library[ 'librarianList' ] ); ?>
+                <?php if ( $this->is_platform_admin ) : ?>
                 <a href="<?php echo htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'].'?cmd=rqShowLibrarian&libraryId='. $libraryId ) );?>">
                     <img src="<?php echo get_icon_url( 'librarian' ); ?>" alt="<?php echo get_lang( 'Manage librarians' ); ?>"/>
                 </a>
+                <?php endif; ?>
             <?php else : ?>
                     -
             <?php endif; ?>
