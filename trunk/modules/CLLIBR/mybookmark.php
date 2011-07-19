74cef7d1b7b395a7461d8179551780fc2a3961f9
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-07-19 10:30:42 +0000

    'old fashioned' menu only displays in porlet

--- trunk/modules/CLLIBR/mybookmark.php
+++ trunk/modules/CLLIBR/mybookmark.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.3.3 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.8.7 $Revision$ - Claroline 1.9
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
@@ -30,6 +30,7 @@ $userId = claro_get_current_user_id();
 $myBookmark = new Collection( Claroline::getDatabase() , 'bookmark' , $userId );
 
 $portlet = new ModuleTemplate( 'CLLIBR' , 'bookmark.tpl.php' );
+$portlet->assign( 'in_portlet' , true );
 $portlet->assign( 'resourceList' , $myBookmark->getResourceList() );
 $portlet->assign( 'userId' , $userId );
 $portlet->assign( 'icon' , get_path( 'rootWeb' ) . 'module/CLLIBR/icon.png' );
