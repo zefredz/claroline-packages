7252f156e087615a681f0f0903cb38151a822fb3
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-08-08 13:44:13 +0000

    added new actions on multi-selection

--- trunk/modules/CLLIBR/templates/bookmark.tpl.php
+++ trunk/modules/CLLIBR/templates/bookmark.tpl.php
@@ -9,6 +9,8 @@
  * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
  */ ?>
 
+<?php include( 'selector.tpl.php' ); ?>
+
 <?php if ( isset( $this->in_portlet ) ) : ?>
 <span> 
     <a class="claroCmd" href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqShowLibrarylist' ) ); ?>">
@@ -17,52 +19,74 @@
     </a>
 </span>
 <?php endif; ?>
-<table class="claroTable emphaseLine" style=" width: 100%;">
-    <thead>
-        <tr class="headerX">
-            <th>
-                <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqShowBookmark' ) ); ?>">
-                <?php echo get_lang( 'Title'); ?>
-                </a>
-            </th>
-            <th>
-                <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqShowBookmark&sort=author' ) ); ?>">
-                <?php echo get_lang( 'Author(s)' ); ?>
-                </a>
-            </th>
-            <th>
-                <?php echo get_lang( 'Actions' ); ?>
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-<?php if ( ! empty( $this->resourceList ) ) : ?>
-    <?php foreach ( $this->resourceList as $resource ) : ?>
-        <tr>
-            <td>
-                <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqView&resourceId='. $resource[ 'id' ] ) );?>">
-                    <?php echo $resource[ 'title' ]; ?>
-                </a>
-            </td>
-            <td> 
-                <?php echo $resource[ 'author' ]; ?>
-            </td>
-            <td align="center">
-                <!--
-                <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=exExport&resourceId='. $resource[ 'id' ] ) );?>">
-                    <img src="<?php echo get_icon_url( 'export' ); ?>" alt="<?php echo get_lang( 'Export' ); ?>"/>
-                </a>
-                -->
-                <a title="<?php echo get_lang( 'Remove' ); ?>" href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=exUnbookmark&resourceId='. $resource[ 'id' ] ) );?>">
-                    <img src="<?php echo get_icon_url( 'delete' ); ?>" alt="<?php echo get_lang( 'Remove' ); ?>"/>
-                </a>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-<?php else: ?>
-        <tr>
-            <td style="text-align: center; font-style: italic; color: silver;" colspan="5"><?php echo get_lang( 'Empty bookmark' ); ?></td>
-        </tr>
-<?php endif; ?>
-    </tbody>
-</table>
+<form method="post"
+      action="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php' ) );?>">
+    <table class="claroTable emphaseLine" style=" width: 100%;">
+        <thead>
+            <tr class="headerX">
+                <th>
+                    <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqShowBookmark' ) ); ?>">
+                    <?php echo get_lang( 'Title'); ?>
+                    </a>
+                </th>
+                <th>
+                    <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqShowBookmark&sort=author' ) ); ?>">
+                    <?php echo get_lang( 'Author(s)' ); ?>
+                    </a>
+                </th>
+            <?php if ( count( $this->resourceList ) > 1 ) : ?>
+                <th>
+                    <input id="selectAll" type="checkbox" />
+                </th>
+            <?php endif; ?>
+                <th>
+                    <?php echo get_lang( 'Actions' ); ?>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+    <?php if ( ! empty( $this->resourceList ) ) : ?>
+        <?php foreach ( $this->resourceList as $resource ) : ?>
+            <tr>
+                <td>
+                    <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=rqView&resourceId='. $resource[ 'id' ] ) );?>">
+                        <?php echo $resource[ 'title' ]; ?>
+                    </a>
+                </td>
+                <td> 
+                    <?php echo $resource[ 'author' ]; ?>
+                </td>
+            <?php if ( count( $this->resourceList ) > 1 ) : ?>
+                <td align="center">
+                    <input class="resourceSelect" type="checkbox" name="resource[<?php echo $resource[ 'id' ]; ?>]" />
+                </td>
+            <?php endif; ?>
+                <td align="center">
+                    <!--
+                    <a href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=exExport&resourceId='. $resource[ 'id' ] ) );?>">
+                        <img src="<?php echo get_icon_url( 'export' ); ?>" alt="<?php echo get_lang( 'Export' ); ?>"/>
+                    </a>
+                    -->
+                    <a title="<?php echo get_lang( 'Remove' ); ?>" href="<?php echo htmlspecialchars( Url::Contextualize( get_module_url( 'CLLIBR' ) .'/index.php?cmd=exUnbookmark&resourceId='. $resource[ 'id' ] ) );?>">
+                        <img src="<?php echo get_icon_url( 'delete' ); ?>" alt="<?php echo get_lang( 'Remove' ); ?>"/>
+                    </a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else: ?>
+            <tr>
+                <td style="text-align: center; font-style: italic; color: silver;" colspan="5"><?php echo get_lang( 'Empty bookmark' ); ?></td>
+            </tr>
+    <?php endif; ?>
+        </tbody>
+    </table>
+    <div id="onSelected">
+        <span class="claroCmd"><?php echo get_lang( 'On selected items' ); ?>:</span>
+        <select name="cmd">
+        <?php if ( $this->userId ) : ?>
+            <option value="exUnbookmark"><?php echo get_lang( 'Remove from my bookmark' ); ?></option>
+        <?php endif; ?>
+        </select>
+        <input id="submit" type="submit" name="submit" value="<?php echo get_lang( 'OK' ); ?>" />
+    </div>
+</form>
