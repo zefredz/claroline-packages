adc824c7502cea1cfdffb64531a01aa23c3aebca
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-09-14 11:30:34 +0000

    french localization complete

--- trunk/modules/CLPAGES/lang/lang_french.php
+++ trunk/modules/CLPAGES/lang/lang_french.php
@@ -2,16 +2,38 @@
 //Page
 $_lang['Add a composant'] = 'Ajouter un composant';
 $_lang['Are you sure to delete page "%pageTitle" ?'] = 'Etes-vous sûr de vouloir supprimer "%pageTitle" ?';
+$_lang['Big'] = 'Grand';
+$_lang['Claroline S5 viewer'] = 'Claroline S5 viewer';
+$_lang['Click to see your slide preview'] = 'Cliquez ici pour l\'aperçu de votre diapporama';
 $_lang['Create a new page'] = 'Créer une nouvelle page';
 $_lang['Creation date'] = 'Date de création';
+$_lang['Deprecated plugin, contact administrator'] = 'Plugon obsolète, contactez votre administateur';
+$_lang['Edit page settings'] = 'Modifier les paramètres de la page';
 $_lang['Empty page successfully created'] = 'Votre nouvelle page a été crée avec succès';
+$_lang['Error : cannot read RSS feed (Check that feed is accessible or ask your administrator if php setting "allow_url_fopen" is turned on).'] = 'Erreur à la lecture du flux RSS (Vérifier que l\'adresse du flux est correcte)';
+$_lang['Fatal error : cannot delete'] = 'Erreur fatale : impossible de supprimer cet élément';
+$_lang['Image'] = 'Image';
+$_lang['Layout :'] = 'Mise en page :';
+$_lang['Loading...'] = 'Chargement en cours...';
 $_lang['No pages'] = 'Aucune page';
+$_lang['Not a correct and valid video Url'] = 'L\'Url de cette vidéo est incorrecte ou invalide';
 $_lang['One page'] = 'Page unique';
 $_lang['Page successfully deleted'] ='Votre page a été supprimée';
 $_lang['Page successfully modified'] = 'Votre page a été modifiée avec succès';
 $_lang['Pages'] = 'Pages';
+$_lang['Problem reading feed.'] = 'Problème lors de la lecture du flux RSS';
+$_lang['Rss'] = 'Rss';
 $_lang['Run the slides show'] = 'Lancer la présentation des slides';
 $_lang['Slideshow'] = 'Diaporama';
+$_lang['Tag'] = 'Tag';
+$_lang['Text A'] = 'Texte A';
+$_lang['Text B'] = 'Texte B';
+$_lang['Url'] = 'Url';
+$_lang['Url of rss feed'] = 'Url ou flux RSS';
+$_lang['Video id'] = 'id de la vidéo';
+$_lang['Video not configured'] = 'Vidéo non configurée';
+$_lang['Video reference'] = 'Référence de la vidéo';
+$_lang['Video size'] = 'Taille de la vidéo';
 
 //PLUGINS
 $_lang['External website'] = 'Site web externe';
