95264da7fbba5244007ed9346fa92cc798ddfc0d
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-09-14 11:00:50 +0000

    added missing lang var

--- modules/CLPAGES/lang/lang_english.php
+++ modules/CLPAGES/lang/lang_english.php
@@ -1,26 +1,56 @@
 <?php
 //Page
-$_lang['Run the slides show'] ='Run the slides show';
+$_lang['Add a composant'] = 'Add a composant';
+$_lang['Are you sure to delete page "%pageTitle" ?'] = 'Are you sure to delete page "%pageTitle" ?';
+$_lang['Create a new page'] = 'Create a new page';
+$_lang['Creation date'] = 'Creation date';
+$_lang['Empty page successfully created'] = 'Empty page successfully created';
+$_lang['No pages'] = 'No pages';
+$_lang['One page'] = 'One page';
+$_lang['Page successfully deleted'] ='Page successfully deleted';
+$_lang['Page successfully modified'] = 'Page successfully modified';
+$_lang['Pages'] = 'Pages';
+$_lang['Run the slides show'] = 'Run the slides show';
+$_lang['Slideshow'] = 'Slideshow';
 
 //PLUGINS
+$_lang['External website'] = 'External website';
+$_lang['Flickr gallery'] = 'Flickr gallery';
+$_lang['Text'] = 'Text';
+$_lang['Text and file'] = 'Text and file';
+$_lang['Text and image'] = 'Text and image';
+$_lang['Text on 2 columns'] = 'Text on 2 columns';
+
+//Commons plugins
+$_lang['Height'] = 'Height';
+$_lang['Layout'] = 'Layout';
+$_lang['Leave emtpy to keep original size'] = 'Leave emtpy to keep original size';
+$_lang['Text on left'] = 'Text on left';
+$_lang['Text on right'] = 'Text on right';
+$_lang['Text A on left'] = 'Text A on left';
+$_lang['Text A on right'] = 'Text A on right';
+$_lang['Url of a file'] = 'Url of a file';
+$_lang['Url of an image'] = 'Url of an image';
+$_lang['Width'] = 'Width';
 
 //Video.class
-$_lang['Not a correct available%videoType video web address : <br> %wrongInput'] ='Not a correct available%videoType video web address : <br> %wrongInput';
-$_lang['Video identifiers'] ='Video identifiers';
-$_lang['Video type'] ='Video type';
-$_lang['Video parameters'] ='Video parameters';
-$_lang['Web video'] ='Web video';
+$_lang['Not a correct available%videoType video web address : <br> %wrongInput'] = 'Not a correct available%videoType video web address : <br> %wrongInput';
+$_lang['Video identifiers'] = 'Video identifiers';
+$_lang['Video type'] = 'Video type';
+$_lang['Video parameters'] = 'Video parameters';
+$_lang['Web video'] = 'Web video';
+
+//flickr.class
+$_lang['Display title'] = 'Display title';
+$_lang['Number of displayed items'] = 'Number of displayed items';
 
 //VIDEOPLUGINS
-
-$_lang['Automatic'] ='Automatic';
-$_lang['DailyMotion'] ='DailyMotion';
-$_lang['Large'] ='Large';
+$_lang['Automatic'] = 'Automatic';
+$_lang['DailyMotion'] = 'DailyMotion';
+$_lang['Large'] = 'Large';
 $_lang['Medium'] ='Medium';
-$_lang['Size'] ='Size';
-$_lang['Small'] ='Small';
-$_lang['Video identification'] ='Video identification';
-$_lang['Video internet address'] ='Video internet address';
-$_lang['YouTube'] ='YouTube';
-
-?>
+$_lang['Size'] = 'Size';
+$_lang['Small'] = 'Small';
+$_lang['Video identification'] = 'Video identification';
+$_lang['Video internet address'] = 'Video internet address';
+$_lang['YouTube'] = 'YouTube';
--- modules/CLPAGES/lang/lang_french.php
+++ modules/CLPAGES/lang/lang_french.php
@@ -1,18 +1,50 @@
 <?php
 //Page
-$_lang['Run the slides show'] ='Lancer la présentation des slides';
+$_lang['Add a composant'] = 'Ajouter un composant';
+$_lang['Are you sure to delete page "%pageTitle" ?'] = 'Etes-vous sûr de vouloir supprimer "%pageTitle" ?';
+$_lang['Create a new page'] = 'Créer une nouvelle page';
+$_lang['Creation date'] = 'Date de création';
+$_lang['Empty page successfully created'] = 'Votre nouvelle page a été crée avec succès';
+$_lang['No pages'] = 'Aucune page';
+$_lang['One page'] = 'Page unique';
+$_lang['Page successfully deleted'] ='Votre page a été supprimée';
+$_lang['Page successfully modified'] = 'Votre page a été modifiée avec succès';
+$_lang['Pages'] = 'Pages';
+$_lang['Run the slides show'] = 'Lancer la présentation des slides';
+$_lang['Slideshow'] = 'Diaporama';
 
 //PLUGINS
+$_lang['External website'] = 'Site web externe';
+$_lang['Flickr gallery'] = 'Galerie Flickr';
+$_lang['Text'] = 'Texte';
+$_lang['Text and file'] = 'Texte + fichier';
+$_lang['Text and image'] = 'Texte + image';
+$_lang['Text on 2 columns'] = 'Texte sur deux colonnes';
+
+//Commons plugins
+$_lang['Height'] = 'Hauteur';
+$_lang['Layout'] = 'Mise en page';
+$_lang['Leave emtpy to keep original size'] = 'Laissez vide pour conserver la taille originale';
+$_lang['Text on left'] = 'Texte à gauche';
+$_lang['Text on right'] = 'Texte à droite';
+$_lang['Text A on left'] = 'Texte A à gauche';
+$_lang['Text A on right'] = 'Texte A à droite';
+$_lang['Url of a file'] = 'Url du fichier';
+$_lang['Url of an image'] = 'Url de l\'image';
+$_lang['Width'] = 'Largeur';
 
 //Video.class
-$_lang['Not a correct available%videoType video web address : <br> %wrongInput'] ='L\'entrée suivante n\'est pas une adresse internet correcte pour le type de vidéo disponible%videoType : <br> %wrongInput';
-$_lang['Video identifiers'] ='Identifiants de la vidéo';
-$_lang['Video type'] ='Type de vidéo';
-$_lang['Video parameters'] ='Paramètres de la vidéo';
-$_lang['Web video'] ='Video web';
+$_lang['Not a correct available%videoType video web address : <br> %wrongInput'] = 'L\'entrée suivante n\'est pas une adresse internet correcte pour le type de vidéo disponible%videoType : <br> %wrongInput';
+$_lang['Video identifiers'] = 'Identifiants de la vidéo';
+$_lang['Video type'] = 'Type de vidéo';
+$_lang['Video parameters'] = 'Paramètres de la vidéo';
+$_lang['Web video'] = 'Video web';
+
+//flickr.class
+$_lang['Display title'] = 'Titre à afficher';
+$_lang['Number of displayed items'] = 'Nombre d\'éléments à afficher';
 
 //VIDEOPLUGINS
-
 $_lang['Automatic'] ='Automatique';
 $_lang['DailyMotion'] ='DailyMotion';
 $_lang['Large'] ='Grande';
@@ -22,4 +54,3 @@ $_lang['Small'] ='Petite';
 $_lang['Video identification'] ='Identifiant unique de la vidéo';
 $_lang['Video internet address'] ='Adresse internet de la vidéo';
 $_lang['YouTube'] ='YouTube';
-$_lang['Creation date'] = 'Date de création';
