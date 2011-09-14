d10d136e1e114b62c01e4e22ee6141594e80a20d
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-09-14 11:30:34 +0000

    french localization complete

--- modules/CLPAGES/lang/lang_english.php
+++ modules/CLPAGES/lang/lang_english.php
@@ -2,16 +2,38 @@
 //Page
 $_lang['Add a composant'] = 'Add a composant';
 $_lang['Are you sure to delete page "%pageTitle" ?'] = 'Are you sure to delete page "%pageTitle" ?';
+$_lang['Big'] = 'Big';
+$_lang['Claroline S5 viewer'] = 'Claroline S5 viewer';
+$_lang['Click to see your slide preview'] = 'Click to see your slide preview';
 $_lang['Create a new page'] = 'Create a new page';
 $_lang['Creation date'] = 'Creation date';
+$_lang['Deprecated plugin, contact administrator'] = 'Deprecated plugin, contact administrator';
+$_lang['Edit page settings'] = 'Edit page settings';
 $_lang['Empty page successfully created'] = 'Empty page successfully created';
+$_lang['Error : cannot read RSS feed (Check that feed is accessible or ask your administrator if php setting "allow_url_fopen" is turned on).'] = 'Error : cannot read RSS feed (Check that feed is accessible or ask your administrator if php setting "allow_url_fopen" is turned on).';
+$_lang['Fatal error : cannot delete'] = 'Fatal error : cannot delete';
+$_lang['Image'] = 'Image';
+$_lang['Layout :'] = 'Layout :';
+$_lang['Loading...'] = 'Loading...';
 $_lang['No pages'] = 'No pages';
+$_lang['Not a correct and valid video Url'] = 'Not a correct and valid video Url';
 $_lang['One page'] = 'One page';
 $_lang['Page successfully deleted'] ='Page successfully deleted';
 $_lang['Page successfully modified'] = 'Page successfully modified';
 $_lang['Pages'] = 'Pages';
+$_lang['Problem reading feed.'] = 'Problem reading feed.';
+$_lang['Rss'] = 'Rss';
 $_lang['Run the slides show'] = 'Run the slides show';
 $_lang['Slideshow'] = 'Slideshow';
+$_lang['Tag'] = 'Tag';
+$_lang['Text A'] = 'Text A';
+$_lang['Text B'] = 'Text B';
+$_lang['Url'] = 'Url';
+$_lang['Url of rss feed'] = 'Url of rss feed';
+$_lang['Video id'] = 'Video id';
+$_lang['Video not configured'] = 'Video not configured';
+$_lang['Video reference'] = 'Video reference';
+$_lang['Video size'] = 'Video size';
 
 //PLUGINS
 $_lang['External website'] = 'External website';
