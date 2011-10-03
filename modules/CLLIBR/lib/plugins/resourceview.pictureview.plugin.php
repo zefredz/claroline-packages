ddbe8f17f8ad88dcd86e7e2dbe505421df7fe082
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-10-03 10:14:34 +0000

    completed image viewer plugin (for external resources)

--- modules/CLLIBR/lib/plugins/resourceview.pictureview.plugin.php
+++ modules/CLLIBR/lib/plugins/resourceview.pictureview.plugin.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.6.2 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.9.3 $Revision$ - Claroline 1.11
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
@@ -15,12 +15,19 @@ class PictureView extends ResourceView
     
     public function render()
     {
-        return '<div id="imageView"><img src="data:'
-             . StoredResource::getMimeType( $this->storedResource->getFileName() )
-             . ';base64,'
-             . base64_encode( $this->storedResource->getFile( StoredResource::RAW_ACCESS ) )
-             . '" alt="'
-             . $this->storedResource->getFileName()
-             . '" /></div>';
+        if ( is_string( $this->storedResource ) )
+        {
+            return '<img src="' . $this->storedResource . '" />';
+        }
+        else
+        {
+            return '<div id="imageView"><img src="data:'
+                 . StoredResource::getMimeType( $this->storedResource->getFileName() )
+                 . ';base64,'
+                 . base64_encode( $this->storedResource->getFile( StoredResource::RAW_ACCESS ) )
+                 . '" alt="'
+                 . $this->storedResource->getFileName()
+                 . '" /></div>';
+        }
     }
 }
--- modules/CLLIBR/lib/resourceview.lib.php
+++ modules/CLLIBR/lib/resourceview.lib.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.6.2 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.9.3 $Revision$ - Claroline 1.11
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
@@ -11,7 +11,7 @@
 
 /**
  * An abstract class for resource view
- * @property StoredResource object $storedResource
+ * @property StoredResource object or string $storedResource
  * @abstract render()
  */
 abstract class ResourceView
@@ -23,6 +23,11 @@ abstract class ResourceView
      */
     public function __construct( $storedResource )
     {
+        if ( ! is_string( $storedResource ) && ! is_a( $storedResource , 'StoredResource' ) )
+        {
+            throw new Exception( 'invalid data' );
+        }
+        
         $this->storedResource = $storedResource;
     }
     
