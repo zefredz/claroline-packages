7b091d9cb87e76bb8aa73793d2efe07fd3eb84cc
systho <systho@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-10-25 12:02:53 +0000

    fixed migration

--- trunk/modules/LVSURVEY/setup/00000000000002/down.sql
+++ trunk/modules/LVSURVEY/setup/00000000000002/down.sql
@@ -1,6 +1,14 @@
-ALTER TABLE `__CL_MAIN__survey2_question` DROP `author_id`;
-
 DROP TABLE IF EXISTS `__CL_MAIN__survey2_version`;
+
+-- --------------------------------------------------------
 
 ALTER TABLE `__CL_MAIN__survey2_participation` 
     ADD UNIQUE KEY `surveyId` (`surveyId`,`userId`);
+-- --------------------------------------------------------
+
+ALTER TABLE `__CL_MAIN__survey2_question` 
+    DROP COLUMN `author_id`,
+    DROP COLUMN `shared`,
+    DROP INDEX author_idx;
+
+-- --------------------------------------------------------
