f694d6a4d7da05e08f09180278dc2a518440d2eb
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-10-26 14:21:37 +0000

    add missing french lang vars

---
+++ trunk/modules/LVSURVEY/lang/lang_italian.incomplete.php
@@ -0,0 +1,30 @@
+<?php 
+
+$_lang['Allow other surveys to use this question'] = 'Allow other surveys to use this question';
+$_lang['Allow users to change their answer until the survey is closed'] = 'Allow users to change their answer until the survey is closed';
+$_lang['Answer required'] = 'Answer required';
+$_lang['Author of question'] = 'Author of question';
+$_lang['Delete question'] = 'Delete question';
+$_lang['Display all questions'] = 'Display all questions';
+$_lang['Display details'] = 'Display details';
+$_lang['Display only my questions'] = 'Display only my questions';
+$_lang['Display only questions appearing in this course'] = 'Display only questions appearing in this course';
+$_lang['Edit separator'] = 'Edit separator';
+$_lang['Edit survey'] = 'Edit survey';
+$_lang['Error'] = 'Error';
+$_lang['List of questions'] = 'List of questions';
+$_lang['Make Invisible'] = 'Make Invisible';
+$_lang['Make Visible'] = 'Make Visible';
+$_lang['Move Down'] = 'Move Down';
+$_lang['Move Up'] = 'Move Up';
+$_lang['No Choices'] = 'No Choices';
+$_lang['No Results'] = 'No Results';
+$_lang['Question preview'] = 'Question preview';
+$_lang['Select'] = 'Select';
+$_lang['Success'] = 'Success';
+$_lang['Survey Recall'] = 'Survey Recall';
+$_lang['This question is used in some surveys . You can\'t delete it'] = 'This question is used in some surveys . You can\'t delete it';
+$_lang['Unknown Question Type or Alignment'] = 'Unknown Question Type or Alignment';
+$_lang['You have forgotten to fill a mandatory field'] = 'You have forgotten to fill a mandatory field';
+$_lang['You have forgotten to give options for choice'] = 'You have forgotten to give options for choice';
+$_lang['char(s) left'] = 'char(s) left';
