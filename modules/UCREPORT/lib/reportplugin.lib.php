c300bb3d162fdb50b92cb3152dc4ff93242a86c8
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-10-27 14:30:57 +0000

    added braket to condition

--- modules/UCREPORT/lib/reportplugin.lib.php
+++ modules/UCREPORT/lib/reportplugin.lib.php
@@ -94,8 +94,8 @@ abstract class ReportPlugin
                     $itemId = $this->toolLabel . '_' . $line[ 'item_id' ];
                     
                     if ( array_key_exists( $itemId , $this->itemList )
-                      && array_key_exists( $userId , $userList )
-                      || empty( $userList ) )
+                      && ( array_key_exists( $userId , $userList )
+                      || empty( $userList ) ) )
                     {
                         $this->dataList[ $userId ][ $itemId ] = $line[ 'score' ];
                         $this->itemList[ $itemId ][ 'submission_count' ]++;
