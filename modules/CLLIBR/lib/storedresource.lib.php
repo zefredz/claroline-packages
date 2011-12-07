3138e78a0317b5dcf4c8d81b3b89dd9bb89f5d01
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2011-12-07 12:41:11 +0000

    fixed bug in metadata export : file name is given by MetadataView::export() method

--- modules/CLLIBR/lib/storedresource.lib.php
+++ modules/CLLIBR/lib/storedresource.lib.php
@@ -2,7 +2,7 @@
 /**
  * Online library for Claroline
  *
- * @version     CLLIBR 0.8.2 $Revision$ - Claroline 1.9
+ * @version     CLLIBR 0.9.7 $Revision$ - Claroline 1.9
  * @copyright   2001-2011 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLLIBR
@@ -146,6 +146,7 @@ class StoredResource
                 'html'  => 'text/html',
                 'url'   => 'text/html',
                 'xml'   => 'application/xml',
+                'rdf'   => 'application/xml',
                 'css'   => 'text/css',
                 'js'    => 'text/javascript',
                 # Syndication
