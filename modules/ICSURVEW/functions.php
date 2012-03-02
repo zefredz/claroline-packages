8d47ba6cf03ddecffcc422785ddc979db182b159
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-03-02 14:07:48 +0000

    uses of new lock.lib

--- modules/ICSURVEW/functions.php
+++ modules/ICSURVEW/functions.php
@@ -8,18 +8,29 @@ include_once dirname( __FILE__ ) . '/lib/survey.lib.php';
 $surveyFileUrl = dirname( __FILE__ ) . '/survey.json';
 $userId = claro_get_current_user_id();
 
-if ( $userId
-  && ! defined( 'ICTERMS_PAGE_ALLOWED' ) // for compatibility with ICTERMS
-  && ! claro_is_platform_admin() )
+if ( Claro_KernelHook_Lock::lockAvailable() )
 {
-    $survey = new ICSURVEW_Survey( $surveyFileUrl );
-    $answer = new ICSURVEW_Answer( $userId , $survey->get() );
-    
-    if (   ! defined('ICSURVEW_ACCESSED')
-        && ! $answer->hasAnswered() 
-        && ! isset( $_SESSION[ 'ICSURVEW_LATER' ] ) )
+    if ( $userId
+      && ! claro_is_platform_admin() )
     {
-        claro_redirect( get_module_url('ICSURVEW') .'/entry.php' );
-        die();
+        if( Claro_KernelHook_Lock::getLock( 'ICSURVEW' ) )
+        {
+            $survey = new ICSURVEW_Survey( $surveyFileUrl );
+            $answer = new ICSURVEW_Answer( $userId , $survey->get() );
+            
+            if ( ! ( $answer->hasAnswered()
+                    && ( ! isset( $_SESSION[ 'ICSURVEW_STAGE' ] )
+                    || $_SESSION[ 'ICSURVEW_STAGE' ] == 4 ) )
+                && ! isset( $_SESSION[ 'ICSURVEW_LATER' ] ) )
+            {
+                claro_redirect( get_module_url('ICSURVEW') .'/entry.php' );
+                die();
+            }
+            else
+            {
+                Claro_KernelHook_Lock::releaseLock( 'ICSURVEW' );
+            }
+        }
     }
 }
+
