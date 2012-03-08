df185c28bbb63661fc104ed4efa75b3581c25307
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-03-08 17:15:36 +0000

    fixed final report message

--- modules/ICADDEXT/templates/report.tpl.php
+++ modules/ICADDEXT/templates/report.tpl.php
@@ -1,5 +1,5 @@
 <?php if( $this->controller->is_ok() ) : ?>
-    <h3><?php echo get_lang( 'success_message' ); ?></h3>
+    <h3><?php echo get_lang( 'added_message' ); ?></h3>
     <textarea rows="<?php echo count( $this->controller->importer->output[ 'success' ] ) + 1; ?>" style="width: 100%;">
         <?php echo $this->controller->importer->getReport(); ?>
     </textarea>
