f62d498da37b26aeae2736e5991f7c5dc1f02912
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-03-27 12:58:18 +0000

    zabelle is happy

--- modules/ICADDEXT/templates/report.tpl.php
+++ modules/ICADDEXT/templates/report.tpl.php
@@ -3,5 +3,5 @@
 <textarea rows="<?php echo count( $this->controller->importer->added ) + 1; ?>" style="width: 100%;">
     <?php echo $this->controller->importer->getReport(); ?>
 </textarea>
-<?php echo claro_html_button( htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'] ) ) , get_lang( 'OK' ) ); ?>
 <?php endif; ?>
+<?php echo claro_html_button( htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'] ) ) , get_lang( 'OK' ) ); ?>
