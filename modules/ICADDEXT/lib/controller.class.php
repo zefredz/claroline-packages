427dc88628619a45b10dd5795c35ebb07ef7d66e
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-03-29 12:37:06 +0000

    removed commented and useless code

--- modules/ICADDEXT/lib/controller.class.php
+++ modules/ICADDEXT/lib/controller.class.php
@@ -84,11 +84,6 @@ class ICADDEXT_Controller
         $this->status_ok = $this->importer->toAdd
                         || $this->importer->conflict
                         || $this->importer->incomplete;
-        /*var_dump( $this->importer->csvParser->data,
-                  $this->importer->output,
-                  $this->importer->toAdd,
-                  $this->importer->conflict,
-                  $this->importer->incomplete );*/
     }
     
     private function _exAdd()
