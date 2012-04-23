f64eaeace2764ba38f97fc7663a1482dfc171d33
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-04-23 12:05:24 +0000

    fixed display problem in report

--- modules/ICADDEXT/templates/report.tpl.php
+++ modules/ICADDEXT/templates/report.tpl.php
@@ -1,7 +1,5 @@
 <?php if( $this->controller->is_ok() && $this->controller->importer->getReport() ) : ?>
 <h3><?php echo get_lang( 'added_message' ); ?></h3>
-<textarea rows="<?php echo count( $this->controller->importer->added ) + 1; ?>" style="width: 100%;">
-    <?php echo $this->controller->importer->getReport(); ?>
-</textarea>
+<textarea rows="<?php echo count( $this->controller->importer->added ) + 1; ?>" style="width: 100%;"><?php echo $this->controller->importer->getReport(); ?></textarea>
 <?php endif; ?>
 <?php echo claro_html_button( htmlspecialchars( Url::Contextualize( $_SERVER['PHP_SELF'] ) ) , get_lang( 'OK' ) ); ?>
