3e5dec794679d96b2722f78c7111239a10562f54
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-05-08 16:15:39 +0000

    update of survey text

--- trunk/modules/ICSURVEW/lang/lang_french.php
+++ trunk/modules/ICSURVEW/lang/lang_french.php
@@ -1,14 +1,14 @@
 <?php
 
 $_lang['_intro_text'] = '<h3>Enquête sur l\'utilisation des cours sur iCampus</h3>
-Afin de détecter les cours non utilisés sur iCampus et de mieux cerner les utilisations des cours, nous vous demandons de répondre à une enquête qui comporte deux questions et une vérification.<br />
+Afin d\'identifier les cours réellement utilisés sur iCampus et de mieux cerner leurs utilisations, nous vous demandons de répondre à une enquête qui comporte deux questions et une vérification.<br />
 Vos réponses sont attendues avant le 5 juillet 2012.<br />
 Cette enquête est réalisée à la demande du prorecteur à la formation et à l\'enseignement.';
 $_lang['_not_complete'] = 'Vous n\'avez pas rempli tous les champs!';
 $_lang['_course_code_verification'] = '<h3>Vérification de la composition des codes cours des cours officiels</h3>
 Depuis septembre 2010, les cours intégrés à un programme officiel UCL ont un code cours qui commence par l\'initiale du site de l\'entité responsable: L pour Louvain-la-Neuve, W pour Woluwé et depuis septembre 2011, M pour Mons.<br />
 Pour que les étudiants trouvent votre cours sans difficulté dans la liste des cours, nous vous suggérons de modifier les codes de vos cours officiels si nécessaire :';
-$_lang['_back'] = 'Revenir à la question précédente';
+$_lang['_back'] = '< Revenir à la question précédente';
 $_lang['_description_stage_0'] = 'Bienvenue';
 $_lang['_description_stage_1'] = 'Questionnaire principal';
 $_lang['_description_stage_2'] = 'Revue des codes cours';
@@ -16,7 +16,7 @@ $_lang['_description_stage_3'] = 'Fin';
 $_lang['_exit_message' ] = 'Merci pour votre collaboration et bon travail sur iCampus ...';
 $_lang['_finish'] = 'Terminer l\'enquête';
 $_lang['_later'] = 'Répondre plus tard';
-$_lang['_next'] = 'Passer à la question suivante';
+$_lang['_next'] = 'Passer à la question suivante >';
 $_lang['_not_finished'] = '<strong>Vous n\'avez pas répondus à toutes les questions. Lors de votre prochaine connexion, il vous sera à nouveau demandé de compléter le questionnaire</strong>';
 $_lang['_other_answered'] = '<span style="color: red; font-weight: bold;">Attention : L\'un des vos co-gestionnaires a déjà dépondu à certaines questions!</span>';
 $_lang['_now'] = 'Répondre maintenant';
