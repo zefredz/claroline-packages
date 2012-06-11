c5499c00446d4405a7c72406830f64b893453568
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-06-11 12:58:57 +0000

    fixed bad redirection problem (2)

--- trunk/modules/ICSURVEW/templates/stage3.tpl.php
+++ trunk/modules/ICSURVEW/templates/stage3.tpl.php
@@ -1,4 +1,4 @@
 <?php echo get_lang( '_exit_message' ); ?><br />
-<a href="<?php echo  get_path( 'url' );?>">
+<a href="<?php echo  get_path( 'rootWeb' );?>">
     <input type="button" value="<?php echo get_lang( 'OK' );?>" />
 </a>
