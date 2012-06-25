1ea5df1041680de07e5b2832dccedf301bb70dd7
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-06-25 11:33:24 +0000

    fixed: user can bypass the lock by clicking on a link in the menu bar

--- modules/ICSURVEW/functions.php
+++ modules/ICSURVEW/functions.php
@@ -8,14 +8,14 @@ include_once dirname( __FILE__ ) . '/lib/surveylist.class.php';
 
 $userId = claro_get_current_user_id();
 
-if ( Claro_KernelHook_Lock::lockAvailable() )
+if ( Claro_KernelHook_Lock::getLock() )
 {
     if ( $userId && ! claro_is_platform_admin() )
     {
         $surveyList = new ICSURVEW_SurveyList();
         $surveyId = $surveyList->getActive();
         
-        if( $surveyId && Claro_KernelHook_Lock::getLock() )
+        if( $surveyId )
         {
             $survey = new ICSURVEW_Survey( $surveyId );
             $answer = new ICSURVEW_Answer( $userId , $survey->get() );
@@ -25,8 +25,13 @@ if ( Claro_KernelHook_Lock::lockAvailable() )
                     || $_SESSION[ 'ICSURVEW_STAGE' ] == 4 ) )
                 && ! isset( $_SESSION[ 'ICSURVEW_LATER' ] ) )
             {
-                claro_redirect( get_module_url('ICSURVEW') .'/entry.php' );
-                die();
+                $uriParts = explode( '?' , $_SERVER['REQUEST_URI'] );
+                
+                if( $uriParts[0] != get_module_url('ICSURVEW') .'/entry.php' )
+                {
+                    claro_redirect( get_module_url('ICSURVEW') .'/entry.php' );
+                    die();
+                }
             }
             else
             {
