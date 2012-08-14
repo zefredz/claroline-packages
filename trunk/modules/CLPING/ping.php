b1463c67c64dfccb1e1748bc4dcd1a3f561325cb
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-08-14 06:30:05 +0000

    a simple ping module to maintain PHP session open while the user is logged in (@todo make it configurable, add max ping number to avoid eternal sessions...)

---
+++ trunk/modules/CLPING/ping.php
@@ -0,0 +1,13 @@
+<?php // $Id$
+
+/**
+ * Ping
+ *
+ * @version     CLPING 1.0 $Revision$ - Claroline 1.11
+ * @copyright   2001-2012 Universite Catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package     CLPING
+ * @author      Frederic Minne <zefredz@claroline.net>
+ */
+
+require dirname( __FILE__ ) . '/../../claroline/inc/claro_init_global.inc.php';
