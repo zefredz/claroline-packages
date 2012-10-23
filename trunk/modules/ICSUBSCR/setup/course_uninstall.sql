9363db26f4f2c7a3a7d9b22ea155afadd8c31651
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-10-23 08:51:33 +0000

    rewriting from scratch

--- trunk/modules/ICSUBSCR/setup/course_uninstall.sql
+++ trunk/modules/ICSUBSCR/setup/course_uninstall.sql
@@ -3,4 +3,5 @@
 
 DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_session`;
 DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_slot`;
-DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_subscription`;
+DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_record`;
+DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_list`;
