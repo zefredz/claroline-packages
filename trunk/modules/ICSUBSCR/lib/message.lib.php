7e96852cd8e3499444596e9b5a4ab40fc68d3448
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-10-26 15:24:31 +0000

    work in progress

---
+++ trunk/modules/ICSUBSCR/lib/message.lib.php
@@ -0,0 +1,67 @@
+<?php // $Id$
+/**
+ * Subscriptions for Claroline
+ *
+ * @version     ICSUBSCR 0.1 $Revision$ - Claroline 1.11
+ * @copyright   2001-2012 Universite catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @package     ICSUBSCR
+ * @author      Frederic Fervaille <frederic.fervaille@uclouvain.be>
+ */
+
+class Message extends DialogBox
+{
+    const SUCCESS = 'success';
+    const ERROR = 'error';
+    const INFO = 'info';
+    const QUESTION = 'question';
+    const WARNING = 'warning';
+    
+    protected $output = array();
+    
+    /**
+     * Adds a mesage into the output
+     * @param string $type : success, error or info
+     * @param string $content : the message itself
+     * @return void
+     */
+    public function addMsg( $type , $content )
+    {
+        $this->validate( $type );
+        $this->output[ $type ][] = $content;
+        $this->{$type}( $content );
+    }
+    
+    public function has( $type = self::ERROR )
+    {
+        $this->validate( $type );
+        
+        if( array_key_exists( $type , $this->output ) )
+        {
+            return count( $this->output[ $type ] );
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    public function hasError()
+    {
+        return $this->has();
+    }
+    
+    private function validate( $type )
+    {
+        if( $type != self::SUCCESS
+            && $type != self::ERROR
+            && $type != self::INFO
+            && $type != self::QUESTION
+            && $type != self::WARNING )
+        {
+            throw new Exception( 'Invalid message type');
+        }
+        
+        return true;
+    }
+}
