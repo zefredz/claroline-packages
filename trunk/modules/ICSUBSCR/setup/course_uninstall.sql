7d07e0ed3859197d545181873ce0e2c3d4891c01
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2012-11-13 14:42:29 +0000

    put rank in a separate table

--- trunk/modules/ICSUBSCR/setup/course_uninstall.sql
+++ trunk/modules/ICSUBSCR/setup/course_uninstall.sql
@@ -4,3 +4,4 @@
 DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_session`;
 DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_slot`;
 DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_record`;
+DROP TABLE IF EXISTS `__CL_COURSE__icsubscr_list`;
