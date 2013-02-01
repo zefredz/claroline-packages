299a6bf01ab0fde0dca8b32109077c7c8dcf8084
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-02-01 10:28:40 +0000

    fixed PHP 5.4 compatibility

--- modules/CLQPOLL/lib/poll2csv.lib.php
+++ modules/CLQPOLL/lib/poll2csv.lib.php
@@ -3,7 +3,7 @@
 /**
  * Claroline Poll Tool
  *
- * @version     CLQPOLL 1.2.0 $Revision$ - Claroline 1.9
+ * @version     CLQPOLL 1.2.2 $Revision$ - Claroline 1.11.5
  * @copyright   2001-2010 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLQPOLL
