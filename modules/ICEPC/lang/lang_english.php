44d0b94445e05299b954242980cdfe1e270bac8b
Frédéric Minne <897742+zefredz@users.noreply.github.com>
2013-03-21 15:09:39 +0000

    ICEPC : language files

---
+++ modules/ICEPC/lang/lang_english.php
@@ -0,0 +1,67 @@
+<?php 
+
+$_lang['(click to show/hide)'] = '(click to show/hide)';
+$_lang['Academic year'] = 'Academic year';
+$_lang['By doing this students witch are already enroled in your course will be linked to their EPC registration. This will allow you to delete them when deleting an EPC list from your course.If not, they will remain in your course when deleting an EPC list.'] = 'By doing this students witch are already enroled in your course will be linked to their EPC registration. This will allow you to delete them when deleting an EPC list from your course.If not, they will remain in your course when deleting an EPC list.';
+$_lang['Course or program code'] = 'Course or program code';
+$_lang['Delete from course'] = 'Delete from course';
+$_lang['EPC'] = 'EPC';
+$_lang['EPC list type'] = 'EPC list type';
+$_lang['Import student list from EPC'] = 'Import student list from EPC';
+$_lang['Import students from EPC'] = 'Import students from EPC';
+$_lang['In course'] = 'In course';
+$_lang['Link already enroled students'] = 'Link already enroled students';
+$_lang['List of student in class %className'] = 'List of student in class %className';
+$_lang['List of students in Class'] = 'List of students in Class';
+$_lang['Manage student lists from EPC'] = 'Manage student lists from EPC';
+$_lang['No EPC student list imported into this course yet'] = 'No EPC student list imported into this course yet';
+$_lang['No student to display'] = 'No student to display';
+$_lang['Not allowed!'] = 'Not allowed!';
+$_lang['Number of students'] = 'Number of students';
+$_lang['Program'] = 'Program';
+$_lang['Search for students in EPC'] = 'Search for students in EPC';
+$_lang['Synchronize students with EPC'] = 'Synchronize students with EPC';
+$_lang['The complete official code of the course or program (see help below), for instance : LBIO1111A, LMAPR2016, BIRA21MS/G, BIR13BA'] = 'The complete official code of the course or program (see help below), for instance : LBIO1111A, LMAPR2016, BIRA21MS/G, BIR13BA';
+$_lang['The given code is'] = 'The given code is';
+$_lang['The program codes are made of 5 elements :  
+<ol>
+<li>SSSSS : Sigle : up to 5 upper case letters</li>
+<li>C : cycle : 1 digit</li>
+<li>N : level (year in the cycle) : 1 digit</li>
+<li>LF : final letters : BA, MA, MS, etc : 2 upper case letters</li>
+<li>OR : orientation : 0, 1 or 2 uper case letters</li>
+</ol>
+Usualy a \'/\' is added before the orientation digits : SSSSCNLF/OR<br />
+Sample program codes : BIRA21MS/G, BIR13BA'] = 'The program codes are made of 5 elements :  
+<ol>
+<li>SSSSS : Sigle : up to 5 upper case letters</li>
+<li>C : cycle : 1 digit</li>
+<li>N : level (year in the cycle) : 1 digit</li>
+<li>LF : final letters : BA, MA, MS, etc : 2 upper case letters</li>
+<li>OR : orientation : 0, 1 or 2 uper case letters</li>
+</ol>
+Usualy a \'/\' is added before the orientation digits : SSSSCNLF/OR<br />
+Sample program codes : BIRA21MS/G, BIR13BA';
+$_lang['The year the wanted academic year started on. For instance, if the academic year started on september 2012, enter 2012.'] = 'The year the wanted academic year started on. For instance, if the academic year started on september 2012, enter 2012.';
+$_lang['UCL course codes are made of 3 elements :  
+<ol>
+<li>SSSSS : Sigle : up to 5 upper case letters</li>
+<li>NNNN : numerical code : 4 digits</li>
+<li>D : Subdivision : 0 or 1 upper case letter</li>
+</ol>
+Sample course code : LBIO1111A, LMAPR2016'] = 'UCL course codes are made of 3 elements :  
+<ol>
+<li>SSSSS : Sigle : up to 5 upper case letters</li>
+<li>NNNN : numerical code : 4 digits</li>
+<li>D : Subdivision : 0 or 1 upper case letter</li>
+</ol>
+Sample course code : LBIO1111A, LMAPR2016';
+$_lang['Update'] = 'Update';
+$_lang['Update from EPC'] = 'Update from EPC';
+$_lang['You are going to delete this class, do you want to continue ?'] = 'You are going to delete this class, do you want to continue ?';
+$_lang['a course'] = 'a course';
+$_lang['a program'] = 'a program';
+$_lang['id'] = 'id';
+$_lang['in course'] = 'in course';
+$_lang['This operation could take some time, please wait until it\'s finished'] = 'This operation could take some time, please wait until it\'s finished';
+$_lang['recommended'] = 'recommended';
