cfca389dcfa4ee2e957a95052eaa82c2b2523f4a
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-03-21 15:09:39 +0000

    ICEPC : language files

---
+++ trunk/modules/ICEPC/lang/lang_french.php
@@ -0,0 +1,67 @@
+<?php // $Id$
+
+$_lang['Search for students in EPC'] = "Recherche d'étudiants dans EPC";
+$_lang['Academic year'] = "Année académique";
+$_lang['The year the wanted academic year started on. For instance, if the academic year started on september 2012, enter 2012.'] = "Année de début de l'année académique souhaitée. Par exemple, pour l'année académique 2012-2013 qui commen&ccedil;ait en septembre 2012, indiquer 2012.";
+$_lang['Course or program code'] = "Code du cours ou de l'année d'étude";
+$_lang['The complete official code of the course or program (see help below), for instance : LBIO1111A, LMAPR2016, BIRA21MS/G, BIR13BA'] = "Le code officiel complet du cours ou de l'année d'étude (voir aide plus bas), par exemple : LBIO1111A, LMAPR2016, BIRA21MS/G, BIR13BA";
+$_lang['The given code is'] = "Le code ci-dessus est";
+$_lang['a program'] = "une année d'étude";
+$_lang['a course'] = "un cours";
+$_lang['(click to show/hide)'] = "(cliquer pour afficher/masquer)";
+$_lang['UCL course codes are made of 3 elements :  
+<ol>
+<li>SSSSS : Sigle : up to 5 upper case letters</li>
+<li>NNNN : numerical code : 4 digits</li>
+<li>D : Subdivision : 0 or 1 upper case letter</li>
+</ol>
+Sample course code : LBIO1111A, LMAPR2016'] = "Les codes de cours UCL sont composés de 3 éléments :
+<ol>
+<li>SSSSS : Sigle : maximum 5 lettres majuscules</li>
+<li>NNNN : CNUM (code numérique) : 4 chiffres</li>
+<li>D : Subdivision : 0 ou 1 lettre majuscule</li>
+</ol>
+Par exemple : LBIO1111A, LMAPR2016";
+$_lang['The program codes are made of 5 elements :  
+<ol>
+<li>SSSSS : Sigle : up to 5 upper case letters</li>
+<li>C : cycle : 1 digit</li>
+<li>N : level (year in the cycle) : 1 digit</li>
+<li>LF : final letters : BA, MA, MS, etc : 2 upper case letters</li>
+<li>OR : orientation : 0, 1 or 2 uper case letters</li>
+</ol>
+Usualy a \'/\' is added before the orientation digits : SSSSCNLF/OR<br />
+Sample program codes : BIRA21MS/G, BIR13BA'] = "Les codes année d'étude sont composés de 5 éléments :  
+<ol>
+<li>SSSSS : Sigle : maximum 5 letttres majuscules</li>
+<li>C : cycle : 1 chiffre</li>
+<li>N : niveau (année dans le cycle) : 1 chiffre</li>
+<li>LF : lettres finales : BA, MA, MS, etc : 2 lettres majuscules</li>
+<li> OR : orientation : 0, 1 ou 2 lettres majuscules</li>
+</ol>
+Les codes sont généralement présentés avec un '/' entre les éléments 4 et 5 : SSSSCNLF/OR<br />
+Par exemple : BIRA21MS/G, BIR13BA";
+$_lang['Link already enroled students'] = 'Lier les étudiants déjà inscrits';
+$_lang['By doing this students witch are already enroled in your course will be linked to their EPC registration. This will allow you to delete them when deleting an EPC list from your course.If not, they will remain in your course when deleting an EPC list.'] = 'En faisant cela, les étudiants déjà inscrits à votre cours seront liés à leur inscription EPC. Ils pourront alors &ecirc;tre supprimés lorsque que vous supprimerez la liste EPC de votre cours. Dans le cas contraire, ils resteront inscrits lors de la suppression de cette liste.';
+$_lang['Delete from course'] = 'Supprimer du cours';
+$_lang['EPC'] = 'EPC';
+$_lang['EPC list type'] = 'Type de liste EPC';
+$_lang['Import student list from EPC'] = 'Importer une liste d\'étudiants depuis EPC';
+$_lang['Import students from EPC'] = 'Importer des étudiants depuis EPC';
+$_lang['In course'] = 'Dans le cours';
+$_lang['List of student in class %className'] = 'Liste des étudiants dans la classe %className';
+$_lang['List of students in Class'] = 'Liste des étudiants dans la classe';
+$_lang['Manage student lists from EPC'] = 'Gérer les listes d\'étudiants EPC';
+$_lang['No EPC student list imported into this course yet'] = 'Pas de liste EPC dans ce cours pour le moment';
+$_lang['No student to display'] = 'Pas d\'étudiants à supprimer';
+$_lang['Not allowed!'] = 'Non autorisé!';
+$_lang['Number of students'] = 'Nombre d\'étudiants';
+$_lang['Program'] = 'Programme';
+$_lang['Synchronize students with EPC'] = 'Synchroniser les étudiants avec EPC';
+$_lang['Update'] = 'Mettre à jour';
+$_lang['Update from EPC'] = 'Mettre à jour depuis EPC';
+$_lang['You are going to delete this class, do you want to continue ?'] = 'Vous allez supprimer cette liste, voulez-vous continuer ?';
+$_lang['id'] = 'id';
+$_lang['in course'] = 'dans le cours';
+$_lang['This operation could take some time, please wait until it\'s finished'] = 'Cette opération risque de prendre du temps, merci de bien vouloir patienter jusqu\'à ce qu\'elle soit terminée';
+$_lang['recommended'] = 'recommandé';
