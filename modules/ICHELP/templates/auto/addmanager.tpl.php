b32f0747da5d6751afab84696d11cc1ae43262fb
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-04-02 08:22:45 +0000

    fixed file encoding + corrected some typing errors

--- modules/ICHELP/templates/auto/addmanager.tpl.php
+++ modules/ICHELP/templates/auto/addmanager.tpl.php
@@ -1,9 +1,9 @@
-Si vous dÃ©sirez partager la gestion dâ€™un de vos espaces de cours afin quâ€™un collÃ¨gue enseignant, un assistant, ou encore une secrÃ©taire, puisse y a avoir les mÃªmes droits de modification que vous, il suffit de suivre la procÃ©dure ci-dessous :
+Si vous désirez partager la gestion de l'un de vos espaces de cours afin qu'un collègue enseignant, un assistant, ou encore une secrétaire, puisse y a avoir les mêmes droits de modification que vous, il suffit de suivre la procédure suivante :
 
-    - PremiÃ¨rement, assurez-vous que cette personne dispose bien d'un compte sur iCampus. Pour cela il suffit qu'elle se soit connectÃ©e au moins une fois.
-    - Inscrivez-la Ã  votre cours, ou demandez-lui de le faire.
-    - Par dÃ©faut, la personne a le profil utilisateur (tout comme les Ã©tudiants). Vous devez lui donner le profil de gestionnaire via lâ€™outil Utilisateurs.
-      En regard de son nom, cliquez sur le crayon et choisissez le profil "gestionnaire" dans le menu dÃ©roulant, puis validez en cliquant sur "OK".
+    - Premièrement, assurez-vous que cette personne dispose bien d'un compte sur iCampus. Pour cela il suffit qu'elle se soit connectée au moins une fois.
+    - Inscrivez-la à votre cours, ou demandez-lui de le faire.
+    - Par défaut, la personne possède le profil "utilisateur" (tout comme les étudiants). Vous devez lui donner le profil de gestionnaire via l'outil "Utilisateurs".
+      En regard de son nom, cliquez sur le crayon et choisissez le profil "gestionnaire" dans le menu déroulant, puis validez en cliquant sur "OK".
 
 Pour en savoir plus:
 http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/gerer-mon-equipe-dencadrement/#ajouter-un-gestionnaire
