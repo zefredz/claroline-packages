9ccb8e3016a2ecafe9c6d219ae04e102165762cb
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-04-03 07:56:21 +0000

    clean datas in session when user quit form

--- trunk/modules/ICHELP/lib/ticketmanager.lib.php
+++ trunk/modules/ICHELP/lib/ticketmanager.lib.php
@@ -2,7 +2,7 @@
 
 /** Online Help Form
  *
- * @version     ICHELP 0.1 $Revision$ - Claroline 1.11.5
+ * @version     ICHELP 0.5 $Revision$ - Claroline 1.11.5
  * @copyright   2001-2013 Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     ICHELP
