88f5e4ac404caf8e34e74a2ad94cc658f84330df
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-04-08 15:29:02 +0000

    replaced icon by a simple text

--- trunk/modules/ICHELP/entry.php
+++ trunk/modules/ICHELP/entry.php
@@ -1,6 +1,6 @@
 <?php
 if( ! isset( $GLOBALS[ 'tlabelReq' ] ) || $GLOBALS[ 'tlabelReq' ] != 'ICHELP' )
 {
-    $claro_buffer->append( '<a href="'. claro_htmlspecialchars( Url::Contextualize( get_module_url( 'ICHELP' ) . '/controller.php?from=' . base64_encode( $_SERVER[ 'REQUEST_URI' ] ) ) ) . '"><img src="' . get_icon_url( 'big' ) . '" /></a>' );
+    $claro_buffer->append( '<a style="font-weight: bold; color: #336699;" href="'. claro_htmlspecialchars( Url::Contextualize( get_module_url( 'ICHELP' ) . '/controller.php?from=' . base64_encode( $_SERVER[ 'REQUEST_URI' ] ) ) ) . '">Contacter le helpdesk iCampus</a>' );
 }
 ?>
