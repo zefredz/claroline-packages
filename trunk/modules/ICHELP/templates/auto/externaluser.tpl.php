649ce2ed9fc12cf55f00632b2d1fa5bf908efbd8
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-04-11 12:19:59 +0000

    fixed spelling errors

--- trunk/modules/ICHELP/templates/auto/externaluser.tpl.php
+++ trunk/modules/ICHELP/templates/auto/externaluser.tpl.php
@@ -1,8 +1,8 @@
 Si vous souhaitez qu'un utilisateur (étudiant ou enseignant) qui ne possède pas de compte UCL ait accès à votre espace cours dans iCampus, contactez-nous à cette adresse: icampus@uclouvain.be.
 
-Vous devrez fournir son nom, son prénom, une adresse email, le nom de l'institution auquel elle appartient, sa date de naissance, un numéro de téléphone, son année d'étude.
+Vous devrez fournir son nom, son prénom, une adresse email, le nom de l'institution auquel il appartient, sa date de naissance, un numéro de téléphone, son année d'étude.
 
-Si vous souhaitez qu'un groupe de plusieurs utilisateurs "extérieurs" obtienne un compte sur iCampus, faites-nous parvenir un fichier de type EXCEL, contenant, dans des colonnes distinctes:
+Si vous souhaitez qu'un groupe de plusieurs utilisateurs "extérieurs" obtienne un compte sur iCampus, faites-nous parvenir un fichier de type EXCEL, contenant, dans des colonnes distinctes :
 
     nom,
     prénom,
@@ -17,5 +17,5 @@ Pour votre facilité, nous vous fournissons un modèle de fichier à compléter à ce
 http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/files/2010/10/listetype1.xls
 
 
-Pour en savoir plus:
+Pour en savoir plus :
 http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/comprendre-comment-les-utilisateurs-accedent-a-mon-cours/#donner-acces-a-icampus-a-des-utilisateurs-non-ucl
--- trunk/modules/ICHELP/templates/auto/spacerequest.tpl.php
+++ trunk/modules/ICHELP/templates/auto/spacerequest.tpl.php
@@ -1,8 +1,8 @@
-Par defaut, l'espace réservé pour les documents d'un cours sur iCampus est de 100 Mo.
+Par défaut, l'espace réservé pour les documents d'un cours sur iCampus est de 100 Mo.
 
 Si vous pensez avoir besoin d'espace supplémentaire, nous pouvons l'augmenter (dans les limites du raisonnable) en nous envoyant une demande motivée à cette adresse : icampus@uclouvain.be
 
-Mais avant de recourrir à nos services, pensez à vérifier s'il n'est pas possible de rationnaliser l'espace dont vous disposez:
+Mais avant de recourir à nos services, pensez à vérifier s'il n'est pas possible de rationnaliser l'espace dont vous disposez :
 
 - Supprimez les documents obsolètes ou inutilisés
 - Préférez le format PDF aux formats "PowerPoint" ou "Word"
@@ -10,6 +10,6 @@ Mais avant de recourrir à nos services, pensez à vérifier s'il n'est pas possibl
 - Evitez les images de fond dans vos diaporamas
 - Scannez vos livres en niveaux de gris... ou mieux en noir et blanc (profondeur d'échantillonnage de 1 bit)
 
-Sachez qu'iCampus est avant tout une plateforme d'elearning, et n'a pas pour vocation de servir d'espace de stockage et/ou de backup.
+Sachez qu'iCampus est avant tout une plateforme d'elearning et n'a pas pour vocation de servir d'espace de stockage et/ou de backup.
 
-Si vous voulez transmettre à vos étudiants des fichiers volumineux, utilisez de préférences le service "Transvol" de l'UCL: http://www.uclouvain.be/12496.html
+Si vous voulez transmettre à vos étudiants des fichiers volumineux, utilisez de préférence le service "Transvol" de l'UCL : http://www.uclouvain.be/12496.html
