0e04ac8e5931434ee89ffdd4483fcea5719b086a
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-06-06 08:11:48 +0000

    message about cookies enabling modified

--- modules/ICHELP/templates/auto/accessproblem.tpl.php
+++ modules/ICHELP/templates/auto/accessproblem.tpl.php
@@ -1,4 +1,9 @@
-Effectuez les vérifications suivantes :
+<?php if( ! $this->userData[ 'jsEnabled' ] ); ?>
+Vous ne parvenez pas à vous authentifier sur la plateforme car votre navigateur refuse actuellement les cookies!
+Changez la configuration de celui-ci afin que les cookies soient acceptés, puis essayez de vous connecter à nouveau.
+
+Si cela ne fonctionne toujours pas, e<?php else; ?>
+E<?php endif; ?>ffectuez les vérifications suivantes :
 
 <?php if( $this->userData[ 'UCLMember' ] ) : ?>
 Parvenez-vous à vous authentifier sur le portail de l'UCL ( à cette adresse : http://www.uclouvain.be ) ?
@@ -11,9 +16,6 @@ Parvenez-vous à vous authentifier sur le portail de l'UCL ( à cette adresse : ht
 
 Vérifiez bien que vous respectez scrupuleusement la casse en tapant votre mot de passe.
 En effet, le système considère les majuscules et les minuscules comme des caractères distincts.
-
-Vérifiez également que votre navigateur est bien configuré pour accepter les cookies.
-Sans cela, l'authentification ne fonctionnera pas!
 
 Si vous avez activé l'autocomplétion des mots de passe au sein de votre navigateur, désactivez-la.
 Il se peut que ce dernier complète le champ avec des données erronées...
