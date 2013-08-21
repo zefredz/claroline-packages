08c6536f2c218ea3d9e9ce9f0c5dd6a3c5d00d68
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-08-21 08:33:40 +0000

    added a rule for user with mail ending with 'uclouvain.be'

--- modules/ICHELP/templates/auto/passwordlost.tpl.php
+++ modules/ICHELP/templates/auto/passwordlost.tpl.php
@@ -1,4 +1,4 @@
-<?php if( $this->userData[ 'UCLMember' ] ) : ?>
+<?php if( $this->userData[ 'UCLMember' ] || substr( $this->userData[ 'mail' ] , -12 ) == 'uclouvain.be' ) : ?>
 Si vous êtes bien membre de l'UCL, veuillez réinitialliser votre mot de passe à cette adresse : http://www.uclouvain.be/4040
 <?php else : ?>
 Si vous n'êtes pas membre de l'UCL (càd que vous n'avez pas de compte global UCL), vous pouvez récupérer votre mot de passe en entrant l'adresse mail auquel votre compte est associé à cette adresse : <a href="http://icampus.uclouvain.be/module/ICPASSWD/" >http://icampus.uclouvain.be/module/ICPASSWD/</a>
