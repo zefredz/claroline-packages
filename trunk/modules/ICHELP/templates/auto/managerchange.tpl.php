f7f83ef94171ee7fde743ee803c3ea9aa13d388b
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-09-16 13:07:11 +0000

    some changes in automatic answers

--- trunk/modules/ICHELP/templates/auto/managerchange.tpl.php
+++ trunk/modules/ICHELP/templates/auto/managerchange.tpl.php
@@ -1,10 +1,19 @@
-En cas de changement de gestionnaire, la procédure à suivre est la suivante :
+Il existe deux manières de gérer le changement de titulaire d'un cours sur iCampus.
+Le choix de la méthode dépendra des rapports que les deux gestionnaires entretiennent entres-eux.
+
+Si le nouveau titulaire souhaite travailler dans la continuité de l'ancien, avec les mêmes ressources, nous vous conseillons de conserver l'espace de cours existant.
+
+Dans ce cas, la procédure à suivre est la suivante :
 
     1 - Le futur gestionnaire s'inscrit au cours, comme un simple étudiant (si ce n'est déjà fait).
     2 - Il demande alors à l'actuel gestionnaire de lui donner le statut de gestionnaire.
     3 - Le nouveau gestionnaire peut alors désinscrire l'ancien du cours.
+    
 
-En tant que simple intermédiaire technique, l'équipe iCampus n'est pas habilitée à opérer ce changment, mais s'il s'avère que l'ancien gestionnaire n'est plus joignable, nous pouvons le faire avec l'aval écrit d'une autorité facultaire...
+Si par contre, leurs dispositifs pédagogiques respectifs sont assez éloignés, il est préférable de créer un nouvel espace de cours et de désactiver de l'ancien afin d'éviter toute confusion dans l'esprit de vos étudiants.
 
-Pour plus d'infos:
-http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/gerer-mon-equipe-dencadrement/#ajouter-un-gestionnaire
+Pour ce faire, une demande de désactivation de l'ancien cours doit nous parvenir via le helpdesk iCampus, avec accord de l'ancien titulaire.
+S'il s'avère que cette personne est injoignable, une demande écrite (mail) d'une autorité facultaire est nécessaire.
+
+
+Pour plus d'infos : http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/gerer-le-changement-de-titulaire-dun-cours-officiel/
