5bdb6cb898dd61e0ad61d5b37678e95217d50273
kitan1982 <kitan1982@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-11-13 13:28:02 +0000

    LPUTRACK : Refactored class name 'LearnPathTrackingListerner' into 'LearnPathTrackingListener'

--- trunk/modules/LPUTRACK/functions.php
+++ trunk/modules/LPUTRACK/functions.php
@@ -1,9 +1,9 @@
 <?php
 
     if ( count( get_included_files() ) == 1 ) die( '---' );
-    
+
     require_once( get_module_path( 'LPUTRACK' ) . '/lib/learnPathTracking.listener.class.php' );
-    
-    $learnPathTrackingListener = new LearnPathTrackingListerner();
+
+    $learnPathTrackingListener = new LearnPathTrackingListener();
     $learnPathTrackingListener->addListener( 'lp_user_module_progress_modified', 'updateModuleTracking' );
     $learnPathTrackingListener->addListener( 'lp_document_time_update', 'updateDocumentTrackingTime' );
