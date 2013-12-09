12fccec8185ff549b9598c4b807e9b60434fe326
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2013-12-09 10:22:48 +0000

    ICPCRDR: change translation for download link

---
+++ trunk/modules/ICPCRDR/lang/lang_english.php
@@ -0,0 +1,30 @@
+<?php 
+
+$_lang['Add a podcast'] = 'Add a podcast';
+$_lang['Change size'] = 'Change size';
+$_lang['Choose a podcast in the list to start'] = 'Choose a podcast in the list on left to start';
+$_lang['Delete feed'] = 'Delete feed';
+$_lang['Do you want to delete the link: %linkTitle ?'] = 'Do you want to delete the link: %linkTitle ?';
+$_lang['Download this video'] = 'If this video is not playing properly, click here';
+$_lang['Edit feed'] = 'Edit feed';
+$_lang['Edit information for the curent link'] = 'Edit information for the curent link';
+$_lang['Error: unable to add the podcast'] = 'Error: unable to add the podcast';
+$_lang['Error: unable to delete the podcast'] = 'Error: unable to delete the podcast';
+$_lang['Error: unable to edit the podcast'] = 'Error: unable to edit the podcast';
+$_lang['Feed url'] = 'Feed url';
+$_lang['Get title from feed'] = 'Get title from feed';
+$_lang['No podcast for now...'] = 'No podcast for now...';
+$_lang['Podcast edited successfully'] = 'Podcast edited successfully';
+$_lang['Podcast feed not found'] = 'Podcast feed not found';
+$_lang['Sort items in rss order (the newest ones first)'] = 'Sort items in rss order (the newest ones first)';
+$_lang['Sort items in chronological order (the oldest ones first)'] = 'Sort items in chronological order (the oldest ones first)';
+$_lang['Suscribe to podcast'] = 'Suscribe to podcast';
+$_lang['The podcast deleted successfully'] = 'The podcast deleted successfully';
+$_lang['The podcast has been added successfully'] = 'The podcast has been added successfully';
+$_lang['The podcast is now invisible'] = 'The podcast is now invisible';
+$_lang['Unable to change the visibility of the podcast'] = 'Unable to change the visibility of the podcast';
+$_lang['Video podcast reader'] = 'Video podcast reader';
+$_lang['The podcast is now visible'] = 'The podcast is now visible';
+$_lang['Error: unable to change the visibility of the podcast'] = 'Error: unable to change the visibility of the podcast';
+$_lang['If the media doesn\'t play correctly, you can download it using the following link (right-click Save Link As or ctrl+click Save Link As)'] = 'If the media doesn\'t play correctly, you can download it using the following link (right-click Save Link As or ctrl+click Save Link As)';
+
