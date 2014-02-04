04a376f3a6cc09fe5c3a20bf95d10fe6b40142b4
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2014-02-04 13:55:28 +0000

    add a new entry for key required course

---
+++ trunk/modules/ICHELP/templates/auto/keyrequired.tpl.php
@@ -0,0 +1,5 @@
+Certains enseignants désirent limiter l'accès leur site de cours en obligeant les étudiants qui désirent s'y inscrire à entrer une "clé".
+Cette clé est généralement communiquée par l'enseignant lors des séances de cours.
+
+Malheureusement, nous ne sommes pas autorisé à vous révéler cette information.
+Nous vous invitons donc à prendre contact avec l'un des gestionnaires du cours pour lui demander de vous la fournir.
