0966763d243c1f9798ce56c1fe5eee002c6ba509
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2014-02-18 12:53:20 +0000

    fixed bad encoding

--- modules/ICHELP/templates/auto/keyrequired.tpl.php
+++ modules/ICHELP/templates/auto/keyrequired.tpl.php
@@ -1,5 +1,5 @@
-Certains enseignants dÃ©sirent limiter l'accÃ¨s leur site de cours en obligeant les Ã©tudiants qui dÃ©sirent s'y inscrire Ã  entrer une "clÃ©".
-Cette clÃ© est gÃ©nÃ©ralement communiquÃ©e par l'enseignant lors des sÃ©ances de cours.
+Certains enseignants désirent limiter l'accès leur site de cours en obligeant les étudiants qui désirent s'y inscrire à entrer une "clé".
+Cette clé est généralement communiquée par l'enseignant lors des séances de cours.
 
-Malheureusement, nous ne sommes pas autorisÃ© Ã  vous rÃ©vÃ©ler cette information.
-Nous vous invitons donc Ã  prendre contact avec l'un des gestionnaires du cours pour lui demander de vous la fournir.
+Malheureusement, nous ne sommes pas autorisé à vous révéler cette information.
+Nous vous invitons donc à prendre contact avec l'un des gestionnaires du cours pour lui demander de vous la fournir.
