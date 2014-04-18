900afdd7f06a514d9420379d4ff75cfe2bd13ac9
zefredz <zefredz@d46d66fa-363b-41f8-8237-52ce30e9732d>
2014-04-18 08:55:10 +0000

    TESTWS: use kernel keyring instead of the CLKRNG module

--- trunk/modules/TESTWS/connector/ajaxservice.cnr.php
+++ trunk/modules/TESTWS/connector/ajaxservice.cnr.php
@@ -25,13 +25,9 @@ class TESTWS_AjaxRemoteService extends Ajax_Remote_Module_Service
 
     public function isMethodInvokationAllowed ( Ajax_Request $request )
     {
-        if ( file_exists( get_module_path('CLKRNG') . '/' . get_module_entry ( 'CLKRNG' ) ) 
-            && get_module_data('CLKRNG','activation') == 'activated' )
-        {
-            From::Module('CLKRNG')->uses('keyring.lib');
-            Keyring::setOption('errorMode','exception');
-            Keyring::checkForService('testws');
-        }
+        
+        Claro_Keyring_Helper::setOption('errorMode','exception');
+        Claro_Keyring_Helper::checkForService('testws');
         
         return true;
     }
