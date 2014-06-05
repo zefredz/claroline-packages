a19d518dae75db158f2da157e4c15b06fb8e9ea8
kitan1982 <kitan1982@d46d66fa-363b-41f8-8237-52ce30e9732d>
2014-06-05 12:13:32 +0000

    [LPUTRACK] Adds an option to hide progression field in Widget

--- modules/LPUTRACK/conf/def/LPUTRACK.def.conf.inc.php
+++ modules/LPUTRACK/conf/def/LPUTRACK.def.conf.inc.php
@@ -10,11 +10,24 @@ $conf_def['section']['main']['label'] = 'Main';
 $conf_def['section']['main']['description'] = '';
 $conf_def['section']['main']['properties'] = array (
         'LPUTRACK_display_progress',
+        'LPUTRACK_display_progress_widget'
 );
 
 // CONFIG PROPERTIES
 $conf_def_property_list['LPUTRACK_display_progress'] = array (
     'label' => 'Display progress',
+    'description' => '',
+    'default'     => TRUE,
+    'type'        => 'boolean',
+    'acceptedValue' => array (
+        'TRUE'=>'Yes',
+        'FALSE'=>'No'
+    ),
+    'display'     => TRUE,
+    'readonly'    => FALSE
+);
+$conf_def_property_list['LPUTRACK_display_progress_widget'] = array (
+    'label' => 'Display progress in Widget',
     'description' => '',
     'default'     => TRUE,
     'type'        => 'boolean',
