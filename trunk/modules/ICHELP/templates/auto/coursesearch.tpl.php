fc929066f94b6b25e116bd83ffbd468f9af7d7d3
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2014-07-01 13:51:50 +0000

    added missing answer

--- trunk/modules/ICHELP/templates/auto/coursesearch.tpl.php
+++ trunk/modules/ICHELP/templates/auto/coursesearch.tpl.php
@@ -1,4 +1,16 @@
-Vous recherchez des cours en utilisant le formulaire qui s'affiche :
+<?php if( $this->userData['issueType'] == 'courseEnroll' ) : ?>
+Même, si la pratique la plus répandue au sein d'iCampus est de laisser aux étudiants l'initiative de s'inscrire eux-mêmes aux sites de cours qu'ils doivent suivre, certains enseignants préfèrent garder un contrôle total des inscriptions.
+Dans ce cas, les inscriptions sont généralement fermées.
+
+<?php endif; ?>
+<?php if( $this->userData['issueType'] == 'courseDisappeared' ) : ?>
+Il est possible que le titulaire du cours vous a désincrit(e).
+Beaucoup d'enseignants ont pris l'habitude (sur nos recommandations) de nettoyer leurs cours en fin d'année en désinscrivant la totalité des étudiants.
+
+Ne paniquez pas : il suffit de vous ré-inscrire au cours pour retrouver toutes les traces de vos activités passées.
+Si les inscriptions sont fermées, et que vous pensez qu'il s'agit d'une erreur, nous vous invitons à prendre contact par mail avec le titulaire.
+
+Pour rappel,<?php else : ?>P<?php endif; ?>our vous inscrire à un cours, commencez par le rechercher des en utilisant le formulaire qui s'affiche :
     - sur la page d'accueil d'iCampus lorsque vous n'êtes pas authentifié
     - après avoir cliqué sur le lien "M'inscrire à un cours" visible sur votre bureau, si vous êtes connecté
 
