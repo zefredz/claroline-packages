6eb25550b556d1dad4def828a379ab985710a02f
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2014-07-04 11:57:41 +0000

    updated automatic answers

--- trunk/modules/ICHELP/templates/auto/accountcreation.tpl.php
+++ trunk/modules/ICHELP/templates/auto/accountcreation.tpl.php
@@ -1,7 +1,7 @@
-Si vous être inscrit(e) comme étudiant(e) à l'université, vous pouvez accéder à iCampus en utilisant votre identifiant global UCL.
+Si vous être inscrit(e) comme étudiant(e) à l'université, ou si vous êtes membre du personnel, vous pouvez accéder à iCampus en utilisant votre identifiant global UCL.
 Lors de votre première connexion, votre compte iCampus sera automatiquement créé.
 
 Attention : votre compte UCL doit avoir été préalablement activé !
 Si ce n'est pas le cas, commencez par le faire, en allant à cette adresse : http://www.uclouvain.be/4040.
 
-Si vous êtes extérieur(e) à l'UCL mais que vous suivez un programme d'étude pour lequel un accès à iCampus est nécessaire, adressez une demande justifiée de création de compte extérieur à cette adresse : icampus@uclouvain.be.
+Si vous êtes extérieur(e) à l'UCL mais que vous êtes impliqué dans un programme d'étude pour lequel un accès à iCampus est nécessaire, adressez une demande de création de compte extérieur via votre responsable de programme.
--- trunk/modules/ICHELP/templates/auto/managerbecome.tpl.php
+++ trunk/modules/ICHELP/templates/auto/managerbecome.tpl.php
@@ -3,7 +3,6 @@ Si vous désirez devenir gestionnaire d'un cours existant, il suffit de suivre la
     1 - Incrivez-vous d'abord au cours en question
     2 - Contactez l'actuel gestionnaire afin qu'il vous donne le statut de co-gestionnaire.
 
-En tant que simple intermédiaire technique, l'équipe iCampus n'est pas habilitée à opérer ce changement, mais s'il s'avère que l'ancien gestionnaire n'est plus joignable, nous pouvons le faire avec l'aval écrit d'une autorité facultaire...
+La procédure est décrite dans le détail à cette adresse : http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/gerer-le-changement-de-titulaire-dun-cours-officiel/
 
-Pour en savoir plus:
-http://blogs.uclouvain.be/aideicampus/manuel-enseignant/administrer-mes-espaces-de-cours/gerer-mon-equipe-dencadrement/#ajouter-un-gestionnaire
+Notez enfin, qu'en tant que simple intermédiaire technique, l'équipe iCampus n'est pas habilitée à opérer ce changement, mais s'il s'avère que l'ancien gestionnaire n'est plus joignable, nous pouvons le faire avec l'aval écrit d'une autorité facultaire...
