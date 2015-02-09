3784de6724c08253f19cc85af54dcc8b8e3d727a
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2015-02-09 13:01:20 +0000

    Oops... forgotten echoes.

--- trunk/modules/MOODLEEX/templates/moodletype/matching.tpl.php
+++ trunk/modules/MOODLEEX/templates/moodletype/matching.tpl.php
@@ -2,9 +2,9 @@
 <?php foreach( $this->question->answerList as $answer ) : ?>
 <subquestion <?php if( MOODLEEX_is_html( $answer[ 'proposition' ] )
                    ||  MOODLEEX_is_html( $answer[ 'answer' ] ) ) : ?>format="html"<?php endif; ?>>
-  <text><?php MOODLEEX_bake( $answer[ 'proposition' ] ); ?></text>
+  <text><?php echo MOODLEEX_bake( $answer[ 'proposition' ] ); ?></text>
   <answer>
-    <text><?php MOODLEEX_bake( $answer[ 'answer' ] ); ?></text>
+    <text><?php echo MOODLEEX_bake( $answer[ 'answer' ] ); ?></text>
   </answer>
 </subquestion>
 <?php endforeach; ?>
