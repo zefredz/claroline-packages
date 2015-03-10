dcb7e80c89b10faed857a947b6a225a969b24c37
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2015-03-10 10:51:48 +0000

    fixed weird sentence + added new entry

---
+++ modules/ICHELP/templates/auto/mailstudent.tpl.php
@@ -0,0 +1,13 @@
+Lorsqu'une personne dispose d'une adresse mail student.uclouvain.be et d'une adresse mail uclouvain.be, c'est l'adresse mail student qui est privilégiée dans iCampus. Cette configuration n'est malheureusement pas modifiable.
+
+Les raisons qui nous ont été données sont des raisons de confidentialités liées au fait que les boîtes @student sont hébergées chez Google.
+
+Certains responsables d'entités ont donc demandé à ce qu'il y ait une séparation stricte entre les boîtes étudiantes et personnelles et que les boîtes étudiantes soient utilisées exclusivement pour tout ce qui concernait l'enseignement, c'est-à-dire y compris pour iCampus.
+
+Nous cherchons une solution avec l'équipe en charge de la gestion du mail afin de ne pas forcer ce comportement par défaut pour l'ensemble des membres de l'UCL, mais nous n'en n'avons trouvé aucune jusqu'ici.
+
+D'ici-là, nous vous proposons de créer une redirection de votre adresse "@student.uclouvian.be" vers votre adresse "@uclouvain.be".
+
+Un tutoriel expliquant comment faire est disponible à cette adresse :
+
+http://blogs.uclouvain.be/aideicampus/faq/rediriger-les-mails-studentuclouvainbe-vers-ma-boite-uclouvainbe/
