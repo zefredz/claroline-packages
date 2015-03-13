b06d929c8ae56521c46be1f1ba1405a8bcdf55e9
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2015-03-13 14:21:33 +0000

    fixed mispelling errors + encoding of file lang_english.php

--- modules/MOODLEEX/lang/lang_english.php
+++ modules/MOODLEEX/lang/lang_english.php
@@ -15,13 +15,13 @@ $_lang[ 'Excercises exporter' ] = 'Excercises exporter';
 $_lang[ 'Export failed' ] = 'Export failed';
 $_lang[ 'Download all course\'s documents in a single zip file' ] = 'Download all course\'s documents in a single zip file';
 
-$_lang[ 'Items' ] = 'Eléments';
-$_lang[ 'Videos' ] = 'Vidéos';
+$_lang[ 'Items' ] = 'Items';
+$_lang[ 'Videos' ] = 'Videos';
 $_lang[ 'Documents and links' ] = 'Documents and links';
 
 $_lang[ 'Questions from quiz "%quizTitle"' ] = 'Questions from quiz "%quizTitle"';
 
-$_lang[ '[Module introduction text] %warning' ] = '<span class="globalDesc">For the moment, the export module enables you to migrate <a href="#exportDocuments">documents and links</a>, <a href="#exportExercises">exercises</a> and <a href="exportVideos">videos</a>. See the <a href="http://blogs.uclouvain.be/aideicampus/manuel-enseignant/migrer-vers-moodleucl/migrer-un-cours-vers-moodleucl/" target="_blank">iCampus manual page documenting the migration procedure in detail</a>.<br />We pursue the module development for giving you at least an archive of other tools data.<br />In case of migration problem, contact <a href="mailto://migration-icampus@uclouvain.be" >migration-icampus@uclouvain.be</a>.<br />If you migrate a course on MoodleUCL, please <a href="mailto://icampus@uclouvain.be">contact us to ask for your iCampus course desactivation</a>. This will avoid confusions among students ;-)</span>';
-$_lang[ '[Documents export] %warning' ] = 'WWith the documents and links zip archive given below, you may reimport all documents with their structure in MoodleUCL.';
+$_lang[ '[Module introduction text] %warning' ] = '<span class="globalDesc">For the moment, the export module enables you to migrate <a href="#exportDocuments">documents and links</a>, <a href="#exportExercises">exercises</a> and <a href="exportVideos">videos</a>. See the <a href="http://blogs.uclouvain.be/aideicampus/manuel-enseignant/migrer-vers-moodleucl/migrer-un-cours-vers-moodleucl/" target="_blank">iCampus manual page documenting the migration procedure in detail</a>.<br />We pursue the module development for giving you at least an archive of other tools data.<br />In case of migration problem, contact <a href="mailto:migration-icampus@uclouvain.be" >migration-icampus@uclouvain.be</a>.<br />If you migrate a course on MoodleUCL, please <a href="mailto:icampus@uclouvain.be">contact us to ask for your iCampus course desactivation</a>. This will avoid confusions among students ;-)</span>';
+$_lang[ '[Documents export] %warning' ] = 'With the documents and links zip archive given below, you may reimport all documents with their structure in MoodleUCL.';
 $_lang[ '[Exercices export] %warning' ] = 'For each iCampus exercise, the export module gives you a file that enables you to import the exercise in a MoodleUCL course.';
 $_lang[ '[Videos export] %warning' ] = 'In MoodleUCL, you better have to integrate videos one by one distributing them in the different course sections. That is the reason why the export module gives you direct links trough individual videos of your feed.';
