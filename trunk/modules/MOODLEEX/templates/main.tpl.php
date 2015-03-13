fd14a5c8da3a1e128e4d8f2fbb281f409d0a3159
ffervaille <ffervaille@d46d66fa-363b-41f8-8237-52ce30e9732d>
2015-03-13 14:16:27 +0000

    version 1.0 final : reworked information text

--- trunk/modules/MOODLEEX/templates/main.tpl.php
+++ trunk/modules/MOODLEEX/templates/main.tpl.php
@@ -1,13 +1,13 @@
 <div id="mainContent">
     <fieldset id="documents">
-        <legend><?php echo get_lang( 'Documents and links' ); ?></legend>
+        <legend><a name="exportDocuments"></a><?php echo get_lang( 'Documents and links' ); ?></legend>
         <span class="sectionDesc">
             <?php echo get_lang( '[Documents export] %warning' ); ?>
         </span>
         <?php include 'documentlist.tpl.php'; ?>
     </fieldset>
     <fieldset id="exercices">
-        <legend><?php echo get_lang( 'Exercises' ); ?></legend>
+        <legend><a name="exportExercises"></a><?php echo get_lang( 'Exercises' ); ?></legend>
         <span class="sectionDesc">
             <?php echo get_lang( '[Exercices export] %warning' , array( '%warning' => $this->warningText ) ); ?>
         </span>
@@ -15,17 +15,11 @@
     </fieldset>
 <?php if( $this->podcastActivated ) : ?>
     <fieldset id="videos">
-        <legend><?php echo get_lang( 'Videos' ); ?></legend>
+        <legend><a name="exportVideos"></a><?php echo get_lang( 'Videos' ); ?></legend>
         <span class="sectionDesc">
             <?php echo get_lang( '[Videos export] %warning' , array( '%warning' => $this->warningText ) ); ?>
         </span>
         <?php include 'videolist.tpl.php'; ?>
     </fieldset>
 <?php endif; ?>
-</div>
-<div class="addInfos">
-    <?php echo get_lang( '[Additionnal infos 1] %warning' , array( '%warning' => $this->warningText ) ); ?>
-</div>
-<div class="addInfos">
-    <?php echo get_lang( '[Additionnal infos 2] %warning' , array( '%warning' => $this->warningText ) ); ?>
 </div>
